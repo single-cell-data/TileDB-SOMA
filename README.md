45ecf5cd73743aae9aee0ae65c7a6505166547a9
Seth Shelnutt <Shelnutt2@gmail.com>
2022-01-21 09:37:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<a href="https://tiledb.com"><img src="https://github.com/TileDB-Inc/TileDB/raw/dev/doc/source/_static/tiledb-logo_color_no_margin_@4x.png" alt="TileDB logo" width="400"></a>
+
+[![Build Status](https://img.shields.io/azure-devops/build/tiledb-inc/836549eb-f74a-4986-a18f-7fbba6bbb5f0/8/master?label=Azure%20Pipelines&logo=azure-pipelines&style=flat-square)](https://dev.azure.com/TileDB-Inc/CI/_build/latest?definitionId=8&branchName=master)
+[![Docker-CLI](https://img.shields.io/static/v1?label=Docker&message=tiledbvcf-cli&color=099cec&logo=docker&style=flat-square)](https://hub.docker.com/repository/docker/tiledb/tiledbvcf-cli)
+[![Docker-Py](https://img.shields.io/static/v1?label=Docker&message=tiledbvcf-py&color=099cec&logo=docker&style=flat-square)](https://hub.docker.com/repository/docker/tiledb/tiledbvcf-py)
+
+# TileDB-SingleCell
+
+A C++ library with APIs in Python and R for efficient storage and retrieval of single cell data using [TileDB Embedded][tiledb].
+
+## Features
+
+
+## What's Included?
+
+- APIs for C++, Python, and Java
+
+
+# Code of Conduct
+
+All participants in TileDB spaces are expected to adhere to high standards of
+professionalism in all interactions. This repository is governed by the
+specific standards and reporting procedures detailed in depth in the
+[TileDB core repository Code Of Conduct](
+https://github.com/TileDB-Inc/TileDB/blob/dev/CODE_OF_CONDUCT.md).
---
+++ libtiledbsc/cmake/Modules/FindTileDB_EP.cmake
@@ -0,0 +1,117 @@
+#
+# FindTileDB_EP.cmake
+#
+#
+# The MIT License
+#
+# Copyright (c) 2018 TileDB, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Finds the TileDB library, installing with an ExternalProject as necessary.
+
+# If TileDB was installed as an EP, need to search the EP install path also.
+set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "${EP_INSTALL_PREFIX}")
+
+if (FORCE_EXTERNAL_TILEDB)
+  find_package(TileDB CONFIG PATHS ${EP_INSTALL_PREFIX} NO_DEFAULT_PATH)
+else()
+  find_package(TileDB CONFIG)
+endif()
+
+if (TILEDB_FOUND)
+  get_target_property(TILEDB_LIB TileDB::tiledb_shared IMPORTED_LOCATION_RELEASE)
+  message(STATUS "Found TileDB: ${TILEDB_LIB}")
+else()
+  if (SUPERBUILD)
+    message(STATUS "Adding TileDB as an external project")
+    if (TILEDB_S3 STREQUAL "OFF")
+      message(STATUS "TileDB will be built WITHOUT S3 support")
+    endif()
+
+    # Try to download prebuilt artifacts unless the user specifies to build from source
+    if(DOWNLOAD_TILEDB_PREBUILT)
+        if (WIN32) # Windows
+          SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.6.0/tiledb-windows-x86_64-2.6.0-66f4b41.zip")
+          SET(DOWNLOAD_SHA1 "7ff516f55e19f460ce68eb1dcb52e17f72f845a3")
+        elseif(APPLE) # OSX
+
+          if (CMAKE_OSX_ARCHITECTURES STREQUAL x86_64 OR CMAKE_SYSTEM_PROCESSOR MATCHES "(x86_64)|(AMD64|amd64)|(^i.86$)")
+            SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.6.0/tiledb-macos-x86_64-2.6.0-66f4b41.tar.gz")
+            SET(DOWNLOAD_SHA1 "3c3a80aa2217b0c38e0fccca5c8fc4e63433e9fa")
+          elseif (CMAKE_OSX_ARCHITECTURES STREQUAL arm64 OR CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "^arm")
+            SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.6.0/tiledb-macos-arm64-2.6.0-66f4b41.tar.gz")
+            SET(DOWNLOAD_SHA1 "f59d766009168afb46dc84dbc33931b05921f8c2")
+          endif()
+        else() # Linux
+          SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.6.0/tiledb-linux-x86_64-2.6.0-66f4b41.tar.gz")
+          SET(DOWNLOAD_SHA1 "57fda45fd19f9e0dd7734de2f1a8eca427de0b10")
+        endif()
+
+        ExternalProject_Add(ep_tiledb
+                PREFIX "externals"
+                URL ${DOWNLOAD_URL}
+                URL_HASH SHA1=${DOWNLOAD_SHA1}
+                CONFIGURE_COMMAND ""
+                BUILD_COMMAND ""
+                UPDATE_COMMAND ""
+                PATCH_COMMAND ""
+                TEST_COMMAND ""
+                INSTALL_COMMAND
+                    ${CMAKE_COMMAND} -E copy_directory ${EP_BASE}/src/ep_tiledb ${EP_INSTALL_PREFIX}
+                LOG_DOWNLOAD TRUE
+                LOG_CONFIGURE FALSE
+                LOG_BUILD FALSE
+                LOG_INSTALL FALSE
+                )
+    else() # Build from source
+        ExternalProject_Add(ep_tiledb
+          PREFIX "externals"
+          URL "https://github.com/TileDB-Inc/TileDB/archive/2.6.0.zip"
+          URL_HASH SHA1=e679786ceb76eb91e5b43c1e5de911aa959bf699
+          DOWNLOAD_NAME "tiledb.zip"
+          CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${EP_INSTALL_PREFIX}
+            -DCMAKE_PREFIX_PATH=${EP_INSTALL_PREFIX}
+            -DTILEDB_S3=${TILEDB_S3}
+            -DTILEDB_VERBOSE=ON
+            -DTILEDB_SERIALIZATION=ON
+            -DTILEDB_TESTS=OFF
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+          UPDATE_COMMAND ""
+          INSTALL_COMMAND
+            ${CMAKE_COMMAND} --build . --target install-tiledb
+          LOG_DOWNLOAD TRUE
+          LOG_CONFIGURE TRUE
+          LOG_BUILD TRUE
+          LOG_INSTALL TRUE
+        )
+    endif()
+
+    list(APPEND FORWARD_EP_CMAKE_ARGS -DEP_TILEDB_BUILT=TRUE)
+    list(APPEND EXTERNAL_PROJECTS ep_tiledb)
+  else()
+    message(FATAL_ERROR "Unable to find TileDB library.")
+  endif()
+endif()
+
+if (EP_TILEDB_BUILT AND TARGET TileDB::tiledb_shared)
+  include(TileDBCommon)
+  install_target_libs(TileDB::tiledb_shared)
+endif()
