52c2cc475d9bd02a41183ce022adbea5c33a3cdc
Bruce Martin <bruce@chanzuckerberg.com>
2022-05-02 08:29:10 -0700

    Expand from_anndata test coverage (#47)
    * first cut at type conversion checks for from_anndata
    
    * lint
    
    * clean up comments
    
    * add test for issue 46
    
    * resolve anndata warning
    
    * update comments

---
+++ apis/python/tests/test_mem_fs.py
@@ -0,0 +1,30 @@
+from anndata import AnnData
+import tiledb
+from tiledbsc import SOMA
+import pandas as pd
+import numpy as np
+
+import pytest
+
+# TODO: re-enable when #46 is resolved
+@pytest.mark.skip(reason="Fails: filed as issue #46")
+def test_from_anndata_memfs():
+    n_obs = 1000
+    n_var = 1
+    obs = pd.DataFrame(data={"A": np.arange(n_obs)}, index=np.arange(n_obs).astype(str))
+    var = pd.DataFrame(data={"A": np.arange(n_var)}, index=np.arange(n_var).astype(str))
+    X = np.ones((n_obs, n_var), dtype=np.float32)
+    adata = AnnData(X=X, obs=obs, var=var, dtype=X.dtype)
+
+    path = "mem://foo"
+    sg = SOMA(path)
+    sg.from_anndata(adata)
+
+    soma_group = tiledb.Group(sg.uri)
+    assert soma_group is not None
+    members = [mbr.uri for mbr in soma_group]
+    assert f"{path}/X" in members
+    assert f"{path}/obs" in members
+    assert f"{path}/var" in members
+
+    soma_group.close()
