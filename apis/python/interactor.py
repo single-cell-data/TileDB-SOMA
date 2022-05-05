da30a644729d04e8ffb458549e2360f44e8566cb
John Kerl <kerl.john.r@gmail.com>
2022-05-05 13:15:27 -0400

    dev-tooling: python -i on shebang line for interactor/peek

--- apis/python/interactor.py
+++ apis/python/interactor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -i
 
 # Invoke this with, for example,
 #
---
+++ apis/python/peek.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python -i
+
+# Invoke this with, for example,
+#
+#   python -i interactor.py ~/scdata/anndata/pbmc-small.h5ad
+#
+# -- then you can inspect the anndata object.
+
+import tiledb
+import tiledbsc
+import sys, os
+
+import anndata
+
+import pandas
+import numpy
+import scipy
+
+input_path = 'anndata/pbmc3k_processed.h5ad'
+if len(sys.argv) != 2:
+    print(f"{sys.argv[0]}: need just one .h5ad file name.", file=sys.stderr)
+    sys.exit(1)
+
+input_path  = sys.argv[1]
+anndata = anndata.read_h5ad(input_path)
+
+# Interact at the prompt now:
+# * anndata.X
+# * anndata.obs.keys()
+# * anndata.obs.head()
+# * anndata.obs.index
+# * anndata.obs.values
+# * etc
