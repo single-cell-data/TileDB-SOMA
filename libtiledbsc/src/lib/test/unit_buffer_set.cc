75e2c7e5202cd4f2fad0e9a9cbff899fa48b6036
George Powley <george.powley@gmail.com>
2022-05-16 22:39:31 -0400

    fix windows build

--- libtiledbsc/src/lib/test/unit_buffer_set.cc
+++ libtiledbsc/src/lib/test/unit_buffer_set.cc
@@ -2,6 +2,7 @@
 #include <tiledb/tiledb>
 
 #include <memory>
+#include <stdexcept>  // for windows
 #include <vector>
 
 #include <catch2/catch_test_macros.hpp>
--- libtiledbsc/test/unit_sc.cc
+++ libtiledbsc/test/unit_sc.cc
@@ -2,6 +2,7 @@
 #include <tiledb/tiledb>
 
 #include <memory>
+#include <stdexcept>  // for windows error C2039
 #include <vector>
 
 #include <catch2/catch_test_macros.hpp>
