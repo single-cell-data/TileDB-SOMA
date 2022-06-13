f75ffb6cbe431c9fc7700b86c457de1dae1e7593
John Kerl <kerl.john.r@gmail.com>
2022-06-12 23:36:42 -0400

    code-review feedback

--- apis/python/tests/test_soco_slice_query.py
+++ apis/python/tests/test_soco_slice_query.py
@@ -40,11 +40,11 @@ def test_soco_slice_query(tmp_path):
 
     soma_slices = []
     for soma in soco:
-        # E.g. querying for 'cell_type == "blood"' but this SOMA doesn'tiledbsc have a cell_type column in
+        # E.g. querying for 'cell_type == "blood"' but this SOMA doesn't have a cell_type column in
         # its obs at all.
         if not soma.obs.has_attr_names(obs_attr_names):
             continue
-        # E.g. querying for 'feature_name == "MT-CO3"' but this SOMA doesn'tiledbsc have a feature_name
+        # E.g. querying for 'feature_name == "MT-CO3"' but this SOMA doesn't have a feature_name
         # column in its var at all.
         if not soma.var.has_attr_names(var_attr_names):
             continue
