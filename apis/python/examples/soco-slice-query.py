09fd6504c67805f927a3a3c7ea2b98830c75178b
John Kerl <kerl.john.r@gmail.com>
2022-06-13 22:46:06 -0400

    Query obs/var dataframes more conservatively at the attribute level

--- apis/python/examples/soco-slice-query.py
+++ apis/python/examples/soco-slice-query.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import tiledbsc
-import os
+import os, sys
 import shutil
 from typing import List
 
@@ -38,45 +38,64 @@ def soco_attribute_filter_and_store(
 
 
 # ----------------------------------------------------------------
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write("Need option")
+        sys.exit(1)
 
-print()
-print("TWO-SIDED QUERY")
-soco_attribute_filter_and_store(
-    soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
-    output_h5ad_path="mini-atlas-two-sided.h5ad",
-    output_soma_path="mini-atlas-two-sided",
-    obs_attr_names=["cell_type"],
-    obs_query_string='cell_type == "B cell"',
-    var_attr_names=["feature_name"],
-    var_query_string='feature_name == "MT-CO3"',
-)
+    if sys.argv[1] == "1":
+        print()
+        print("TWO-SIDED QUERY")
+        soco_attribute_filter_and_store(
+            soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
+            output_h5ad_path="mini-atlas-two-sided.h5ad",
+            output_soma_path="mini-atlas-two-sided",
+            obs_attr_names=["cell_type"],
+            obs_query_string='cell_type == "B cell"',
+            var_attr_names=["feature_name"],
+            var_query_string='feature_name == "MT-CO3"',
+        )
 
-print()
-print("OBS-ONLY QUERY")
-soco_attribute_filter_and_store(
-    soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
-    output_h5ad_path="mini-atlas-obs-sided.h5ad",
-    output_soma_path="mini-atlas-obs-sided",
-    obs_attr_names=["cell_type"],
-    obs_query_string='cell_type == "B cell"',
-)
+    if sys.argv[1] == "2":
+        print()
+        print("OBS-ONLY QUERY")
+        soco_attribute_filter_and_store(
+            soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
+            output_h5ad_path="mini-atlas-obs-sided.h5ad",
+            output_soma_path="mini-atlas-obs-sided",
+            obs_attr_names=["cell_type"],
+            obs_query_string='cell_type == "B cell"',
+        )
 
-print()
-print("VAR-ONLY QUERY")
-soco_attribute_filter_and_store(
-    soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
-    output_h5ad_path="mini-atlas-var-sided.h5ad",
-    output_soma_path="mini-atlas-var-sided",
-    var_attr_names=["feature_name"],
-    var_query_string='feature_name == "MT-CO3"',
-)
+    if sys.argv[1] == "3":
+        print()
+        print("VAR-ONLY QUERY")
+        soco_attribute_filter_and_store(
+            soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
+            output_h5ad_path="mini-atlas-var-sided.h5ad",
+            output_soma_path="mini-atlas-var-sided",
+            var_attr_names=["feature_name"],
+            var_query_string='feature_name == "MT-CO3"',
+        )
 
-print()
-print("OBS-ONLY QUERY")
-soco_attribute_filter_and_store(
-    soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
-    output_h5ad_path="cell-ontology-236.h5ad",
-    output_soma_path="cell-ontology-236",
-    obs_attr_names=["cell_type_ontology_term_id"],
-    obs_query_string='cell_type_ontology_term_id == "CL:0000236"',
-)
+    if sys.argv[1] == "4":
+        print()
+        print("OBS-ONLY QUERY")
+        soco_attribute_filter_and_store(
+            soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
+            output_h5ad_path="cell-ontology-236.h5ad",
+            output_soma_path="cell-ontology-236",
+            obs_attr_names=["cell_type_ontology_term_id"],
+            obs_query_string='cell_type_ontology_term_id == "CL:0000236"',
+        )
+
+    if sys.argv[1] == "5":
+        print()
+        print("OBS-ONLY QUERY")
+        soco_attribute_filter_and_store(
+            soco=tiledbsc.SOMACollection("/Users/johnkerl/mini-corpus/atlas"),
+            output_h5ad_path="kidney.h5ad",
+            output_soma_path="kidney",
+            obs_attr_names=["tissue"],
+            obs_query_string='tissue == "kidney"',
+        )
