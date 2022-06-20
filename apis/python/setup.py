05c11af3b7893071a5697e6f9d9fb2224d9da020
John Kerl <kerl.john.r@gmail.com>
2022-06-20 16:34:14 -0400

    trying a suggestion

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -1,4 +1,11 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        setup_requires=["setuptools_scm"],
+        use_scm_version={
+            "version_scheme": "guess-next-dev",
+            "local_scheme": "dirty-tag",
+            "write_to": "tiledb/ml/version.py",
+        },
+    )
