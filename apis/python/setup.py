28b3a081425aabb9da2a5578ac4bcfd825388108
John Kerl <kerl.john.r@gmail.com>
2022-06-20 16:41:34 -0400

    iterating

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
         use_scm_version={
             "version_scheme": "guess-next-dev",
             "local_scheme": "dirty-tag",
-            "write_to": "tiledb/ml/version.py",
+            # This is weird. The write_to requires apis/python/..., as though the "pwd" is two
+            # levels up from here.  Yet the root requires ../.., as though the "pwd" is right here.
+            "write_to": "apis/python/version.py",
+            "root": "../..",
         },
     )
