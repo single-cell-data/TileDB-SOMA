2dc14e27b524d27b2d310a99fd0ec718058d7b60
John Kerl <kerl.john.r@gmail.com>
2022-06-27 22:19:00 -0400

    Make it easier to see progress indicators in notebook ingests (#202)

--- apis/python/src/tiledbsc/logging.py
+++ apis/python/src/tiledbsc/logging.py
@@ -1,3 +1,21 @@
 import logging
 
 logger = logging.getLogger(__name__)  # Nominally __name__ is 'tiledbsc'
+
+
+def info():
+    """
+    Sets tiledbsc logging to an INFO level. Use `tiledbsc.logging.info()` in notebooks to see
+    progress indicators for data ingestion.
+    """
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+
+
+def warning():
+    """
+    Sets tiledbsc logging to a WARNING level. Use `tiledbsc.logging.info()` in notebooks to suppress
+    progress indicators for data ingestion.
+    """
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(logging.StreamHandler())
