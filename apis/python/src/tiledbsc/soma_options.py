c6f65e445f072cb702762a4e17ab50d2f1894a62
John Kerl <kerl.john.r@gmail.com>
2022-08-13 11:20:23 -0400

    Use max_workers config for bounded thread-pool execution (#240)

--- apis/python/src/tiledbsc/soma_options.py
+++ apis/python/src/tiledbsc/soma_options.py
@@ -23,3 +23,5 @@ class SOMAOptions:
     # Allows relocatability for local disk / S3, and correct behavior for TileDB Cloud
     member_uris_are_relative: Optional[bool] = None
     allows_duplicates: bool = False
+
+    max_thread_pool_workers: int = 8
