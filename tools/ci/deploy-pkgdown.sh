c16bd53e1bd8b81912f53a0baf0aeae839091be4
John Kerl <kerl.john.r@gmail.com>
2022-09-16 14:00:56 -0400

    iterate on pkgdown

---
+++ tools/ci/deploy-pkgdown.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd apis/R
+
+git config --local user.name "$GITHUB_ACTOR"
+git config --local user.email "$GITHUB_ACTOR@users.noreply.github.com"
+Rscript -e 'pkgdown::deploy_to_branch(new_process = FALSE)'
+
+cd ../..
