e101e18e59013149eaf979458dc08891457e283e
John Kerl <kerl.john.r@gmail.com>
2022-09-16 13:26:04 -0400

    build-and-check-r-package.sh

---
+++ tools/ci/build-and-check-r-package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd apis/R
+
+echo "::group::Build TileDB R package"
+## build package
+R CMD build --no-build-vignettes --no-manual .
+echo "::endgroup::"
+
+echo "::group::Check TileDB R package"
+## set flag to tolerate missing suggested packages (as eg vignette building tools)
+export _R_CHECK_FORCE_SUGGESTS_=FALSE
+## set an 'under valgrind' variable (see SC-19185)
+export _RUNNING_UNDER_VALGRIND_=TRUE
+## tell valgrind to use '-s'
+export VALGRIND_OPTS="-s"
+## check package
+R CMD check --use-valgrind --as-cran --no-manual --ignore-vignettes tiledb_*.tar.gz
+echo "::endgroup::"
+
+cd ../..
