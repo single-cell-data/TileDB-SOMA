fd49d70421d66c119c37939c060226ed5b574dc8
John Kerl <kerl.john.r@gmail.com>
2022-09-16 16:47:38 -0400

    more code-review feedback

--- tools/ci/install-r-dependencies.sh
+++ tools/ci/install-r-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Context: .github/workflows/*.yaml
 export R_REMOTES_NO_ERRORS_FROM_WARNINGS='true'
-Rscript -e 'install.packages(c("remotes", "rcmdcheck"))'
+Rscript -e 'install.packages("remotes")'
 cd apis/R
 Rscript -e 'remotes::install_deps(dependencies = TRUE)'
 cd ../..
