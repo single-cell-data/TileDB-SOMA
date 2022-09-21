c3f436fc59095bc007bdc7772c936b2df4c9b173
George Powley <george.powley@gmail.com>
2022-09-21 14:51:55 -0400

    update cli example; add more c++ comments

--- libtiledbsoma/include/tiledbsoma/array_buffers.h
+++ libtiledbsoma/include/tiledbsoma/array_buffers.h
@@ -53,26 +53,70 @@ class ArrayBuffers {
     ArrayBuffers(ArrayBuffers&&) = default;
     ~ArrayBuffers() = default;
 
+    /**
+     * @brief Return the buffer with the given name.
+     *
+     * @param name Column name
+     * @return std::shared_ptr<ColumnBuffer> Column buffer
+     */
     std::shared_ptr<ColumnBuffer> at(const std::string& name) {
+        if (!contains(name)) {
+            throw TileDBSOMAError(
+                fmt::format("[ArrayBuffers] column '{}' does not exist", name));
+        }
         return buffers_[name];
     }
 
+    /**
+     * @brief Return true if a buffer with the given name exists.
+     *
+     * @param name Column name
+     * @return True if a buffer with the given name exists
+     */
     bool contains(const std::string& name) {
         return buffers_.contains(name);
     }
 
+    /**
+     * @brief Add a column buffer with the given name to the ArrayBuffers,
+     * maintaining the insertion order.
+     *
+     * @param name Column name
+     * @param buffer Column buffer
+     */
     void emplace(
         const std::string& name, std::shared_ptr<ColumnBuffer> buffer) {
+        if (contains(name)) {
+            throw TileDBSOMAError(
+                fmt::format("[ArrayBuffers] column '{}' already exists", name));
+        }
         names_.push_back(name);
         buffers_.emplace(name, buffer);
     }
 
+    /**
+     * @brief Returns the ordered vector of names.
+     *
+     * @return const std::vector<std::string>& Vector of names
+     */
     const std::vector<std::string>& names() {
         return names_;
     }
 
+    /**
+     * @brief Returns the number of rows in the array buffer.
+     *
+     * @return uint64_t Number of rows
+     */
+    uint64_t num_rows() const {
+        return buffers_.at(names_.front())->size();
+    }
+
    private:
+    // A vector of column names that maintains the order the columns were added
     std::vector<std::string> names_;
+
+    // Map: column name -> ColumnBuffer
     std::unordered_map<std::string, std::shared_ptr<ColumnBuffer>> buffers_;
 };
 
