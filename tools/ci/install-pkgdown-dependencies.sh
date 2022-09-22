948835ce078f04e4813869cdb752b5e3ca2c78af
John Kerl <john.kerl@tiledb.com>
2022-09-22 04:24:04 +0000

    rename apis/R -> apis/r

--- tools/ci/install-pkgdown-dependencies.sh
+++ tools/ci/install-pkgdown-dependencies.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 # Context: .github/workflows/*.yaml
 export R_REMOTES_NO_ERRORS_FROM_WARNINGS='true'
 Rscript -e 'install.packages(c("remotes", "pkgdown"))'
-cd apis/R
+cd apis/r
 Rscript -e 'remotes::install_deps(dependencies = TRUE)'
 Rscript -e 'remotes::install_local()'
 cd ../..
