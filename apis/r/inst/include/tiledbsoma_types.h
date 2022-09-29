07a44aac60c38c4139a039eaac6a47b24dd4f3df
Dirk Eddelbuettel <edd@debian.org>
2022-09-29 12:05:25 -0500

    Add initial C++ support for the R package

---
+++ apis/r/inst/include/tiledbsoma_types.h
@@ -0,0 +1,10 @@
+
+// this file becomes accessible to the auto-generated RcppExports.cpp
+
+#include <cstring>
+#include <R_ext/Print.h>
+
+extern "C" {
+  void REprintf(const char *, ...);
+  void R_FlushConsole(void);
+}
