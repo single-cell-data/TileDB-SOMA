f061436b581319100b809881b6f9e23f87cad0e9
Dirk Eddelbuettel <edd@debian.org>
2022-10-03 16:18:23 -0500

    Add initial C++ support for the R package (#352)
    * Add initial C++ support for the R package
    
    * small doc update (and RcppExports rebuild)
    
    * support spdlog use in the R API via RcppSpdlog setup
    
    * removed some commented out code, standardize one interface

---
+++ apis/r/inst/include/tiledbsoma/managed_query.h
@@ -0,0 +1,285 @@
+/**
+ * @file   managed_query.h
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2022 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ *   This declares the managed query API
+ */
+
+#ifndef MANAGED_QUERY_H
+#define MANAGED_QUERY_H
+
+#include <stdexcept>  // for windows: error C2039: 'runtime_error': is not a member of 'std'
+#include <unordered_set>
+
+#include <tiledb/tiledb>
+
+#include "tiledbsoma/array_buffers.h"
+#include "tiledbsoma/column_buffer.h"
+#include "tiledbsoma/common.h"
+
+namespace tiledbsoma {
+
+using namespace tiledb;
+
+class ManagedQuery {
+   public:
+    //===================================================================
+    //= public non-static
+    //===================================================================
+
+    /**
+     * @brief Construct a new Managed Query object
+     *
+     * @param array TileDB array
+     * @param name Name of the array
+     */
+    ManagedQuery(
+        std::shared_ptr<Array> array, std::string_view name = "unnamed");
+
+    ManagedQuery() = delete;
+    ManagedQuery(const ManagedQuery&) = delete;
+    ManagedQuery(ManagedQuery&&) = default;
+    ~ManagedQuery() = default;
+
+    /**
+     * @brief Select columns names to query (dim and attr). If the
+     * `if_not_empty` parameter is `true`, the column will be selected iff the
+     * list of selected columns is empty. This prevents a `select_columns` call
+     * from changing an empty list (all columns) to a subset of columns.
+     *
+     * @param names Vector of column names
+     * @param if_not_empty Prevent changing an "empty" selection of all columns
+     */
+    void select_columns(
+        const std::vector<std::string>& names, bool if_not_empty = false);
+
+    /**
+     * @brief Select dimension ranges to query.
+     *
+     * @tparam T Dimension type
+     * @param dim Dimension name
+     * @param ranges Vector of dimension ranges
+     */
+    template <typename T>
+    void select_ranges(
+        const std::string& dim, const std::vector<std::pair<T, T>>& ranges) {
+        for (auto& [start, stop] : ranges) {
+            subarray_->add_range(dim, start, stop);
+        }
+    }
+
+    /**
+     * @brief Select dimension points to query.
+     *
+     * @tparam T Dimension type
+     * @param dim Dimension name
+     * @param points Vector of dimension points
+     */
+    template <typename T>
+    void select_points(const std::string& dim, const std::vector<T>& points) {
+        for (auto& point : points) {
+            subarray_->add_range(dim, point, point);
+        }
+    }
+
+    /**
+     * @brief Select dimension points to query.
+     *
+     * @tparam T Dimension type
+     * @param dim Dimension name
+     * @param points Vector of dimension points
+     */
+    template <typename T>
+    void select_points(const std::string& dim, const std::span<T> points) {
+        for (auto& point : points) {
+            subarray_->add_range(dim, point, point);
+        }
+    }
+
+    /**
+     * @brief Select dimension point to query.
+     *
+     * @tparam T Dimension type
+     * @param dim Dimension name
+     * @param point Dimension points
+     */
+    template <typename T>
+    void select_point(const std::string& dim, const T& point) {
+        subarray_->add_range(dim, point, point);
+    }
+
+    /**
+     * @brief Set a query condition.
+     *
+     * @param qc A TileDB QueryCondition
+     */
+    void set_condition(const QueryCondition& qc) {
+        query_->set_condition(qc);
+    }
+
+    /**
+     * @brief Submit the query.
+     *
+     */
+    void submit();
+
+    /**
+     * @brief Return the query status.
+     *
+     * @return Query::Status Query status
+     */
+    Query::Status status() {
+        return query_->query_status();
+    }
+
+    /**
+     * @brief Check if the query is complete.
+     *
+     * @return true Query status is COMPLETE
+     */
+    bool is_complete() {
+        return query_->query_status() == Query::Status::COMPLETE;
+    }
+
+    /**
+     * @brief Return true if the query result buffers hold all results from the
+     * query. The return value is false if the query was incomplete.
+     *
+     * @return true The buffers hold all results from the query.
+     */
+    bool results_complete() {
+        return is_complete() && results_complete_;
+    }
+
+    /**
+     * @brief Returns the total number of cells read so far, including any
+     * previous incomplete queries.
+     *
+     * @return size_t Total number of cells read
+     */
+    size_t total_num_cells() {
+        return total_num_cells_;
+    }
+
+    /**
+     * @brief Return a view of data in column `name`.
+     *
+     * @tparam T Data type
+     * @param name Column name
+     * @return std::span<T> Data view
+     */
+    template <typename T>
+    std::span<T> data(const std::string& name) {
+        check_column_name(name);
+        return buffers_->at(name)->data<T>();
+    }
+
+    /**
+     * @brief Return a vector of strings from the column `name`.
+     *
+     * @param name Column name
+     * @return std::vector<std::string> Strings
+     */
+    std::vector<std::string> strings(const std::string& name) {
+        check_column_name(name);
+        return buffers_->at(name)->strings();
+    }
+
+    /**
+     * @brief Return a string_view of the string at the provided cell index from
+     * column `name`.
+     *
+     * @param name Column name
+     * @param index Cell index
+     * @return std::string_view String view
+     */
+    std::string_view string_view(const std::string& name, uint64_t index) {
+        check_column_name(name);
+        return buffers_->at(name)->string_view(index);
+    }
+
+    /**
+     * @brief Return results from the query.
+     *
+     * @return std::shared_ptr<ArrayBuffers>
+     */
+    std::shared_ptr<ArrayBuffers> results();
+
+   private:
+    //===================================================================
+    //= private non-static
+    //===================================================================
+
+    /**
+     * @brief Check if column name is contained in the query results.
+     *
+     * @param name Column name
+     */
+    void check_column_name(const std::string& name) {
+        if (!buffers_->contains(name)) {
+            throw TileDBSOMAError(fmt::format(
+                "[ManagedQuery] Column '{}' is not available in the query "
+                "results.",
+                name));
+        }
+    }
+
+    // TileDB array being queried.
+    std::shared_ptr<Array> array_;
+
+    // Name displayed in log messages
+    std::string name_;
+
+    // Array schema
+    ArraySchema schema_;
+
+    // TileDB query being managed.
+    std::unique_ptr<Query> query_;
+
+    // TileDB subarray containing the ranges for slicing.
+    std::unique_ptr<Subarray> subarray_;
+
+    // Set of column names to read (dim and attr). If empty, query all columns.
+    std::vector<std::string> columns_;
+
+    // Results in the buffers are complete (the query was never incomplete)
+    bool results_complete_ = true;
+
+    // Total number of cells read by the query
+    size_t total_num_cells_ = 0;
+
+    // A collection of ColumnBuffers attached to the query
+    std::shared_ptr<ArrayBuffers> buffers_;
+
+    // True if the query has been submitted and the results have not been read
+    bool query_submitted_ = false;
+};
+
+};  // namespace tiledbsoma
+
+#endif
---
+++ apis/r/src/pyapi/arrow_adapter.h
@@ -0,0 +1,169 @@
+#ifndef ARROW_ADAPTER_H
+#define ARROW_ADAPTER_H
+
+#include "carrow.h"
+#include "tiledbsoma/tiledbsoma"
+
+// https://arrow.apache.org/docs/format/CDataInterface.html
+// https://arrow.apache.org/docs/format/Columnar.html#buffer-listing-for-each-layout
+// https://arrow.apache.org/docs/format/CDataInterface.html#exporting-a-simple-int32-array
+
+namespace tiledbsoma {
+
+/**
+ * @brief The ArrowBuffer holds a shared pointer to a ColumnBuffer, which
+ * manages the lifetime of a ColumnBuffer used to back an Arrow array.
+ *
+ * The ArrowArray.release callback will delete the ArrowBuffer, and
+ * automatically decrement the use count of the ColumnBuffer's shared pointer.
+ *
+ */
+struct ArrowBuffer {
+    ArrowBuffer(std::shared_ptr<ColumnBuffer> buffer)
+        : buffer_(buffer){};
+
+    std::shared_ptr<ColumnBuffer> buffer_;
+};
+
+class ArrowAdapter {
+   public:
+    static void release_schema(struct ArrowSchema* schema) {
+        schema->release = nullptr;
+        LOG_TRACE("[ArrowAdapter] release_schema");
+    }
+
+    static void release_array(struct ArrowArray* array) {
+        auto arrow_buffer = static_cast<ArrowBuffer*>(array->private_data);
+
+        LOG_TRACE(fmt::format(
+            "[ArrowAdapter] release_array {} use_count={}",
+            arrow_buffer->buffer_->name(),
+            arrow_buffer->buffer_.use_count()));
+
+        // Delete the ArrowBuffer, which was allocated with new.
+        // If the ArrowBuffer.buffer_ shared_ptr is the last reference to the
+        // underlying ColumnBuffer, the ColumnBuffer will be deleted.
+        delete arrow_buffer;
+
+        if (array->buffers != nullptr) {
+            free(array->buffers);
+        }
+        array->release = nullptr;
+    }
+
+    /**
+     * @brief Convert ColumnBuffer to an Arrow array.
+     *
+     * @return auto [Arrow array, Arrow schema]
+     */
+    static auto to_arrow(std::shared_ptr<ColumnBuffer> column) {
+        std::unique_ptr<ArrowSchema> schema = std::make_unique<ArrowSchema>();
+        std::unique_ptr<ArrowArray> array = std::make_unique<ArrowArray>();
+
+        schema->format = to_arrow_format(column->type()).data();  // mandatory
+        schema->name = nullptr;                                   // optional
+        schema->metadata = nullptr;                               // optional
+        schema->flags = 0;                                        // optional
+        schema->n_children = 0;                                   // mandatory
+        schema->children = nullptr;                               // optional
+        schema->dictionary = nullptr;                             // optional
+        schema->release = &release_schema;                        // mandatory
+        schema->private_data = nullptr;                           // optional
+
+        int n_buffers = column->is_var() ? 3 : 2;
+
+        auto arrow_buffer = new ArrowBuffer(column);
+
+        array->length = column->size();             // mandatory
+        array->null_count = 0;                      // mandatory
+        array->offset = 0;                          // mandatory
+        array->n_buffers = n_buffers;               // mandatory
+        array->n_children = 0;                      // mandatory
+        array->buffers = nullptr;                   // mandatory
+        array->children = nullptr;                  // optional
+        array->dictionary = nullptr;                // optional
+        array->release = &release_array;            // mandatory
+        array->private_data = (void*)arrow_buffer;  // mandatory
+
+        LOG_TRACE(fmt::format(
+            "[ArrowAdapter] create array name='{}' use_count={}",
+            column->name(),
+            column.use_count()));
+
+        array->buffers = (const void**)malloc(sizeof(void*) * n_buffers);
+        assert(array->buffers != nullptr);
+        array->buffers[0] = nullptr;  // validity
+        if (n_buffers == 2) {
+            array->buffers[1] = column->data<void*>().data();
+        } else if (n_buffers == 3) {
+            array->buffers[1] = column->offsets().data();
+            array->buffers[2] = column->data<void*>().data();
+        }
+
+        return std::pair(std::move(array), std::move(schema));
+    }
+
+    /**
+     * @brief Get Arrow format string from TileDB datatype.
+     *
+     * @param datatype TileDB datatype.
+     * @return std::string_view Arrow format string.
+     */
+    static std::string_view to_arrow_format(tiledb_datatype_t datatype) {
+        switch (datatype) {
+            case TILEDB_STRING_ASCII:
+            case TILEDB_STRING_UTF8:
+                return "U";  // large because TileDB uses 64bit offsets
+            case TILEDB_CHAR:
+            case TILEDB_BLOB:
+                return "Z";  // large because TileDB uses 64bit offsets
+            case TILEDB_BOOL:
+                return "C";  // TILEDB_BOOL is 8bit but arrow BOOL is 1bit
+            case TILEDB_INT32:
+                return "i";
+            case TILEDB_INT64:
+                return "l";
+            case TILEDB_FLOAT32:
+                return "f";
+            case TILEDB_FLOAT64:
+                return "g";
+            case TILEDB_INT8:
+                return "c";
+            case TILEDB_UINT8:
+                return "C";
+            case TILEDB_INT16:
+                return "s";
+            case TILEDB_UINT16:
+                return "S";
+            case TILEDB_UINT32:
+                return "I";
+            case TILEDB_UINT64:
+                return "L";
+            case TILEDB_TIME_SEC:
+                return "tts";
+            case TILEDB_TIME_MS:
+                return "ttm";
+            case TILEDB_TIME_US:
+                return "ttu";
+            case TILEDB_TIME_NS:
+                return "ttn";
+            case TILEDB_DATETIME_SEC:
+                return "tss:";
+            case TILEDB_DATETIME_MS:
+                return "tsm:";
+            case TILEDB_DATETIME_US:
+                return "tsu:";
+            case TILEDB_DATETIME_NS:
+                return "tsn:";
+            default:
+                break;
+        }
+        throw TileDBSOMAError(fmt::format(
+            "ArrowAdapter: Unsupported TileDB datatype: {} ",
+            tiledb::impl::type_to_str(datatype)));
+    }
+};
+
+};  // namespace tiledbsoma
+
+#endif
