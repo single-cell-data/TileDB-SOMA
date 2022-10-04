9d77200bfb104c73d669bafc7ecd51b54d363565
bkmartinjr <bruce@chanzuckerberg.com>
2022-10-04 09:50:21 -0700

    work around and test SparseCOOTensor issue

--- apis/python/src/tiledbsoma/soma_sparse_nd_array.py
+++ apis/python/src/tiledbsoma/soma_sparse_nd_array.py
@@ -193,10 +193,19 @@ class SOMASparseNdArray(TileDBArray):
                 return_incomplete=True,
             )
             for arrow_tbl in query.df[coords]:
+                """
+                In PyArrow 9.0.0, there is a bug preventing the creation of "empty" (zero element)
+                SparseCOOTensor objects. See https://issues.apache.org/jira/browse/ARROW-17933
+
+                Just stop the iteration when we run out of results. The caller must be
+                prepared to have a StopIteration, rather than an empty tensor, as the result of
+                an empty query.
+                """
                 if arrow_tbl.num_rows == 0:
                     return
 
                 if format == "coo":
+
                     coo_data = arrow_tbl.column("data").to_numpy()
                     coo_coords = np.array(
                         [
@@ -231,9 +240,6 @@ class SOMASparseNdArray(TileDBArray):
                 return_incomplete=True,
             )
             for arrow_tbl in query.df[coords]:
-                if arrow_tbl.num_rows == 0:
-                    return
-
                 yield arrow_tbl
 
     def read_as_pandas(self, coords: SOMASparseNdCoordinates) -> Iterator[pd.DataFrame]:
--- apis/python/tests/test_soma_sparse_nd_array.py
+++ apis/python/tests/test_soma_sparse_nd_array.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from typing import Optional, Tuple, Union
 
 import numpy as np
@@ -304,6 +305,56 @@ def test_soma_sparse_nd_array_read_as_pandas(
     dim_names = [f"__dim_{n}" for n in range(len(shape))]
     assert df.sort_values(by=dim_names, ignore_index=True).equals(
         data.to_pandas().sort_values(by=dim_names, ignore_index=True)
+    )
+
+
+def test_empty_read(tmp_path):
+    """
+    Verify that queries expected to return empty results actually
+    work. There are edge cases around SparseTensors, which are unable
+    to represent empty arrays.
+    """
+    a = soma.SOMASparseNdArray(uri=tmp_path.as_posix())
+    a.create(type=pa.uint16(), shape=(10, 100))
+    assert a.exists()
+
+    #
+    # First, test reads of zero element array
+    #
+
+    # These work as expected
+    coords = (slice(None),)
+    assert sum(len(t) for t in a.read_table(coords)) == 0
+    assert (
+        sum(t.non_zero_length for t in a.read_sparse_tensor(coords, format="coo")) == 0
+    )
+    assert (
+        sum(t.non_zero_length for t in a.read_sparse_tensor(coords, format="csr")) == 0
+    )
+    assert (
+        sum(t.non_zero_length for t in a.read_sparse_tensor(coords, format="csc")) == 0
+    )
+
+    #
+    # Next, test empty queries on non-empty array
+    #
+    a.write_sparse_tensor(
+        pa.SparseCOOTensor.from_scipy(
+            sparse.coo_matrix(([1], ([0], [0])), shape=a.shape)
+        )
+    )
+    assert sum(len(t) for t in a.read_table((slice(None),))) == 1
+
+    coords = (1, 1)  # no element at this coordinate
+    assert sum(len(t) for t in a.read_table(coords)) == 0
+    assert (
+        sum(t.non_zero_length for t in a.read_sparse_tensor(coords, format="coo")) == 0
+    )
+    assert (
+        sum(t.non_zero_length for t in a.read_sparse_tensor(coords, format="csr")) == 0
+    )
+    assert (
+        sum(t.non_zero_length for t in a.read_sparse_tensor(coords, format="csc")) == 0
     )
 
 
