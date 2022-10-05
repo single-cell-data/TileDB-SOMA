5b248283ef1665afece9cf6ebe02d1a914427a53
Dirk Eddelbuettel <edd@debian.org>
2022-10-05 16:04:28 -0500

    generalize to get list of columns in single call

--- apis/r/src/rinterface.cpp
+++ apis/r/src/rinterface.cpp
@@ -65,17 +65,15 @@ bool export_column(const std::string& uri, const std::string& colname,
 // still relies on helper from package 'arch' that we expect to be
 // replaced in due course by package 'nanoarrow' (once released)
 //
+//' @rdname get_table
+//' @export
 // [[Rcpp::export]]
-SEXP export_column_direct(const std::string& uri, const std::string& colname) {
+SEXP export_column_direct(const std::string& uri, const std::vector<std::string>& colnames) {
 
-    // this allocates, and properly wraps as external pointers controlling lifetime
-    SEXP schemaxp = arch_c_allocate_schema();
-    SEXP arrayxp = arch_c_allocate_array_data();
+    tdbs::LOG_INFO(fmt::format("Reading from {}", uri));
 
-    std::map<std::string, std::string> config;
-
-    // Read all values from the obs array (obs is unique_ptr<SOMAReader>)
-    auto obs = tdbs::SOMAReader::open(uri);
+    // Read selected columns from the obs array (obs is unique_ptr<SOMAReader>)
+    auto obs = tdbs::SOMAReader::open(uri, "", {}, colnames);
     obs->submit();
 
     // Getting next batch:  std::optinal<std::shared_ptr<ArrayBuffers>>
@@ -83,24 +81,41 @@ SEXP export_column_direct(const std::string& uri, const std::string& colname) {
     if (!obs->results_complete()) {
         Rcpp::warning("Read of '%s' incomplete", uri);
     }
+    tdbs::LOG_INFO(fmt::format("Read complete with {} obs and {} cols",
+                               obs_data->get()->num_rows(),
+                               obs_data->get()->names().size()));
 
-    // now buf is a shared_ptr to ColumnBuffer
-    auto buf = obs_data->get()->at(colname);
+    auto ncol = obs_data->get()->names().size();
+    Rcpp::List reslist(ncol);
+    reslist.attr("names") = obs_data->get()->names();
 
-    // this is pair of array and schema pointer
-    auto pp = tdbs::ArrowAdapter::to_arrow(buf);
+    for (size_t i=0; i<ncol; i++) {
+        // this allocates, and properly wraps as external pointers controlling lifetime
+        SEXP schemaxp = arch_c_allocate_schema();
+        SEXP arrayxp = arch_c_allocate_array_data();
 
-    memcpy((void*) R_ExternalPtrAddr(schemaxp), pp.second.get(), sizeof(ArrowSchema));
-    memcpy((void*) R_ExternalPtrAddr(arrayxp), pp.first.get(), sizeof(ArrowArray));
+        // now buf is a shared_ptr to ColumnBuffer
+        auto buf = obs_data->get()->at(colnames[i]);
 
-    // in R:
-    //   aa <- arch::arch_array(schema, array, FALSE)    # R/array.R:16
-    // which just passes throught and then creates as S3 class
-    // sp here we just set an S3 object up: a list with a class attribute
-    Rcpp::List res = Rcpp::List::create(Rcpp::Named("schema") = schemaxp,
-                                        Rcpp::Named("array_data") = arrayxp);
-    res.attr("class") = Rcpp::CharacterVector::create("arch_array");
-    // in R:  arch::from_arch_array()
+        tdbs::LOG_INFO(fmt::format("Accessing {} at {}", colnames[i], i));
 
-    return res;
+        // this is pair of array and schema pointer
+        auto pp = tdbs::ArrowAdapter::to_arrow(buf);
+
+        memcpy((void*) R_ExternalPtrAddr(schemaxp), pp.second.get(), sizeof(ArrowSchema));
+        memcpy((void*) R_ExternalPtrAddr(arrayxp), pp.first.get(), sizeof(ArrowArray));
+
+        // in R:
+        //   aa <- arch::arch_array(schema, array, FALSE)    # R/array.R:16
+        // which just passes throught and then creates as S3 class
+        // sp here we just set an S3 object up: a list with a class attribute
+        Rcpp::List res = Rcpp::List::create(Rcpp::Named("schema") = schemaxp,
+                                            Rcpp::Named("array_data") = arrayxp);
+        res.attr("class") = Rcpp::CharacterVector::create("arch_array");
+        // in R:  arch::from_arch_array()
+
+        reslist[i] = res;
+    }
+
+    return reslist;
 }
