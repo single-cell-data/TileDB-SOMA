b9fb71c82a03162417b6d260f2d14b27047d20c5
John Kerl <john.kerl@tiledb.com>
2022-10-13 23:20:50 +0000

    disambiguate bytes/ascii on tiledb schema -> arrow schema

--- apis/python/src/tiledbsoma/util_scipy.py
+++ apis/python/src/tiledbsoma/util_scipy.py
@@ -4,7 +4,7 @@ import scipy.sparse as sp
 
 def csr_from_tiledb_df(df: pd.DataFrame, num_rows: int, num_cols: int) -> sp.csr_matrix:
     """
-    TODO: COMMENT
+    Given a tiledb dataframe, return a ``scipy.sparse.csr_matrx``.
     """
     return sp.csr_matrix(
         (df["soma_data"], (df["soma_dim_0"], df["soma_dim_1"])),
