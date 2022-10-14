9a686be5de3eb92fad45e659202f2aad652edfac
John Kerl <kerl.john.r@gmail.com>
2022-10-14 16:37:57 -0400

    Match SparseNdArray dim/attr names to spec

--- apis/r/tests/testthat/test_SOMASparseNdArray.R
+++ apis/r/tests/testthat/test_SOMASparseNdArray.R
@@ -4,9 +4,9 @@ test_that("SOMASparseNdArray creation", {
   ndarray$create(arrow::int32(), shape = c(10, 10))
 
   expect_equal(tiledb::tiledb_object_type(uri), "ARRAY")
-  expect_equal(ndarray$dimnames(), c("__dim_0", "__dim_1"))
+  expect_equal(ndarray$dimnames(), c("soma_dim_0", "soma_dim_1"))
 
-  expect_equal(ndarray$attrnames(), "data")
+  expect_equal(ndarray$attrnames(), "soma_data")
   expect_equal(tiledb::datatype(ndarray$attributes()$data), "INT32")
 
   mat <- create_sparse_matrix_with_int_dims(10, 10)
