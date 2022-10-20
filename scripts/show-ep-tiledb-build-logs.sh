4f3c866962037ff79d2252179bbe210950ff6d50
John Kerl <kerl.john.r@gmail.com>
2022-10-19 22:58:34 -0400

    debug

--- scripts/show-ep-tiledb-build-logs.sh
+++ scripts/show-ep-tiledb-build-logs.sh
@@ -2,6 +2,11 @@
 
 for x in /home/runner/work/TileDB-SOMA/TileDB-SOMA/build/externals/src/ep_tiledb-stamp/ep_tiledb-build-*.log; do
     echo
-    echo ------------------ $x
+    echo "================================================================ BEGIN EP BUILD LOG"
+    echo $x
+    echo "----------------------------------------------------------------"
     cat $x
+    echo "================================================================ END   EP BUILD LOG"
 done
+
+false # this is invokved as an error-handler so pass error status along
