aa34aba691498cec3e5e2beae5950b3c6ccd3d49
John Kerl <kerl.john.r@gmail.com>
2022-10-28 11:13:03 -0400

    temp

--- apis/python/src/tiledbsoma/sparse_nd_array.py
+++ apis/python/src/tiledbsoma/sparse_nd_array.py
@@ -187,25 +187,25 @@ class SparseNdArray(TileDBArray):
         with self._tiledb_open("r") as A:
             shape = list(A.shape)
 
-        for i in range(len(coords)):
-            coord = coords[i]
-            if coord is None:
-                pass
-            elif isinstance(coord, int):
-                shape[i] = 1
-            elif isinstance(coord, list):
-                shape[i] = len(coord)
-            elif isinstance(coord, pa.ChunkedArray):
-                shape[i] = len(coord)
-            elif isinstance(coord, pa.Array):
-                shape[i] = len(coord)
-            elif isinstance(coord, slice):
-                lo_hi = util.slice_to_range(coord)
-                if lo_hi is not None:
-                    lo, hi = lo_hi
-                    shape[i] = hi - lo + 1
-            else:
-                raise SOMAError(f"coord type {type(coord)} at slot {i} unhandled")
+#        for i in range(len(coords)):
+#            coord = coords[i]
+#            if coord is None:
+#                pass
+#            elif isinstance(coord, int):
+#                shape[i] = 1
+#            elif isinstance(coord, list):
+#                shape[i] = len(coord)
+#            elif isinstance(coord, pa.ChunkedArray):
+#                shape[i] = len(coord)
+#            elif isinstance(coord, pa.Array):
+#                shape[i] = len(coord)
+#            elif isinstance(coord, slice):
+#                lo_hi = util.slice_to_range(coord)
+#                if lo_hi is not None:
+#                    lo, hi = lo_hi
+#                    shape[i] = hi - lo + 1
+#            else:
+#                raise SOMAError(f"coord type {type(coord)} at slot {i} unhandled")
 
         for arrow_tbl in self.read_table(coords):
             """
@@ -289,7 +289,7 @@ class SparseNdArray(TileDBArray):
                 elif isinstance(coord, slice):
                     lo_hi = util.slice_to_range(coord)
                     if lo_hi is not None:
-                        sr.set_dim_ranges(dim_name, lo_hi)
+                        sr.set_dim_ranges(dim_name, [lo_hi])
                 else:
                     raise SOMAError(f"coord type {type(coord)} at slot {i} unhandled")
 
