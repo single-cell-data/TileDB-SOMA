48e1afb486d10931332c4979143bd81f0a6edfc8
John Kerl <kerl.john.r@gmail.com>
2022-11-08 19:40:54 -0500

    xfail a case which depends on an open issue

--- apis/python/tests/test_indexed_dataframe.py
+++ apis/python/tests/test_indexed_dataframe.py
@@ -385,12 +385,16 @@ def make_multiply_indexed_dataframe(tmp_path, index_column_names: List[str]):
             "A": [],
             "throws": None,
         },
-        {
-            "index_column_names": ["index1"],
-            "ids": [[]],
-            "A": [],
-            "throws": None,
-        },
+        pytest.param(
+            # TODO: use after https://github.com/single-cell-data/TileDB-SOMA/issues/484 is resolved
+            {
+                "index_column_names": ["index1"],
+                "ids": [[]],
+                "A": [],
+                "throws": None,
+            },
+            marks=pytest.mark.xfail,
+        ),
         # 1D: indexing slot is tuple
         {
             "index_column_names": ["index1"],
