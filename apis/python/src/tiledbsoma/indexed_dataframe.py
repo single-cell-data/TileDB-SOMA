be365f0bc85ee07398c4ebb64268c1a14ed7ad51
Bruce Martin <bruce@chanzuckerberg.com>
2022-11-10 13:06:14 -0800

    add temporary configuration to SOMAReader instances (#527)

--- apis/python/src/tiledbsoma/indexed_dataframe.py
+++ apis/python/src/tiledbsoma/indexed_dataframe.py
@@ -212,6 +212,7 @@ class IndexedDataFrame(TileDBArray):
                 schema=A.schema,  # query_condition needs this
                 column_names=column_names,
                 query_condition=query_condition,
+                platform_config={} if self._ctx is None else self._ctx.config().dict(),
             )
 
             if ids is not None:
