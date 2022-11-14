d69fd52f0b791f327be1bc1750d49d5981b9b64e
Aaron Wolen <aaron@wolen.com>
2022-11-14 13:20:11 -0600

    Test helper for comparing reordered dfs and mats

--- apis/r/tests/testthat/helpers.R
+++ apis/r/tests/testthat/helpers.R
@@ -16,3 +16,18 @@ with_allocation_size_preference <- function(value, .local_envir = parent.frame()
   )
   tiledb::set_allocation_size_preference(value)
 }
+
+
+#' Compare data.frames or matrices after sorting dimensions
+#' Useful for comparing objects were numerically or lexicographically sorted
+#' after being reconstituted from TileDB.
+#' @noRd
+expect_equal_after_ordering_dims <- function(object, expected, ...) {
+  stopifnot(
+    is.data.frame(object) || is_matrix(object),
+    is.data.frame(expected) || is_matrix(expected)
+  )
+
+  object <- object[rownames(expected), colnames(expected)]
+  testthat::expect_equal(object, expected, ...)
+}
