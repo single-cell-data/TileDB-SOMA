91ccfb0160628f94c4d5b11193a6e320857b6147
John Kerl <john.kerl@tiledb.com>
2022-11-16 15:55:39 +0000

    Unit testing for schema-only mode

--- apis/python/tests/test_basic_anndata_io.py
+++ apis/python/tests/test_basic_anndata_io.py
@@ -25,131 +25,175 @@ def adata(h5ad_file):
     return anndata.read_h5ad(h5ad_file)
 
 
-def test_import_anndata(adata):
+@pytest.mark.parametrize(
+    "schema_onlies",
+    [
+        [False],  # Standard ingest: normal use-case
+        [True],  # User only creates schema
+        [False, True],  # User creates schema, then writes data
+        [
+            False,
+            True,
+            True,
+        ],  # User creates schema, then writes and re-writes the same data
+        [True, True],  # User writes and re-writes the same data
+    ],
+)
+def test_import_anndata(adata, schema_onlies):
 
-    # Set up anndata input path and tiledb-group output path
-    tempdir = tempfile.TemporaryDirectory()
-    output_path = tempdir.name
+    for schema_only in schema_onlies:
 
-    orig = adata
+        # Set up anndata input path and tiledb-group output path
+        tempdir = tempfile.TemporaryDirectory()
+        output_path = tempdir.name
 
-    # Ingest
-    soma = tiledbsoma.SOMA(output_path)
-    tiledbsoma.io.from_anndata(soma, orig)
+        orig = adata
 
-    # Structure:
-    #   X/data
-    #   obs
-    #   var
-    #   obsm/X_pca
-    #   obsm/X_tsne
-    #   obsm/X_umap
-    #   obsm/X_draw_graph_fr
-    #   varm/PCs
-    #   obsp/distances
-    #   obsp/connectivities
-    #   raw/X/data
-    #   raw/var
-    #   raw/varm/PCs
+        # Ingest
+        soma = tiledbsoma.SOMA(output_path)
+        tiledbsoma.io.from_anndata(soma, orig, schema_only=schema_only)
 
-    with tiledb.Group(output_path) as G:
-        assert G.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY] == "SOMA"
+        # Structure:
+        #   X/data
+        #   obs
+        #   var
+        #   obsm/X_pca
+        #   obsm/X_tsne
+        #   obsm/X_umap
+        #   obsm/X_draw_graph_fr
+        #   varm/PCs
+        #   obsp/distances
+        #   obsp/connectivities
+        #   raw/X/data
+        #   raw/var
+        #   raw/varm/PCs
 
-    # Check X/data (dense)
-    with tiledb.open(os.path.join(output_path, "X", "data")) as A:
-        df = A[:]
-        keys = list(df.keys())
-        assert keys == ["value", "obs_id", "var_id"]
-        assert A.ndim == 2
-        assert A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY] == "AssayMatrix"
-    # Convenience accessors
-    assert soma.X["data"].shape() == soma.X.data.shape()
+        with tiledb.Group(output_path) as G:
+            assert G.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY] == "SOMA"
 
-    # Check X/raw (sparse)
-    with tiledb.open(os.path.join(output_path, "raw", "X", "data")) as A:
-        df = A.df[:]
-        assert df.columns.to_list() == ["obs_id", "var_id", "value"]
-        # verify sparsity of raw data
-        assert df.shape[0] == orig.raw.X.nnz
-        assert A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY] == "AssayMatrix"
-
-    # Check obs
-    with tiledb.open(os.path.join(output_path, "obs")) as A:
-        df = A.df[:]
-        assert df.columns.to_list() == orig.obs_keys()
-        assert (
-            A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
-            == "AnnotationDataFrame"
-        )
-    assert sorted(soma.obs.ids()) == sorted(list(orig.obs_names))
-    # Convenience accessors
-    assert soma.obs_keys() == soma.obs_names
-    assert soma.obs_names == soma.obs.ids()
-    assert soma.n_obs == len(soma.obs.ids())
-
-    # Check var
-    with tiledb.open(os.path.join(output_path, "var")) as A:
-        df = A.df[:]
-        assert df.columns.to_list() == orig.var_keys()
-        assert (
-            A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
-            == "AnnotationDataFrame"
-        )
-    assert sorted(soma.var.ids()) == sorted(list(orig.var_names))
-    # Convenience accessors
-    assert soma.var_keys() == soma.var_names
-    assert soma.var_names == soma.var.ids()
-    assert soma.n_var == len(soma.var.ids())
-
-    # Check some annotation matrices
-    # Note: pbmc3k_processed doesn't have varp.
-    assert sorted(soma.obsm.keys()) == sorted(orig.obsm.keys())
-    for key in orig.obsm_keys():
-        with tiledb.open(os.path.join(output_path, "obsm", key)) as A:
-            df = A.df[:]
-            assert df.shape[0] == orig.obsm[key].shape[0]
-            assert soma.obsm[key].shape() == orig.obsm[key].shape
-            assert (
-                A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
-                == "AnnotationMatrix"
-            )
-    # Convenience accessors: soma.obsm.X_pca <-> soma.obsm['X_pca']
-    for key in soma.obsm.keys():
-        assert getattr(soma.obsm, key).shape() == soma.obsm[key].shape()
-
-    assert sorted(soma.varm.keys()) == sorted(orig.varm.keys())
-    for key in orig.varm_keys():
-        with tiledb.open(os.path.join(output_path, "varm", key)) as A:
-            df = A.df[:]
-            assert df.shape[0] == orig.varm[key].shape[0]
-            assert soma.varm[key].shape() == orig.varm[key].shape
-            assert (
-                A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
-                == "AnnotationMatrix"
-            )
-    # Convenience accessors:
-    for key in soma.varm.keys():
-        assert getattr(soma.varm, key).shape() == soma.varm[key].shape()
-
-    assert sorted(soma.obsp.keys()) == sorted(orig.obsp.keys())
-    for key in list(orig.obsp.keys()):
-        with tiledb.open(os.path.join(output_path, "obsp", key)) as A:
-            df = A.df[:]
-            assert df.columns.to_list() == ["obs_id_i", "obs_id_j", "value"]
-            assert df.shape[0] == orig.obsp[key].nnz
-            # https://github.com/single-cell-data/TileDB-SingleCell/issues/125
-            # At present (without that PR's suggested enhancement) the best we
-            # can get is the NNZ x attrs shape -- note that there are two
-            # dims and one attr so the shape is nnz x 1.
-            shape = soma.obsp[key].df().shape
-            assert shape[0] == orig.obsp[key].nnz
-            assert shape[1] == 1
+        # Check X/data (dense)
+        with tiledb.open(os.path.join(output_path, "X", "data")) as A:
+            df = A[:]
+            keys = list(df.keys())
+            assert keys == ["value", "obs_id", "var_id"]
+            assert A.ndim == 2
             assert (
                 A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY] == "AssayMatrix"
             )
-    # Convenience accessors:
-    for key in soma.obsp.keys():
-        assert getattr(soma.obsp, key).shape() == soma.obsp[key].shape()
+        # Convenience accessors
+        assert soma.X["data"].shape() == soma.X.data.shape()
+
+        # Check X/raw (sparse)
+        with tiledb.open(os.path.join(output_path, "raw", "X", "data")) as A:
+            df = A.df[:]
+            assert df.columns.to_list() == ["obs_id", "var_id", "value"]
+            # verify sparsity of raw data
+            if schema_only:
+                assert df.shape[0] == 0
+            else:
+                assert df.shape[0] == orig.raw.X.nnz
+            assert (
+                A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY] == "AssayMatrix"
+            )
+
+        # Check obs
+        with tiledb.open(os.path.join(output_path, "obs")) as A:
+            df = A.df[:]
+            assert df.columns.to_list() == orig.obs_keys()
+            assert (
+                A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
+                == "AnnotationDataFrame"
+            )
+        if schema_only:
+            assert sorted(soma.obs.ids()) == []
+        else:
+            assert sorted(soma.obs.ids()) == sorted(list(orig.obs_names))
+        # Convenience accessors
+        assert soma.obs_keys() == soma.obs_names
+        assert soma.obs_names == soma.obs.ids()
+        assert soma.n_obs == len(soma.obs.ids())
+
+        # Check var
+        with tiledb.open(os.path.join(output_path, "var")) as A:
+            df = A.df[:]
+            assert df.columns.to_list() == orig.var_keys()
+            assert (
+                A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
+                == "AnnotationDataFrame"
+            )
+        if schema_only:
+            assert sorted(soma.var.ids()) == []
+        else:
+            assert sorted(soma.var.ids()) == sorted(list(orig.var_names))
+        # Convenience accessors
+        assert soma.var_keys() == soma.var_names
+        assert soma.var_names == soma.var.ids()
+        assert soma.n_var == len(soma.var.ids())
+
+        # Check some annotation matrices
+        # Note: pbmc3k_processed doesn't have varp.
+        assert sorted(soma.obsm.keys()) == sorted(orig.obsm.keys())
+        for key in orig.obsm_keys():
+            with tiledb.open(os.path.join(output_path, "obsm", key)) as A:
+                df = A.df[:]
+                if schema_only:
+                    assert df.shape[0] == 0
+                    assert soma.obsm[key].shape() == (0, orig.obsm[key].shape[1])
+                else:
+                    assert df.shape[0] == orig.obsm[key].shape[0]
+                    assert soma.obsm[key].shape() == orig.obsm[key].shape
+                assert (
+                    A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
+                    == "AnnotationMatrix"
+                )
+        # Convenience accessors: soma.obsm.X_pca <-> soma.obsm['X_pca']
+        for key in soma.obsm.keys():
+            assert getattr(soma.obsm, key).shape() == soma.obsm[key].shape()
+
+        assert sorted(soma.varm.keys()) == sorted(orig.varm.keys())
+        for key in orig.varm_keys():
+            with tiledb.open(os.path.join(output_path, "varm", key)) as A:
+                df = A.df[:]
+                if schema_only:
+                    assert df.shape[0] == 0
+                    assert soma.varm[key].shape() == (0, orig.varm[key].shape[1])
+                else:
+                    assert df.shape[0] == orig.varm[key].shape[0]
+                    assert soma.varm[key].shape() == orig.varm[key].shape
+                assert (
+                    A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
+                    == "AnnotationMatrix"
+                )
+        # Convenience accessors:
+        for key in soma.varm.keys():
+            assert getattr(soma.varm, key).shape() == soma.varm[key].shape()
+
+        assert sorted(soma.obsp.keys()) == sorted(orig.obsp.keys())
+        for key in list(orig.obsp.keys()):
+            with tiledb.open(os.path.join(output_path, "obsp", key)) as A:
+                df = A.df[:]
+                assert df.columns.to_list() == ["obs_id_i", "obs_id_j", "value"]
+                if schema_only:
+                    assert df.shape[0] == 0
+                else:
+                    assert df.shape[0] == orig.obsp[key].nnz
+                # https://github.com/single-cell-data/TileDB-SingleCell/issues/125
+                # At present (without that PR's suggested enhancement) the best we
+                # can get is the NNZ x attrs shape -- note that there are two
+                # dims and one attr so the shape is nnz x 1.
+                shape = soma.obsp[key].df().shape
+                if schema_only:
+                    assert shape[0] == 0
+                else:
+                    assert shape[0] == orig.obsp[key].nnz
+                assert shape[1] == 1
+                assert (
+                    A.meta[tiledbsoma.util.SOMA_OBJECT_TYPE_METADATA_KEY]
+                    == "AssayMatrix"
+                )
+        # Convenience accessors:
+        for key in soma.obsp.keys():
+            assert getattr(soma.obsp, key).shape() == soma.obsp[key].shape()
 
     tempdir.cleanup()
 
