ab549172c5502cd2eb055ca3bba2a5371dde877f
Paul Fisher <paul.fisher@tiledb.com>
2022-11-21 12:24:53 -0500

    Flesh out tests of platform_config.

--- apis/python/tests/test_platform_config.py
+++ apis/python/tests/test_platform_config.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import anndata
 import pytest
+import tiledb
 
 import tiledbsoma
 import tiledbsoma.io
@@ -26,25 +27,37 @@ def adata(h5ad_file):
 def test_platform_config(adata):
 
     # Set up anndata input path and tiledb-group output path
-    tempdir = tempfile.TemporaryDirectory()
-    output_path = tempdir.name
+    with tempfile.TemporaryDirectory() as output_path:
+        # Ingest
+        exp = tiledbsoma.Experiment(output_path)
+        tiledbsoma.io.from_anndata(
+            exp,
+            adata,
+            "mRNA",
+            platform_config={
+                "tiledb": {
+                    "create": {
+                        "capacity": 8888,
+                        "offsets_filters": ["RleFilter", "NoOpFilter"],
+                        "dims": {
+                            "soma_dim_0": {"tile": 6},
+                            "soma_dim_1": {"filters": []},
+                        },
+                        "attrs": {"soma_data": {"filters": ["NoOpFilter"]}},
+                        "cell_order": "row-major",
+                        "tile_order": "col-major",
+                    },
+                },
+            },
+        )
 
-    # platform_config = {"tiledb": TileDBPlatformConfig(X_capacity=8888))
-    # platform_config = {"tiledb": {"X_capacity":8888}}
-    # platform_config = {"tiledb": {"capacity":8888}}
-    # Hi Paul! Two notes:
-    # * In `main-old` we had `X_capacity` separate from capacities for every array. Might be worth
-    #   doing here in `main`.
-    # * The above three commented-out attempts didn't work for me until I read the code.
-    #   This is PEBKAC for sure, however, we might do a bit more aggressive shape-checking
-    #   to warn others users, who may be as careless as myself, earlier than later.
-    platform_config = {"tiledb": {"create": {"capacity": 8888}}}
-
-    # Ingest
-    exp = tiledbsoma.Experiment(output_path)
-    tiledbsoma.io.from_anndata(exp, adata, "mRNA", platform_config=platform_config)
-
-    with exp.ms["mRNA"].X["data"]._tiledb_open() as X:
-        assert X.schema.capacity == 8888
-
-    # soma.TileDBPlatformConfig(obs_extent=999, X_capacity=8888)
+        with exp.ms["mRNA"].X["data"]._tiledb_open() as arr:
+            sch = arr.schema
+            assert sch.capacity == 8888
+            assert sch.cell_order == "row-major"
+            assert sch.tile_order == "col-major"
+            assert sch.offsets_filters == [tiledb.RleFilter(), tiledb.NoOpFilter()]
+            assert arr.attr("soma_data").filters == [tiledb.NoOpFilter()]
+            assert arr.dim("soma_dim_0").tile == 6
+            assert arr.dim("soma_dim_1").filters == []
+            print(sch)
