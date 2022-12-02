f24fa0f511082bf7814fca6edde1253cea1c7ebd
John Kerl <kerl.john.r@gmail.com>
2022-12-02 11:43:26 -0500

    Allow soma.query with X/data not in X_layer_names (#567)

--- apis/python/src/tiledbsoma/annotation_dataframe.py
+++ apis/python/src/tiledbsoma/annotation_dataframe.py
@@ -414,21 +414,23 @@ class AnnotationDataFrame(TileDBArray):
                 # Force ASCII storage if string, in order to make obs/var columns queryable.
                 column_types[column_name] = "ascii"
 
-        tiledb.from_pandas(
-            uri=self.uri,
-            dataframe=dataframe,
-            name=self.name,
-            sparse=True,
-            allows_duplicates=False,
-            offsets_filters=offsets_filters,
-            attr_filters=attr_filters,
-            dim_filters=dim_filters,
-            capacity=100000,
-            tile=extent,
-            column_types=column_types,
-            ctx=self._ctx,
-            mode=mode,
-        )
+        # XXX TEMP
+        with tiledb.scope_ctx(self._ctx):
+            tiledb.from_pandas(
+                uri=self.uri,
+                dataframe=dataframe,
+                name=self.name,
+                sparse=True,
+                allows_duplicates=False,
+                offsets_filters=offsets_filters,
+                attr_filters=attr_filters,
+                dim_filters=dim_filters,
+                capacity=100000,
+                tile=extent,
+                column_types=column_types,
+                ctx=self._ctx,
+                mode=mode,
+            )
 
         self._set_object_type_metadata()
 
