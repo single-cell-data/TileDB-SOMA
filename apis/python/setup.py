1392e90e57cdf35889bdb1010feb26cd984d93f7
Mike Lin <dna@mlin.net>
2022-12-11 23:55:01 -1000

    iterate on py-packaging workflow

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -47,10 +47,7 @@ def find_or_build(setuptools_cmd):
 
     # Call the build script if the install library directory does not exist
     if not os.path.exists(lib_dir):
-        bld_env = dict(os.environ)
-        bld_env["PYTHON_EXECUTABLE"] = sys.executable
-        bld_env["PYTHON_EXEC_PREFIX"] = sys.exec_prefix
-        subprocess.run("bash bld", cwd=scripts_dir, shell=True, env=bld_env)
+        subprocess.run("bash bld", cwd=scripts_dir, shell=True)
 
     # Copy native libs into the package dir so they can be found by package_data
     package_data = []
