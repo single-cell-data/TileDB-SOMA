c189718dc155ac34a7ececcd4976e0b1e3fe798f
John Kerl <kerl.john.r@gmail.com>
2023-01-03 14:41:39 -0500

    [python] Add some docstrings (#635)
    * Add some docstrings
    
    * code-review feedback

--- apis/python/src/tiledbsoma/tiledb_object.py
+++ apis/python/src/tiledbsoma/tiledb_object.py
@@ -99,10 +99,16 @@ class TileDBObject(ABC):
 
     @property
     def uri(self) -> str:
+        """
+        Accessor for the object's storage URI
+        """
         return self._uri
 
     @abstractproperty
     def soma_type(self) -> str:
+        """
+        Returns the SOMA object type, e.g. "SOMADataFrame".
+        """
         ...
 
     def exists(self) -> bool:
