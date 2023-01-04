62ab9191ae1adc0e97e5647a45f2505d6b636ef7
bkmartinjr <bruce@chanzuckerberg.com>
2023-01-04 18:32:46 +0000

    add comments clarifying confusing naming

--- apis/python/src/tiledbsoma/experiment_query/anndata.py
+++ apis/python/src/tiledbsoma/experiment_query/anndata.py
@@ -7,7 +7,18 @@ import scipy.sparse as sparse
 from .eq_types import ExperimentQueryReadArrowResult
 
 
-def arrow_to_scipy_csr(X: pa.Table, shape: Tuple[int, int]) -> sparse.csr_matrix:
+def _arrow_to_scipy_csr(X: pa.Table, shape: Tuple[int, int]) -> sparse.csr_matrix:
+    """
+    Private utility which converts a table repesentation of X to a CSR matrix.
+
+    IMPORTANT: by convention, assumes that the data is positionally indexed (hence
+    the use of _dim_{n} rather than soma_dim{n}).
+
+    See query.py::_rewrite_X_for_positional_indexing for more info.
+    """
+    assert "_dim_0" in X.column_names, "X must be positionally indexed"
+    assert "_dim_1" in X.column_names, "X must be positionally indexed"
+
     return sparse.csr_matrix(
         (X["soma_data"].to_numpy(), (X["_dim_0"].to_numpy(), X["_dim_1"].to_numpy())),
         shape=shape,
@@ -28,12 +39,12 @@ def make_anndata(query_result: ExperimentQueryReadArrowResult) -> anndata.AnnDat
 
     X = query_result.get("X", None)
     if X is not None:
-        X = arrow_to_scipy_csr(X, shape)
+        X = _arrow_to_scipy_csr(X, shape)
 
     X_layers = query_result.get("X_layers", {})
     layers: Dict[str, sparse.csr_matrix] = {}
     for X_layer_name, X_layer_table in X_layers.items():
-        layers[X_layer_name] = arrow_to_scipy_csr(X_layer_table, shape)
+        layers[X_layer_name] = _arrow_to_scipy_csr(X_layer_table, shape)
 
     return anndata.AnnData(
         X=X, obs=obs, var=var, layers=(layers if len(layers) else None)
