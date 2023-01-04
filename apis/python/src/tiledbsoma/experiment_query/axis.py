e1ca5a0764dd916f21cb60aa75933e32894bc960
bkmartinjr <bruce@chanzuckerberg.com>
2023-01-04 01:10:07 +0000

    Add AxisQuery support for using coords and value-filter simultaneously

--- apis/python/src/tiledbsoma/experiment_query/axis.py
+++ apis/python/src/tiledbsoma/experiment_query/axis.py
@@ -25,16 +25,31 @@ class AxisQuery:
     Define a single-axis dataframe query based upon either a value filter predicate or coordinates
     [lifecycle: experimental].
 
-    Can have value:
-    * None - no query, ie, all data
-    * Coordinates - a set of coordinates on the axis dataframe index (or soma_rowids if a dense dataframe)
-    * A SOMA `value_filter` across columns in the axis dataframe
+    Per dimension, can have value of:
+    * None - all data
+    * Coordinates - a set of coordinates on the axis dataframe index (or soma_rowids if a dense dataframe),
+      expressed as an int, a slice or a NumPy "array like" of ints.
+    * A SOMA `value_filter` across columns in the axis dataframe, expressed as string
+    * Or, a combination of coordinates and value filter.
 
-    Examples:
+    Parameters
+    ----------
+    coords : Optional[tuple]
+        Query (slice) by dimension. Tuple must have lenth less than or equal to the number
+        of dimensions. For each dimension, coordinate is an ``int``, ``slice`` or NumPy
+        ``array-like`` of ints.
+    value_filter : Optional[str]
+        A string specifying a SOMA value_filter.
+
+    Examples
+    --------
     ```
         AxisQuery()
-        AxisQuery(coords=[0,1,2])
+        AxisQuery(coords=(slice(1,10),))  # 1D, slice
+        AxisQuery(coords=([0,1,2]))  # 1D, point indexing using array-like
+        AxisQuery(coords=(slice(None), numpy.array([0,88,1001])))  # 2D
         AxisQuery(value_filter="tissue == 'lung'")
+        AxisQuery(coords=(slice(1,None),), value_filter="tissue == 'lung'")
     ```
     """
 
@@ -42,21 +57,16 @@ class AxisQuery:
     coords: Optional[AxisCoordinates] = None
 
     def __post_init__(self) -> None:
-        # TODO: Error class - should not be throwing generic Exceptions
-        if (self.value_filter is None) and (self.coords is None):
-            self.coords = (slice(None),)
-        if not (self.value_filter is None) != (self.coords is None):
-            raise Exception(
-                "FilterSpec - value_filter or coords may be specified, but not both."
-            )
-
         if self.value_filter is not None:
-            # If a a value_filter, default to all coords
+            if not isinstance(self.value_filter, str):
+                raise TypeError("AxisQuery - value_filter must be a str type")
+
+        if self.coords is None:
             self.coords = (slice(None),)
         else:
             if not isinstance(self.coords, tuple):
-                raise Exception(
-                    "FilterSpec - coords must be tuple of int, slice or numpy.array_like"
+                raise TypeError(
+                    "AxisQuery - coords must be tuple of int, slice or numpy.array_like"
                 )
             coords = []
             for c in self.coords:
@@ -65,7 +75,3 @@ class AxisQuery:
                 else:
                     coords.append(pa.array(np.array(c, dtype=np.int64)))
             self.coords = tuple(coords)
-
-    def is_value_filter(self) -> bool:
-        """Return True if this is a value filter, else False if coordinates"""
-        return self.value_filter is not None
