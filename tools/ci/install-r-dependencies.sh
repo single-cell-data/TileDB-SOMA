785ede5d085ad36b66216d8c0b146c4a01c116f3
John Kerl <kerl.john.r@gmail.com>
2023-01-07 19:06:15 -0500

    TileDB-R 0.17.1

--- tools/ci/install-r-dependencies.sh
+++ tools/ci/install-r-dependencies.sh
@@ -2,18 +2,7 @@
 set -euo pipefail
 # Context: .github/workflows/*.yaml
 export R_REMOTES_NO_ERRORS_FROM_WARNINGS='true'
-
 Rscript -e 'install.packages("remotes")'
-
-# TODO: Remove this once we have either a TileDB-R 0.17.1 depending on core 2.13.1, or, a TileDB-R
-# 0.18 depending on core 2.14.
-#
-# See also https://github.com/single-cell-data/TileDB-SOMA/issues/654
-#
-# * Enable repository from tiledb-inc
-# * Download and install tiledb in R
-Rscript -e 'options(repos = c(tiledbinc = "https://tiledb-inc.r-universe.dev", CRAN = "https://cloud.r-project.org")); install.packages("tiledb")'
-
 cd apis/r
 Rscript -e 'remotes::install_deps(dependencies = TRUE)'
 cd ../..
