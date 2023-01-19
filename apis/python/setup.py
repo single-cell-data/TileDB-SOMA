04e864dbf5aac232cd761230d5c8fd76a7ca9a35
George Sakkis <george.sakkis@tiledb.com>
2023-01-19 17:10:11 +0200

    Replace flake8 linter with ruff

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -117,7 +117,7 @@ setuptools.setup(
     extras_require={
         "dev": [
             "black",
-            "flake8-bugbear",
+            "ruff",
             "isort",
             "pytest",
             "typeguard",
