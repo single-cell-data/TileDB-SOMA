2dbda8f329780e09a4885cb276facaefb2c837b1
George Sakkis <george.sakkis@tiledb.com>
2023-01-20 16:44:11 +0200

    Add pandas-stubs for mypy and fix uncovered typing issues (#772)
    * Add pandas-stubs for mypy and fix uncovered typing issues
    
    * Rename NDArray to NPNDArray

--- apis/python/src/tiledbsoma/io.py
+++ apis/python/src/tiledbsoma/io.py
@@ -29,10 +29,10 @@ from tiledbsoma.exception import SOMAError
 
 from .constants import SOMA_JOINID
 from .options import SOMATileDBContext, TileDBCreateOptions
-from .types import INGEST_MODES, IngestMode, NDArray, Path
+from .types import INGEST_MODES, IngestMode, NPNDArray, Path
 
 SparseMatrix = Union[sp.csr_matrix, sp.csc_matrix, SparseDataset]
-Matrix = Union[NDArray, SparseMatrix]
+Matrix = Union[NPNDArray, SparseMatrix]
 
 
 # ----------------------------------------------------------------
@@ -380,8 +380,7 @@ def _write_dataframe(
             # This lets us check for already-ingested dataframes, when in resume-ingest mode.
             with soma_df._tiledb_open() as A:
                 storage_ned = A.nonempty_domain()
-            sorted_dim_values = sorted(list(df.index))
-            dim_range = ((sorted_dim_values[0], sorted_dim_values[-1]),)
+            dim_range = ((int(df.index.min()), int(df.index.max())),)
             if _chunk_is_contained_in(dim_range, storage_ned):
                 logging.log_io(
                     f"Skipped {soma_df.uri}",
