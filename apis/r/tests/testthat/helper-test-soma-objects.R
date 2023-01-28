44355e624be859f5ba1402976971d587f5abb889
Aaron Wolen <aaron@wolen.com>
2023-01-28 15:49:10 -0600

    Add test helper to create a populated SOMAExperiment

--- apis/r/tests/testthat/helper-test-soma-objects.R
+++ apis/r/tests/testthat/helper-test-soma-objects.R
@@ -16,16 +16,29 @@ create_and_populate_var <- function(uri, nrows = 10L, seed = 1) {
   sdf
 }
 
-create_and_populate_var <- function(uri) {
+# Create a SOMAExperiment with a single measurement, "RNA"
+create_and_populate_experiment <- function(uri, n_obs, n_var, X_layer_names) {
 
-  tbl <- arrow::arrow_table(
-    soma_joinid = bit64::as.integer64(1L:4L),
-    quux = c("alvin", "simon", "theodore", "dave"),
-    xyzzy = c(12.3, 45.6, 78.9, 10.11)
+  experiment <- SOMAExperiment$new(uri)$create()
+  experiment$obs <- create_and_populate_obs(
+    uri = file.path(uri, "obs"),
+    nrows = n_obs
   )
+  experiment$ms <- SOMACollection$new(file.path(uri, "ms"))$create()
 
-  sdf <- SOMADataFrame$new(uri)
-  sdf$create(tbl$schema, index_column_names = "soma_joinid")
-  sdf$write(tbl)
-  sdf
+  ms_rna <- SOMAMeasurement$new(file.path(uri, "ms", "RNA"))$create()
+  ms_rna$var <- create_and_populate_var(file.path(uri, "var"), nrows = n_var)
+  ms_rna$X <- SOMACollection$new(file.path(uri, "ms", "X"))$create()
+
+  for (layer_name in X_layer_names) {
+    nda <- create_and_populate_sparse_nd_array(
+      uri = file.path(uri, "ms", "X", layer_name),
+      nrows = n_obs,
+      ncols = n_var
+    )
+    ms_rna$X$set(nda, name = layer_name)
+  }
+
+  experiment$ms$set(ms_rna, name = "RNA")
+  experiment
 }
