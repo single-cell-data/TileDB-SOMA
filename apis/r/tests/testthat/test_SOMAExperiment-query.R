928d1f3c381429b768cb56a77f87ea061a303205
Aaron Wolen <aaron@wolen.com>
2023-01-31 09:59:12 -0600

    Add tests for value_filter + coords queries

--- apis/r/tests/testthat/test_SOMAExperiment-query.R
+++ apis/r/tests/testthat/test_SOMAExperiment-query.R
@@ -157,3 +157,79 @@ test_that("querying by value filters", {
   expect_equal(query$obs()$baz$as_vector(), obs_label_values)
   expect_equal(query$var()$quux$as_vector(), var_label_values)
 })
+
+test_that("querying by both coordinates and value filters", {
+  uri <- withr::local_tempdir("soma-experiment-query-coords-and-value-filters")
+
+  n_obs <- 1001L
+  n_var <- 99L
+
+  obs_label_values <- c("1003", "1007", "1038", "1099")
+  var_label_values <- c("1018", "1034", "1067")
+
+# TODO: simplify once tiledb-r supports membership expressions
+  obs_value_filter <- paste0(
+    sprintf("baz == '%s'", obs_label_values),
+    collapse = "||"
+  )
+  var_value_filter <- paste0(
+    sprintf("quux == '%s'", var_label_values),
+    collapse = "||"
+  )
+
+  # obs slice / var value filter
+  query <- ExperimentAxisQuery$new(
+    experiment = experiment,
+    measurement_name = "RNA",
+    obs_query = AxisQuery$new(
+      coords = list(soma_joinid = obs_slice),
+    ),
+    var_query = AxisQuery$new(
+      value_filter = var_value_filter
+    )
+  )
+
+  expect_true(query$n_obs == diff(range(obs_slice)) + 1)
+  expect_true(query$n_vars == length(var_label_values))
+
+  #  obs value filter / var slice
+  query <- ExperimentAxisQuery$new(
+    experiment = experiment,
+    measurement_name = "RNA",
+    obs_query = AxisQuery$new(
+      value_filter = obs_value_filter
+    ),
+    var_query = AxisQuery$new(
+      coords = list(soma_joinid = var_slice)
+    )
+  )
+
+  expect_true(query$n_obs == length(obs_label_values))
+  expect_true(query$n_vars == diff(range(var_slice)) + 1)
+
+  # obs/var slice and value filter
+  query <- ExperimentAxisQuery$new(
+    experiment = experiment,
+    measurement_name = "RNA",
+    obs_query = AxisQuery$new(
+      coords = list(soma_joinid = obs_slice),
+      value_filter = obs_value_filter
+    ),
+    var_query = AxisQuery$new(
+      coords = list(soma_joinid = var_slice),
+      value_filter = var_value_filter
+    )
+  )
+
+  # Determine expected results
+  obs_df <- experiment$obs$read()$to_data_frame()
+  obs_hits <- obs_df$soma_joinid %in% as.integer(obs_slice) &
+    obs_df$baz %in% obs_label_values
+
+  var_df <- experiment$ms$get("RNA")$var$read()$to_data_frame()
+  var_hits <- var_df$soma_joinid %in% as.integer(var_slice) &
+    var_df$quux %in% var_label_values
+
+  expect_equal(query$obs()$to_data_frame(), obs_df[obs_hits,])
+  expect_equal(query$var()$to_data_frame(), var_df[var_hits,])
+})
