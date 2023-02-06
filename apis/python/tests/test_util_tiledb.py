360517bf9cd67e37a4701ca93540e576d969ddb9
Paul Fisher <paul.fisher@tiledb.com>
2023-02-06 14:46:19 -0500

    Capture stdout/stderr from `stats_dump` output.
    Because of missing `stats_reset` calls in previous code, there would be
    huge volumes (megabytes) of stats output if you did `pytest -vs ./tests`
    with no options. This captures the output from `stats_dump` in a fixture
    and verifies its presence, and removes debugging stats management calls.

--- apis/python/tests/test_util_tiledb.py
+++ apis/python/tests/test_util_tiledb.py
@@ -1,9 +1,14 @@
+import pytest
+
 import tiledbsoma
 
 
-def test_stats():
-    """Make sure these exist and don't throw"""
+def test_stats(capfd: pytest.CaptureFixture[str]):
+    """Make sure these exist, don't throw, and write correctly."""
     tiledbsoma.stats_enable()
     tiledbsoma.stats_dump()
     tiledbsoma.stats_reset()
     tiledbsoma.stats_disable()
+    stdout, stderr = capfd.readouterr()
+    assert stdout != ""
+    assert stderr == ""
