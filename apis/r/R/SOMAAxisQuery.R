7f6c5a47494929a89ee30f2138d7337421fbaeed
Aaron Wolen <aaron@wolen.com>
2023-02-10 09:12:15 -0600

    More consistent coordinate validation

--- apis/r/R/SOMAAxisQuery.R
+++ apis/r/R/SOMAAxisQuery.R
@@ -22,37 +22,3 @@ AxisQuery <- R6Class(
     }
   )
 )
-
-#' Validate read coordinates
-#' Ensures that coords is a named list of numeric vectors and, optionally, that
-#' the names of the list correspond to the dimension names of the array.
-#' @param coords A list of coordinates
-#' @param dimnames vector of array dimension names
-#' @noRd
-validate_read_coords <- function(coords, dimnames = NULL) {
-  stopifnot(
-    "'coords' must be a named list" =
-      is.null(coords) || is_named_list(coords),
-    "'coords' must be a list of numeric vectors" =
-      all(vapply_lgl(coords, is.numeric))
-  )
-
-  if (!is.null(dimnames)) {
-    stopifnot(
-      "'dimnames' must be a character vector" = is.character(dimnames),
-      "names of 'coords' must correspond to dimension names" =
-        all(names(coords) %in% dimnames)
-    )
-  }
-  coords
-}
-
-#' Validate read/query value filter
-#' @noRd
-validate_read_value_filter <- function(value_filter) {
-  stopifnot(
-    "'value_filter' must be a scalar character" =
-      is.null(value_filter) || is_scalar_character(value_filter)
-    )
-  value_filter
-}
