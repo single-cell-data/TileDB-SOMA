1176c91125bdc04f57225362524e1a4e9038b30e
John Kerl <kerl.john.r@gmail.com>
2023-02-16 15:41:14 -0500

    rebase

--- apis/python/src/tiledbsoma/__init__.py
+++ apis/python/src/tiledbsoma/__init__.py
@@ -1,6 +1,7 @@
 from somacore import AxisColumnNames, AxisQuery, ExperimentAxisQuery
 
 from ._collection import Collection
+from ._constants import SOMA_JOINID
 from ._dataframe import DataFrame
 from ._dense_nd_array import DenseNDArray
 from ._exception import DoesNotExistError, SOMAError
@@ -13,15 +14,14 @@ from ._general_utilities import (
     get_storage_engine,
     show_package_versions,
 )
+from ._measurement import Measurement
+from ._sparse_nd_array import SparseNDArray
 from .libtiledbsoma import (
     tiledbsoma_stats_disable,
     tiledbsoma_stats_dump,
     tiledbsoma_stats_enable,
     tiledbsoma_stats_reset,
 )
-from ._measurement import Measurement
-from ._sparse_nd_array import SparseNDArray
-from ._exception import DoesNotExistError, SOMAError
 from .options import SOMATileDBContext
 
 __version__ = get_implementation_version()
@@ -44,6 +44,7 @@ __all__ = [
     "show_package_versions",
     "SOMAError",
     "SOMATileDBContext",
+    "SOMA_JOINID",
     "SparseNDArray",
     "tiledbsoma_stats_disable",
     "tiledbsoma_stats_dump",
