d8d6a960fa7afa1e123a79d464d8f6d8c9da40ac
Paul Hoffman <mojaveazure@users.noreply.github.com>
2023-03-01 12:05:46 -0500

    [r] [WIP] Implement `PlatformConfig` and `SOMATileDBContext` for R (#1026)
    * Add new SOMAContextBase class for R
    New mapping class to handle context options for SOMA
    Knows about certain options and can validate them
    Supports any user-provided option
    
    * Add new TileDB Context for SOMA class
    Combines SOMAContextBase with tiledb::tiledb_ctx
    Knows about SOMA and TileDB values and can validate them as needed
    
    * Update imports/exports
    
    * Reimplement SOMA contexts in R6
    Build new R6 mapping type and scalar map
    Use R6 map as basis of SOMAContextBase and SOMATileDBContext
    
    * Update docs for MappingBase
    
    * qol improvements
    
    * Initial version of PlatformConfig
    
    * Updates to PlatformConfig
    Improve setting values in a PlatformConfig
    Improve setting values in a ConfigList
    Add MappingBase$update to add values from an existing map
    
    * Update imports
    Update methods
    Remove S4 code
    
    * Add tests
    
    * Update tests
    
    * Move SOMA code to SOMAContextBase
    Fixes for TileDB context
    New `is_named2` utility function to handle empty names (`""`)
    
    * Add tests for SOMATileDBContext
    
    * Remove unused Collate section
    
    * New roxygen utility functions
    
    * Update docs
    
    * Update docs
    
    * Remove unused template variable
    
    * Add roxygen templates
    
    * Update docs
    
    * devtools::document() updated these
    
    * Add new `as.list.MappingBase()` method
    Update docs
    
    * Throw errors by default for missing values using rlang::missing_arg()
    
    * New to_context() method for SOMATileDBContext objects
    
    * Plumb contexts through
    Ensure TileDBObject is virtual
    Turn TileDBObject$platform_config and ctx to read-only
    Ensure platform_config is PlatformConfig
    Ensure ctx is SOMATileDBContext
    
    * Pass contexts correctly
    
    * Updates to tests to support default error for `$get` methods
    
    * Update typo in docs

---
+++ apis/r/R/ScalarMap.R
@@ -0,0 +1,57 @@
+#' A Mapping Type for Scalars
+#'
+#' An R6 mapping type that is limited to scalar atomic vector types only; can
+#' optionally be limited further to a specific atomic vector type
+#' (eg. \dQuote{\code{logical}})
+#'
+#' @export
+#'
+#' @noMd
+#'
+ScalarMap <- R6::R6Class(
+  classname = 'ScalarMap',
+  inherit = MappingBase,
+  public = list(
+    #' @param type Limit the \code{ScalarMap} to a preset type; choose from:
+    #' \Sexpr[results=rd]{tiledbsoma::rd_atomic()}
+    #'
+    #' @return An instantiated \code{ScalarMap} object with the
+    #' type set to \code{type}
+    #'
+    initialize = function(type = 'any') {
+      private$.type <- match.arg(arg = type, choices = .SCALAR_TYPES())
+    },
+    #' @param key Key to set
+    #' @templateVar key key
+    #' @template param-value
+    #'
+    #' @return \[chainable\] Invisibly returns \code{self} with
+    #' \code{value} added as \code{key}
+    #'
+    set = function(key, value) {
+      stopifnot(is.null(x = value) || length(x = value) == 1L)
+      if (!is.null(x = value) && self$type != 'any') {
+        stopifnot(inherits(x = value, what = self$type))
+      }
+      super$set(key = key, value = value)
+      return(invisible(x = self))
+    }
+  ),
+  active = list(
+    #' @field type The type that this \code{ScalarMap} is limited to
+    #'
+    type = function(value) {
+      if (!missing(x = value)) {
+        stop("The map type cannot be changed", call. = FALSE)
+      }
+      return(private$.type)
+    }
+  ),
+  private = list(
+    .type = character(length = 1L)
+  )
+)
+
+.SCALAR_TYPES <- function() {
+  return(c('any', 'numeric', 'integer', 'character', 'logical'))
+}
