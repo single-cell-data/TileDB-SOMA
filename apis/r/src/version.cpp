9294377e1eff89722010ca298e906482e9ef93a1
John Kerl <john.kerl@tiledb.com>
2023-03-08 22:56:16 +0000

    non-exported `tiledb_embedded_version`

---
+++ apis/r/src/version.cpp
@@ -0,0 +1,13 @@
+
+#include <Rcpp.h>
+#include <tiledbsoma/tiledbsoma>
+
+//' TileDB Embedded Version interface
+//'
+//' This gets the version of the TileDB Embedded library that is currently in use.
+//'
+//' @export
+// [[Rcpp::export]]
+std::string tiledb_embedded_version() {
+    return tiledbsoma::version::as_string();
+}
