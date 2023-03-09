f9e29883c7de5e4c93b3b0a68a3fe69fcceee362
John Kerl <john.kerl@tiledb.com>
2023-03-09 22:46:40 +0000

    undo previous; needed to branch

--- apis/r/src/riterator.cpp
+++ apis/r/src/riterator.cpp
@@ -48,7 +48,7 @@ namespace tdbs = tiledbsoma;
 //' \dontrun{
 //' ctx <- tiledb::tiledb_ctx()
 //' uri <- "test/soco/pbmc3k_processed/obs"
-//' sr <- sr_setup(uri, config=as.character(tiledb::config(ctx)), loglevel="warn")
+//' sr <- sr_setup(uri, config=as.character(config(ctx)), loglevel="warn")
 //' rl <- data.frame()
 //' while (!sr_complete(sr)) {
 //'     sr |>
