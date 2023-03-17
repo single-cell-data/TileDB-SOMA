a0427ab14231eba8758825ea99889bf02584e2ee
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-03-17 16:52:06 -0400

    Add notebooks as tutorials to the docsite

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,7 @@ release = "1.0.0rc5"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.intersphinx", 'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.intersphinx", 'sphinx.ext.napoleon', "nbsphinx"]
 
 # Mapping for linking between RTD subprojects.
 if readthedocs:
