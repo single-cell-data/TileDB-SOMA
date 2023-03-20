218a74654dcef95da553a90bcaadd106d26df762
John Kerl <kerl.john.r@gmail.com>
2023-03-20 17:09:55 -0400

    [c++] Have TileDB-SOMA 1.0 depend on  TileDB Embedded 2.14.2

--- apis/r/tools/get_tarball.R
+++ apis/r/tools/get_tarball.R
@@ -8,13 +8,13 @@ macosver <- ""
 
 if (isMac) {
     if (isX86) {
-        url <- "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-macos-x86_64-2.14.0-27eed08.tar.gz"
+        url <- "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-macos-x86_64-2.14.2-42147ab.tar.gz"
         macosver <- "-mmacosx-version-min=10.14"
     } else {
-        url <- "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-macos-arm64-2.14.0-27eed08.tar.gz"
+        url <- "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-macos-arm64-2.14.2-42147ab.tar.gz"
     }
 } else if (isLinux) {
-    url <- "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-linux-x86_64-2.14.0-27eed08.tar.gz"
+    url <- "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-linux-x86_64-2.14.2-42147ab.tar.gz"
 } else {
     stop("Unsupported platform for downloading artifacts. Please have TileDB Core installed locally.")
 }
