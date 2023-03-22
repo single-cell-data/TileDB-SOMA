9f1030de750cf642e1d0f10dcdf70e5d06dfd378
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-03-22 11:15:15 -0400

    add version testing

---
+++ apis/system/tests/test_version_match.py
@@ -0,0 +1,23 @@
+import tiledb
+
+from .common import BasePythonRInterop
+
+
+class TestVersionMatch(BasePythonRInterop):
+    def test_version_match(self):
+        """
+        Verifies that the TileDB version of R and Python match. If they don't, the roundtrip
+        testing will likely fail.
+        """
+        version = tiledb.libtiledb.version()
+        major, minor = version[0], version[1]
+
+        self.execute_R_script(
+            f"""
+        library("tiledb")
+        library("testthat")
+        version = tiledb_version()
+        expect_equal(as.integer(version["major"]), {major})
+        expect_equal(as.integer(version["minor"]), {minor})
+        """
+        )
