8b89847d92a33195da5c9849306d2a86b4eac5f0
John Kerl <kerl.john.r@gmail.com>
2023-03-31 18:15:20 -0400

    [c++] Fix build on MacOS 13.3 (#1206)

--- libtiledbsoma/cmake/Modules/FindCatch_EP.cmake
+++ libtiledbsoma/cmake/Modules/FindCatch_EP.cmake
@@ -1,3 +1,9 @@
+# catch2 has a set-but-unused-variable warning as of MacOS 13.3.
+# So we need to not use -Werror, as that's third-party code out of our control.
+#
+# See also:
+# https://discourse.cmake.org/t/how-to-turn-off-warning-flags-for-project-added-by-fetchcontent-declare/2461
+
 Include(FetchContent)
 
 FetchContent_Declare(
@@ -6,4 +12,23 @@ FetchContent_Declare(
   GIT_TAG        v3.0.1
 )
 
+get_property(
+    compile_options
+    DIRECTORY
+    PROPERTY COMPILE_OPTIONS
+)
+
+set_property(
+    DIRECTORY
+    APPEND
+    PROPERTY COMPILE_OPTIONS -Wno-error=unused-but-set-variable
+)
+
 FetchContent_MakeAvailable(Catch2)
+
+set_property(
+    DIRECTORY
+    PROPERTY COMPILE_OPTIONS ${compile_options}
+)
+
+unset(compile_options)
