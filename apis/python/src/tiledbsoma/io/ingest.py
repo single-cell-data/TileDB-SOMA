59adb55b409e63025b1142844978da93e1a5ae61
John Kerl <kerl.john.r@gmail.com>
2023-04-11 11:02:08 -0400

    code-review feedback: dense storage

--- apis/python/src/tiledbsoma/io/ingest.py
+++ apis/python/src/tiledbsoma/io/ingest.py
@@ -1376,7 +1376,7 @@ def to_anndata(
                     )
                 obsm[key] = conversions.csr_from_tiledb_df(
                     matrix, nobs, nobs_times_width // nobs
-                )
+                ).todense()
 
     varm = {}
     if "varm" in measurement:
@@ -1405,7 +1405,7 @@ def to_anndata(
                     )
                 varm[key] = conversions.csr_from_tiledb_df(
                     matrix, nvar, nvar_times_width // nvar
-                )
+                ).todense()
 
     obsp = {}
     if "obsp" in measurement:
