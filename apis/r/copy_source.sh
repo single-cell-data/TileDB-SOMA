4a855657426d07ad3b49b24ca2c66367783ba30c
Dirk Eddelbuettel <edd@debian.org>
2023-04-24 17:28:04 -0500

    [r] [WIP] build against shared library libtiledbsoma

--- apis/r/copy_source.sh
+++ apis/r/copy_source.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## part one: if we are inside the git repo, simply copy over
-test -d ../../libtiledbsoma && cp -a ../../libtiledbsoma src/
+test -d ../../libtiledbsoma && cp -a ../../libtiledbsoma src/ && cp -a ../../scripts/bld src/libtiledbsoma && rm -rf src/libtiledbsoma/build src/libtiledbsoma/test/__pycache__
 
 ## part two: also create a tar.gz 'for keeps' as we may need to install from partial source tree
-test -d ../../libtiledbsoma && cd ../.. && tar -cz -f apis/r/libtiledbsoma.tar.gz libtiledbsoma
+#test -d ../../libtiledbsoma && cd ../.. && tar -cz -f apis/r/libtiledbsoma.tar.gz libtiledbsoma
