5da0a3a5231b8da916f3e5e90348cbb35a9e2b14
Dirk Eddelbuettel <edd@debian.org>
2023-05-13 07:58:28 -0500

    [r] Provide reference to `libtiledbsoma` source via soft-link (#1372)
    * Add symbolic link
    
    * Build libtiledbsoma if needed off soft-link / expanded directory
    
    R CMD build does the right thing too
    
    * Correct stray characters in comment [ci skip]
    
    * Roll micro-version [ci skip]

---
+++ apis/r/tools/get_tarball.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+## version pinning info
+tiledb_core_version <- "2.15.2"
+tiledb_core_sha1 <- "90f30eb"
+
+if ( ! dir.exists("inst/") ) {
+    stop("No 'inst/' directory. Exiting.", call. = FALSE)
+}
+
+makeUrl <- function(arch, ver=tiledb_core_version, sha1=tiledb_core_sha1) {
+    sprintf("https://github.com/TileDB-Inc/TileDB/releases/download/%s/tiledb-%s-%s-%s.tar.gz", ver, arch, ver, sha1)
+}
+
+isX86 <- Sys.info()["machine"] == "x86_64"
+isMac <- Sys.info()["sysname"] == "Darwin"
+isLinux <- Sys.info()["sysname"] == "Linux"
+
+if (isMac && isX86) {
+    url <- makeUrl("macos-x86_64")
+} else if (isMac && !isX86) {
+    url <- makeUrl("macos-arm64")
+} else if (isLinux) {
+    url <- makeUrl("linux-x86_64")
+} else {
+    stop("Unsupported platform for downloading artifacts. Please have TileDB Core installed locally.")
+}
+
+tarball <- "tiledb.tar.gz"
+if (!file.exists(tarball)) download.file(url, tarball, quiet=TRUE)
+if (!dir.exists("inst/tiledb")) untar(tarball, exdir="inst/tiledb")
