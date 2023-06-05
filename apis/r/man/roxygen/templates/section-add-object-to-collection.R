c2b41676461f14957422f8663909dcc616e330d9
Aaron Wolen <aaron@wolen.com>
2023-06-05 12:22:19 -0500

    Review feedback [skip ci]

--- apis/r/man/roxygen/templates/section-add-object-to-collection.R
+++ apis/r/man/roxygen/templates/section-add-object-to-collection.R
@@ -1,7 +1,7 @@
 #' @section Adding new objects to a collection:
 #'
 #' The [`<%= class %>`] class provides a number of type-specific methods for
-#' adding new object to the collection, such as `add_new_sparse_ndarray()` and
+#' adding new a object to the collection, such as `add_new_sparse_ndarray()` and
 #' `add_new_dataframe()`. These methods will create the new object and add it as
 #' member of the `<%= class %>`. The new object will always inherit the parent
 #' context (see [`SOMATileDBContext`]) and, by default, its platform
