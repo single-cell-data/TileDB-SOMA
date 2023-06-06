3d44f6983ead3a16c46c8e82602a2a66b5f9e75e
Dirk Eddelbuettel <edd@debian.org>
2023-06-06 09:38:28 -0500

    [r] Use artifacts if no args or system libs found

---
+++ apis/r/tools/get_tarball_soma.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+
+## version pinning info (NB: temporary from test repo url below)
+tiledb_soma_version <- "0.0.0.11.9026"
+tiledb_soma_sha1 <- "39be1e2"
+##tiledb_soma_repo <- "single-cell-data/TileDB-SOMA"
+tiledb_soma_repo <- "eddelbuettel/tldbsm2"
+
+if ( ! dir.exists("inst/") ) {
+    stop("No 'inst/' directory. Exiting.", call. = FALSE)
+}
+
+makeUrl <- function(arch, repo=tiledb_soma_repo, ver=tiledb_soma_version, sha1=tiledb_soma_sha1) {
+    sprintf("https://github.com/%s/releases/download/%s/libtiledbsoma-%s-%s-%s.tar.gz", repo, ver, arch, ver, sha1)
+}
+
+isX86 <- Sys.info()["machine"] == "x86_64"
+isMac <- Sys.info()["sysname"] == "Darwin"
+isLinux <- Sys.info()["sysname"] == "Linux"
+
+if (isMac && isX86) {
+    url <- makeUrl("macos-x86_64")
+} else if (isMac && !isX86) {
+    url <- makeUrl("macos-arm64")
+} else if (isLinux) {
+    url <- makeUrl("linux-x86_64")
+} else {
+    stop("Unsupported platform for downloading artifacts. Please have TileDB Core installed locally.")
+}
+
+tarball <- "libtiledbsoma.tar.gz"
+if (!file.exists(tarball)) download.file(url, tarball, quiet=TRUE)
+if (!dir.exists("inst/tiledbsoma")) untar(tarball, exdir="inst/tiledbsoma")
