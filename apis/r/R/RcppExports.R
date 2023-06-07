9879a95bba6ccd015f321f86b967a44afb64e2fe
Aaron Wolen <aaron@wolen.com>
2023-06-07 10:42:02 -0500

    Update docs

--- apis/r/R/RcppExports.R
+++ apis/r/R/RcppExports.R
@@ -22,11 +22,9 @@
 #' @param config Optional character vector containing TileDB config.
 #' @return A List object with two pointers to Arrow array data and schema is returned
 #' @examples
-#' \dontrun{
-#' uri <- "test/soco/pbmc3k_processed/obs"
+#' uri <- extract_dataset("soma-dataframe-pbmc3k-processed-obs")
 #' z <- soma_array_reader(uri)
-#' tb <- as_arrow_table(z)
-#' }
+#' arrow::RecordBatch$import_from_c(z$array_data, z$schema)
 #' @export
 soma_array_reader <- function(uri, colnames = NULL, qc = NULL, dim_points = NULL, dim_ranges = NULL, batch_size = "auto", result_order = "auto", loglevel = "auto", config = NULL) {
     .Call(`_tiledbsoma_soma_array_reader`, uri, colnames, qc, dim_points, dim_ranges, batch_size, result_order, loglevel, config)
@@ -96,22 +94,16 @@ shape <- function(uri, config = NULL) {
 #' returns a boolean, and \code{sr_next} returns an Arrow array helper object.
 #'
 #' @examples
-#' \dontrun{
+#' uri <- extract_dataset("soma-dataframe-pbmc3k-processed-obs")
 #' ctx <- tiledb::tiledb_ctx()
-#' uri <- "test/soco/pbmc3k_processed/obs"
-#' sr <- sr_setup(uri, config=as.character(tiledb::config(ctx)), loglevel="auto")
+#' sr <- sr_setup(uri, config=as.character(tiledb::config(ctx)))
 #' rl <- data.frame()
 #' while (!sr_complete(sr)) {
-#'     sr |>
-#'         sr_next() |>
-#'         as_arrow_table() |>
-#'         collect() |>
-#'         as.data.frame() |>
-#'         data.table() -> D
-#'     rl <- rbind(rl, D)
+#'   dat <- sr_next(sr)
+#'   rb <- arrow::RecordBatch$import_from_c(dat$array_data, dat$schema)
+#'   rl <- rbind(rl, as.data.frame(rb))
 #' }
 #' summary(rl)
-#' }
 #' @export
 sr_setup <- function(uri, config, colnames = NULL, qc = NULL, dim_points = NULL, dim_ranges = NULL, batch_size = "auto", result_order = "auto", timestamp_end = NULL, loglevel = "auto") {
     .Call(`_tiledbsoma_sr_setup`, uri, config, colnames, qc, dim_points, dim_ranges, batch_size, result_order, timestamp_end, loglevel)
