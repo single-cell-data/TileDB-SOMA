679d103bde64afd7ddffbe543a1758109b6b2e34
Vivian Nguyen <vivian@tiledb.com>
2023-07-13 17:08:16 -0500

    [c++] Addition of `SOMADenseNDArray`

---
+++ libtiledbsoma/src/soma/soma_dense_ndarray.cc
@@ -0,0 +1,136 @@
+/**
+ * @file   soma_dense_ndarray.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2023 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ *   This file defines the SOMADenseNDArray class.
+ */
+
+#include "soma_dense_ndarray.h"
+#include "soma_array.h"
+
+namespace tiledbsoma {
+using namespace tiledb;
+
+//===================================================================
+//= public static
+//===================================================================
+
+std::unique_ptr<SOMADenseNDArray> SOMADenseNDArray::create(
+    std::shared_ptr<Context> ctx, std::string_view uri, ArraySchema schema) {
+    SOMAArray::create(ctx, uri, schema, "SOMADenseNDArray");
+    return std::make_unique<SOMADenseNDArray>(
+        TILEDB_READ, uri, ctx, std::vector<std::string>(), std::nullopt);
+}
+
+std::unique_ptr<SOMADenseNDArray> SOMADenseNDArray::open(
+    tiledb_query_type_t mode,
+    std::string_view uri,
+    std::vector<std::string> column_names,
+    std::map<std::string, std::string> platform_config,
+    std::optional<std::pair<uint64_t, uint64_t>> timestamp) {
+    auto ctx = std::make_shared<Context>(Config(platform_config));
+    return std::make_unique<SOMADenseNDArray>(
+        mode, uri, ctx, column_names, timestamp);
+}
+
+std::unique_ptr<SOMADenseNDArray> SOMADenseNDArray::open(
+    tiledb_query_type_t mode,
+    std::shared_ptr<Context> ctx,
+    std::string_view uri,
+    std::vector<std::string> column_names,
+    std::optional<std::pair<uint64_t, uint64_t>> timestamp) {
+    return std::make_unique<SOMADenseNDArray>(
+        mode, uri, ctx, column_names, timestamp);
+}
+
+//===================================================================
+//= public non-static
+//===================================================================
+
+SOMADenseNDArray::SOMADenseNDArray(
+    tiledb_query_type_t mode,
+    std::string_view uri,
+    std::shared_ptr<Context> ctx,
+    std::vector<std::string> column_names,
+    std::optional<std::pair<uint64_t, uint64_t>> timestamp) {
+    array_ = std::make_shared<SOMAArray>(
+        mode,
+        uri,
+        "unnamed",  // name
+        ctx,
+        column_names,
+        "auto",  // batch_size,
+        "auto",  // result_order,
+        timestamp);
+    array_->reset();
+    array_->submit();
+}
+
+void SOMADenseNDArray::open(
+    tiledb_query_type_t mode,
+    std::optional<std::pair<uint64_t, uint64_t>> timestamp) {
+    array_->open(mode, timestamp);
+    array_->reset();
+    array_->submit();
+}
+
+void SOMADenseNDArray::close() {
+    array_->close();
+}
+
+const std::string& SOMADenseNDArray::uri() const {
+    return array_->uri();
+}
+
+std::shared_ptr<Context> SOMADenseNDArray::ctx() {
+    return array_->ctx();
+}
+
+std::shared_ptr<ArraySchema> SOMADenseNDArray::schema() const {
+    return array_->schema();
+}
+
+std::vector<int64_t> SOMADenseNDArray::shape() const {
+    return array_->shape();
+}
+
+int64_t SOMADenseNDArray::ndim() const {
+    return array_->ndim();
+}
+
+std::optional<std::shared_ptr<ArrayBuffers>> SOMADenseNDArray::read_next() {
+    return array_->read_next();
+}
+
+void SOMADenseNDArray::write(std::shared_ptr<ArrayBuffers> buffers) {
+    array_->reset();
+    array_->submit();
+    array_->write(buffers);
+}
+
+}  // namespace tiledbsoma
---
+++ libtiledbsoma/src/soma/soma_dense_ndarray.h
@@ -0,0 +1,218 @@
+/**
+ * @file   soma_dense_ndarray.h
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2023 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ *   This file defines the SOMADenseNDArray class.
+ */
+
+#ifndef SOMA_DENSE_NDARRAY
+#define SOMA_DENSE_NDARRAY
+
+#include <tiledb/tiledb>
+#include "soma_object.h"
+
+namespace tiledbsoma {
+
+class SOMAArray;
+class ArrayBuffers;
+
+using namespace tiledb;
+
+class SOMADenseNDArray : public SOMAObject {
+   public:
+    //===================================================================
+    //= public static
+    //===================================================================
+
+    /**
+     * @brief Create a SOMADenseNDArray object at the given URI.
+     *
+     * @param ctx TileDB context
+     * @param uri URI to create the SOMADenseNDArray
+     * @param schema TileDB ArraySchema
+     */
+    static std::unique_ptr<SOMADenseNDArray> create(
+        std::shared_ptr<Context> ctx, std::string_view uri, ArraySchema schema);
+
+    /**
+     * @brief Open and return a SOMADenseNDArray object at the given URI.
+     *
+     * @param uri URI to create the SOMADenseNDArray
+     * @param schema TileDB ArraySchema
+     * @param platform_config Platform-specific options used to create this
+     * DataFrame
+     * @param column_names A list of column names to use as user-defined index
+     * columns (e.g., ``['cell_type', 'tissue_type']``). All named columns must
+     * exist in the schema, and at least one index column name is required.
+     * @param timestamp If specified, overrides the default timestamp used to
+     * open this object. If unset, uses the timestamp provided by the context.
+     * @return std::unique_ptr<SOMADenseNDArray> SOMADenseNDArray
+     */
+    static std::unique_ptr<SOMADenseNDArray> open(
+        tiledb_query_type_t mode,
+        std::string_view uri,
+        std::vector<std::string> column_names = {},
+        std::map<std::string, std::string> platform_config = {},
+        std::optional<std::pair<uint64_t, uint64_t>> timestamp = std::nullopt);
+
+    /**
+     * @brief Open and return a SOMADenseNDArray object at the given URI.
+     *
+     * @param ctx TileDB context
+     * @param uri URI to create the SOMADenseNDArray
+     * @param schema TileDB ArraySchema
+     * @param platform_config Platform-specific options used to create this
+     * DataFrame
+     * @param column_names A list of column names to use as user-defined index
+     * columns (e.g., ``['cell_type', 'tissue_type']``). All named columns must
+     * exist in the schema, and at least one index column name is required.
+     * @param timestamp If specified, overrides the default timestamp used to
+     * open this object. If unset, uses the timestamp provided by the context.
+     * @return std::unique_ptr<SOMADenseNDArray> SOMADenseNDArray
+     */
+    static std::unique_ptr<SOMADenseNDArray> open(
+        tiledb_query_type_t mode,
+        std::shared_ptr<Context> ctx,
+        std::string_view uri,
+        std::vector<std::string> column_names = {},
+        std::optional<std::pair<uint64_t, uint64_t>> timestamp = std::nullopt);
+
+    //===================================================================
+    //= public non-static
+    //===================================================================
+
+    /**
+     * @brief Construct a new SOMADenseNDArray object.
+     *
+     * @param mode TILEDB_READ or TILEDB_WRITE
+     * @param uri URI of the array
+     * @param name name of the array
+     * @param ctx TileDB context
+     * @param column_names Columns to read
+     * @param batch_size Batch size
+     * @param result_order Result order
+     * @param timestamp Timestamp
+     */
+    SOMADenseNDArray(
+        tiledb_query_type_t mode,
+        std::string_view uri,
+        std::shared_ptr<Context> ctx,
+        std::vector<std::string> column_names,
+        std::optional<std::pair<uint64_t, uint64_t>> timestamp);
+
+    /**
+     * Open the SOMADenseNDArray object.
+     *
+     * @param mode TILEDB_READ or TILEDB_WRITE
+     * @param timestamp Timestamp
+     */
+    void open(
+        tiledb_query_type_t mode,
+        std::optional<std::pair<uint64_t, uint64_t>> timestamp = std::nullopt);
+
+    /**
+     * Closes the SOMADenseNDArray object.
+     */
+    void close();
+
+    /**
+     * Returns the constant "SOMADenseNDArray".
+     *
+     * @return std::string
+     */
+    std::string type() const {
+        return "SOMADenseNDArray";
+    }
+
+    /**
+     * Get the Context associated with the SOMADenseNDArray.
+     *
+     * @return std::shared_ptr<Context>
+     */
+    std::shared_ptr<Context> ctx();
+
+    /**
+     * Return whether the SOMADenseNDArray is sparse.
+     *
+     * @return false
+     */
+    bool is_sparse() {
+        return false;
+    };
+
+    /**
+     * @brief Get URI of the SOMADenseNDArray.
+     *
+     * @return std::string URI
+     */
+    const std::string& uri() const;
+
+    /**
+     * Return data schema, in the form of a TileDB ArraySchema.
+     *
+     * @return std::shared_ptr<ArraySchema>
+     */
+    std::shared_ptr<ArraySchema> schema() const;
+
+    /**
+     * @brief Get the capacity of each dimension.
+     *
+     * @return A vector with length equal to the number of dimensions; each
+     * value in the vector is the capcity of each dimension.
+     */
+    std::vector<int64_t> shape() const;
+
+    /**
+     * Return the number of dimensions.
+     *
+     * @return int64_t
+     */
+    int64_t ndim() const;
+
+    /**
+     * @brief Read the next chunk of results from the query. If all results have
+     * already been read, std::nullopt is returned.
+     */
+    std::optional<std::shared_ptr<ArrayBuffers>> read_next();
+
+    /**
+     * @brief Write ArrayBuffers data to the dataframe.
+     */
+    void write(std::shared_ptr<ArrayBuffers>);
+
+   private:
+    //===================================================================
+    //= private non-static
+    //===================================================================
+
+    // SOMAArray
+    std::shared_ptr<SOMAArray> array_;
+};
+}  // namespace tiledbsoma
+
+#endif  // SOMA_DENSE_NDARRAY
