c452c26bd63e6cd093ad77613d367f2c8e716af3
Dirk Eddelbuettel <edd@debian.org>
2023-07-27 08:43:31 -0500

    Cosmetic

--- apis/r/R/TableReadIter.R
+++ apis/r/R/TableReadIter.R
@@ -1,8 +1,8 @@
 #' SOMA Read Iterator over Arrow Table
 #'
 #' @description
-#' `TableReadIter` is a class that allows for iteration over 
-#'  a reads on \link{SOMASparseNDArray} and \link{SOMADataFrame}. 
+#' `TableReadIter` is a class that allows for iteration over
+#'  a reads on \link{SOMASparseNDArray} and \link{SOMADataFrame}.
 #' Iteration chunks are retrieved as arrow::\link[arrow]{Table}
 #' @export
 
@@ -11,28 +11,28 @@ TableReadIter <- R6::R6Class(
   inherit = ReadIter,
 
   public = list(
-    
+
     #' @description  Concatenate remainder of iterator.
     #' @return arrow::\link[arrow]{Table}
     concat = function(){
-  
+
       if(self$read_complete()) {
         warning("Iteration complete, returning NULL")
         return(NULL)
       }
-    
+
       tbl <- self$read_next()
-      
+
       while (!self$read_complete()) {
         tbl <- arrow::concat_tables(tbl, self$read_next())
       }
-      
+
       tbl
-      
+
     }),
-  
+
   private = list(
-                 
+
     ## refined from base class
     soma_reader_transform = function(x) {
       at <- soma_array_to_arrow_table(x)
@@ -40,7 +40,7 @@ TableReadIter <- R6::R6Class(
           if (!is.null(private$enums[[n]])) {
               at[[n]] <- arrow::DictionaryArray$create(at[[n]]$as_vector(), private$enums[[n]])
           }
-      }  # DE::TEMP::FIXME start
+      }  # DE::TEMP::FIXME end
       at
     }
 
