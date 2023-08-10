c94d73543864da0fc0ed2adf4f48e2f8c58a94da
Paul Hoffman <paul.hoffman@tiledb.com>
2023-08-09 23:41:40 -0400

    Replace `airway` with `pbmc3k.sce`

--- apis/r/tests/testthat/test-SummarizedExperimentIngest.R
+++ apis/r/tests/testthat/test-SummarizedExperimentIngest.R
@@ -1,27 +1,43 @@
 test_that("Write SummarizedExperiment mechanics", {
   skip_if_not_installed('SummarizedExperiment', '1.28.0')
-  skip_if_not_installed('airway')
-  se <- get_data('airway', package = 'airway')
-  uri <- withr::local_tempdir('airway')
+  skip_if_not_installed('pbmc3k.sce')
 
-  expect_no_condition(uri <- write_soma(se, uri, 'airway'))
+  se <- get_data('pbmc3k.final', package = 'pbmc3k.sce')
+  var_df <- SummarizedExperiment::rowData(se)
+  features <- rownames(se)
+
+  se <- as(se, 'SummarizedExperiment')
+  SummarizedExperiment::rowData(se) <- var_df
+  rownames(se) <- features
+
+  uri <- withr::local_tempdir('summarized-experiment')
+
+  expect_no_condition(uri <- suppressMessages(write_soma(se, uri, 'RNA')))
 
   expect_type(uri, 'character')
-  expect_true(grepl('^airway', basename(uri)))
+  expect_true(grepl('^summarized-experiment', basename(uri)))
 
   expect_no_condition(experiment <- SOMAExperimentOpen(uri))
+  expect_s3_class(experiment, 'SOMAExperiment')
   on.exit(experiment$close())
 
   expect_no_error(experiment$ms)
 
   expect_equal(experiment$mode(), "READ")
   expect_s3_class(experiment, 'SOMAExperiment')
-  expect_true(grepl('^airway', basename(experiment$uri)))
+  expect_true(grepl('^summarized-experiment', basename(experiment$uri)))
 
-  expect_identical(experiment$ms$names(), 'airway')
-  expect_s3_class(ms <- experiment$ms$get('airway'), 'SOMAMeasurement')
+  expect_s3_class(experiment$obs, 'SOMADataFrame')
 
-  expect_identical(ms$X$names(), SummarizedExperiment::assayNames(se))
+  expect_identical(experiment$ms$names(), 'RNA')
+  expect_s3_class(ms <- experiment$ms$get('RNA'), 'SOMAMeasurement')
+
+  expect_s3_class(ms$var, 'SOMADataFrame')
+
+  expect_identical(
+    sort(ms$X$names()),
+    sort(SummarizedExperiment::assayNames(se))
+  )
 
   expect_error(ms$obsm)
   expect_error(ms$varm)
@@ -31,6 +47,11 @@ test_that("Write SummarizedExperiment mechanics", {
   expect_identical(
     setdiff(experiment$obs$attrnames(), 'obs_id'),
     names(SummarizedExperiment::colData(se))
+  )
+
+  expect_identical(
+    setdiff(ms$var$attrnames(), 'var_id'),
+    names(SummarizedExperiment::rowData(se))
   )
 
   # Test ms_name assertions
