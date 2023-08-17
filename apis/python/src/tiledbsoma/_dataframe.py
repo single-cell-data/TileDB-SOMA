02241e85d4b7ffb93e9ca37f93002bb4b9f37d4b
Vivian Nguyen <vivian@tiledb.com>
2023-08-17 15:36:42 -0400

    Run pre-commit hook

--- apis/python/src/tiledbsoma/_dataframe.py
+++ apis/python/src/tiledbsoma/_dataframe.py
@@ -135,7 +135,9 @@ class DataFrame(TileDBArray, somacore.DataFrame):
         platform_config: Optional[options.PlatformConfig] = None,
         context: Optional[SOMATileDBContext] = None,
         tiledb_timestamp: Optional[OpenTimestamp] = None,
-        enumerations: Optional[dict[str, Sequence[Any]]] = None,
+        enumerations: Optional[
+            Dict[str, Union[Sequence[Any], np.ndarray[Any, Any]]]
+        ] = None,
         ordered_enumerations: Optional[Sequence[str]] = None,
         column_to_enumerations: Optional[dict[str, str]] = None,
     ) -> "DataFrame":
