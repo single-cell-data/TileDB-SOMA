78a4f082a5a5f418d1bc08e29f9359453f43e5d7
Dirk Eddelbuettel <edd@debian.org>
2023-08-18 12:33:42 -0500

    Snapshot with finalizer in R6 class

--- apis/r/src/riterator.cpp
+++ apis/r/src/riterator.cpp
@@ -146,12 +146,12 @@ Rcpp::XPtr<tdbs::SOMAArray> sr_setup(const std::string& uri,
 
 // [[Rcpp::export]]
 bool sr_complete(Rcpp::XPtr<tdbs::SOMAArray> sr) {
-   check_xptr_tag<tdbs::SOMAArray>(sr);
-   bool complt = sr->is_complete(true);
-   bool initial = sr->is_initial_read();
-   bool res = complt && !initial; // completed transfer if query status complete and query ran once
-   spdl::debug("[sr_complete] Complete query test {} (compl {} initial {})", res, complt, initial);
-   return res;
+    check_xptr_tag<tdbs::SOMAArray>(sr);
+    bool complt = sr->is_complete(true);
+    bool initial = sr->is_initial_read();
+    bool res = complt && !initial; // completed transfer if query status complete and query ran once
+    spdl::debug("[sr_complete] Complete query test {} (compl {} initial {})", res, complt, initial);
+    return res;
 }
 
 Rcpp::List create_empty_arrow_table() {
@@ -233,14 +233,22 @@ inline void registerXptrFinalizer(SEXP s, R_CFinalizer_t f, bool onexit = true) 
 }
 // Finalizer helper with void f(SEXP) signature
 void SOMAArrayFinalizer(SEXP sx) {
+    spdl::debug(tfm::format("[SOMAArrayFinalizer] entered"));
     Rcpp::XPtr<tdbs::SOMAArray> xp(sx);
+    spdl::debug(tfm::format("[SOMAArrayFinalizer] instantiated"));
     check_xptr_tag<tdbs::SOMAArray>(xp);
+    spdl::debug(tfm::format("[SOMAArrayFinalizer] checked"));
     std::shared_ptr<tiledb::Context> ctx = xp->ctx();
+    spdl::debug(tfm::format("[SOMAArrayFinalizer] referenced once"));
+    if (ctx == nullptr) return;
     std::shared_ptr<tiledb_ctx_t> ctx_cptr = ctx->ptr();
+    if (ctx_cptr == nullptr) return;
     spdl::debug(tfm::format("[SOMAArrayFinalizer] count on ctx %d cptr %d",
                             ctx.use_count(), ctx_cptr.use_count()));
     tiledb_ctx_t* ptr = ctx_cptr.get();
+    if (ptr == nullptr) return;
     tiledb_ctx_free(&ptr);
+    ptr = nullptr;
     spdl::debug(tfm::format("[SOMAArrayFinalizer] freed"));
 }
 
