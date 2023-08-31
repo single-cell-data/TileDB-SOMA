f92dce51dfb9fea3500e2d3a17e9ab1c00d35d53
John Kerl <kerl.john.r@gmail.com>
2023-08-31 19:59:10 -0400

    bisecting

--- apis/r/tests/testthat/test-Arrow-utils.R
+++ apis/r/tests/testthat/test-Arrow-utils.R
@@ -82,40 +82,40 @@ test_that("TileDB classes can be converted to Arrow equivalents", {
 })
 
 # #1651 CI debug start
-test_that("Validating arrow data type compatibility", {
-  expect_false(check_arrow_data_types(arrow::int32(), arrow::float32()))
-  expect_true(check_arrow_data_types(arrow::int32(), arrow::int32()))
-  # strings and large strings are compatible
-  expect_true(check_arrow_data_types(arrow::string(), arrow::large_utf8()))
-
-  expect_error(
-    check_arrow_data_types("not an arrow data type", arrow::int32())
-  )
-})
-
-test_that("Validating arrow schema data type compatibility", {
-  from <- arrow::schema(foo = arrow::int32())
-  to <- arrow::schema(foo = arrow::int32())
-  expect_true(check_arrow_schema_data_types(from, to))
-
-  # Add incompatible fields
-  from$bar <- arrow::int16()
-  to$bar <- arrow::float16()
-  expect_error(
-    check_arrow_schema_data_types(from, to),
-    "Schemas are incompatible"
-  )
-
-  # Schemas with different fields
-  from$baz <- arrow::string()
-  expect_error(
-    check_arrow_schema_data_types(from, to),
-    "'from' and 'to' must have the same number of fields"
-  )
-
-  to$fizz <- arrow::string()
-  expect_error(
-    check_arrow_schema_data_types(from, to),
-    "'from' and 'to' must have the same field names"
-  )
-})
+#test_that("Validating arrow data type compatibility", {
+#  expect_false(check_arrow_data_types(arrow::int32(), arrow::float32()))
+#  expect_true(check_arrow_data_types(arrow::int32(), arrow::int32()))
+#  # strings and large strings are compatible
+#  expect_true(check_arrow_data_types(arrow::string(), arrow::large_utf8()))
+#
+#  expect_error(
+#    check_arrow_data_types("not an arrow data type", arrow::int32())
+#  )
+#})
+#
+#test_that("Validating arrow schema data type compatibility", {
+#  from <- arrow::schema(foo = arrow::int32())
+#  to <- arrow::schema(foo = arrow::int32())
+#  expect_true(check_arrow_schema_data_types(from, to))
+#
+#  # Add incompatible fields
+#  from$bar <- arrow::int16()
+#  to$bar <- arrow::float16()
+#  expect_error(
+#    check_arrow_schema_data_types(from, to),
+#    "Schemas are incompatible"
+#  )
+#
+#  # Schemas with different fields
+#  from$baz <- arrow::string()
+#  expect_error(
+#    check_arrow_schema_data_types(from, to),
+#    "'from' and 'to' must have the same number of fields"
+#  )
+#
+#  to$fizz <- arrow::string()
+#  expect_error(
+#    check_arrow_schema_data_types(from, to),
+#    "'from' and 'to' must have the same field names"
+#  )
+#})
