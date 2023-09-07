1b7f419dc57827d72ae53e9a93cfe05436883720
Vivian Nguyen <vivian@tiledb.com>
2023-09-07 14:41:53 -0500

    check ifattr exists

--- apis/python/src/tiledbsoma/_arrow_types.py
+++ apis/python/src/tiledbsoma/_arrow_types.py
@@ -193,11 +193,14 @@ def df_to_arrow(df: pd.DataFrame) -> pa.Table:
     for key in df:
         column = df[key]
         if is_categorical_dtype(column.dtype):
-            column = column.astype("category")
-            df[key] = pd.core.arrays.categorical.Categorical(
-                values=column,
-                categories=column.categories,
-                ordered=bool(column.ordered),
+            if hasattr(column.values, "categories"):
+                categories = column.values.categories
+
+            if hasattr(column.values, "ordered"):
+                ordered = column.values.ordered
+
+            df[key] = pd.Categorical(
+                values=column, categories=categories, ordered=ordered
             )
 
     arrow_table = pa.Table.from_pandas(df)
