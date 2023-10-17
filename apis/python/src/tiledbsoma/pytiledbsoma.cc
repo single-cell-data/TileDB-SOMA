857dbf2c836c1a68784b2d95705fa93e1d3efb2f
Behnam Robatmili <brobatmili@chanzuckerberg.com>
2023-10-17 14:32:34 -0700

    C++ IntIndexer based on khash
    -It inclcudes khash files
    -It also includes a unitests trying different combinations of hash and lookup keys

--- apis/python/src/tiledbsoma/pytiledbsoma.cc
+++ apis/python/src/tiledbsoma/pytiledbsoma.cc
@@ -37,6 +37,7 @@
 #include <pybind11/stl_bind.h>
 
 #include <tiledbsoma/tiledbsoma>
+#include <reindexer.h>
 
 #include "query_condition.cc"
 
@@ -693,5 +694,69 @@ PYBIND11_MODULE(pytiledbsoma, m) {
         .def("get_enum_is_ordered", get_enum_is_ordered)
 
         .def("get_enum_label_on_attr", &SOMAArray::get_enum_label_on_attr);
+    // Efficient C++ re-indexing (aka hashing unique key values to an index
+    // between 0 and number of keys - 1) based on khash
+    py::class_<IntIndexer>(m, "IntIndexer")
+        .def(py::init<>())
+        .def(py::init([](py::array_t<int64_t> keys, int num_threads) {
+            auto buffer = keys.request();
+            int64_t* data = static_cast<int64_t*>(buffer.ptr);
+            size_t length = buffer.shape[0];
+            IntIndexer indexer(data, length, num_threads);
+            return indexer;
+        }))
+        .def(
+            "map_locations",
+            [](IntIndexer& indexer,
+               py::array_t<int64_t> keys,
+               int num_threads) {
+                auto buffer = keys.request();
+                int64_t* data = static_cast<int64_t*>(buffer.ptr);
+                size_t length = buffer.shape[0];
+                indexer.map_locations(keys.data(), keys.size(), num_threads);
+            })
+        .def(
+            "map_locations",
+            [](IntIndexer& indexer,
+               std::vector<int64_t> keys,
+               int num_threads) {
+                indexer.map_locations(keys.data(), keys.size(), num_threads);
+            })
+        // Perform lookup for a large input array of keys and return the looked
+        // up value array (passing ownership from C++ to python)
+        .def(
+            "get_indexer",
+            [](IntIndexer& indexer, py::array_t<int64_t> lookups) {
+                auto input_buffer = lookups.request();
+                int64_t* input_ptr = static_cast<int64_t*>(input_buffer.ptr);
+                size_t size = input_buffer.shape[0];
+                auto results = py::array_t<int64_t>(size);
+                auto results_buffer = results.request();
+                size_t results_size = results_buffer.shape[0];
+
+                int64_t* results_ptr = static_cast<int64_t*>(
+                    results_buffer.ptr);
+
+                indexer.lookup(input_ptr, results_ptr, size);
+                return results;
+            })
+        // Perform lookup for a large input array of keys and writes the looked
+        // up values into previously allocated array (works for the cases in
+        // which python and R pre-allocate the array)
+        .def(
+            "get_indexer",
+            [](IntIndexer& indexer,
+               py::array_t<int64_t> lookups,
+               py::array_t<int64_t>& results) {
+                auto input_buffer = lookups.request();
+                int64_t* input_ptr = static_cast<int64_t*>(input_buffer.ptr);
+                size_t size = input_buffer.shape[0];
+
+                auto results_buffer = results.request();
+                int64_t* results_ptr = static_cast<int64_t*>(
+                    results_buffer.ptr);
+                size_t results_size = input_buffer.shape[0];
+                indexer.lookup(input_ptr, input_ptr, size);
+            });
 }
 }  // namespace tiledbsoma
