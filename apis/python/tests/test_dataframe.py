0f0f8b235fd5ed9257b63e55d85050529161a1a0
Vivian Nguyen <vivian@tiledb.com>
2023-10-19 13:21:38 -0500

    Support All Enumeration Value Types in `to_arrow`

--- apis/python/tests/test_dataframe.py
+++ apis/python/tests/test_dataframe.py
@@ -830,6 +830,8 @@ def test_write_categorical_types(tmp_path):
         sdf.write(pa.Table.from_pandas(df))
 
     with soma.DataFrame.open(tmp_path.as_posix()) as sdf:
+        print(pa.Table.from_pandas(df))
+        print(sdf.read().concat())
         assert (df == sdf.read().concat().to_pandas()).all().all()
 
 
