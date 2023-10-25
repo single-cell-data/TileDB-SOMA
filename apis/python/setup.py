4c7bac4e77a261651180cea31e49f19910bb3469
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-10-26 00:53:29 +0300

    Reformat.

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -263,7 +263,8 @@ setuptools.setup(
             library_dirs=LIB_DIRS,
             libraries=["tiledbsoma", "tiledb"],
             extra_link_args=CXX_FLAGS,
-            extra_compile_args=["-std=c++17" if os.name != "nt" else "/std:c++17"] + CXX_FLAGS,
+            extra_compile_args=["-std=c++17" if os.name != "nt" else "/std:c++17"]
+            + CXX_FLAGS,
             language="c++",
         )
     ],
