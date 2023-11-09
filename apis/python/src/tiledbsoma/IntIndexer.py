5c135d8653b119627c5698a1635162365442e480
Behnam Robatmili <brobatmili@chanzuckerberg.com>
2023-11-09 09:30:34 -0800

    [C++] Workflow fix to skip external files for reindexer and format fixesr

--- apis/python/src/tiledbsoma/IntIndexer.py
+++ apis/python/src/tiledbsoma/IntIndexer.py
@@ -1,23 +1,26 @@
-from typing import Any
-
 import numpy as np
 import pandas as pd
 import tiledb
-import numpy.typing as npt
 
 from . import pytiledbsoma as clib
 from .options import SOMATileDBContext
 from .options._soma_tiledb_context import _validate_soma_tiledb_context
 
-'''External API'''
-class IntIndexer:
+"""External API"""
 
+
+class IntIndexer:
     @staticmethod
-    def map_locations(keys: np.ndarray[np.int64, Any]) -> clib.IntIndexer:
+    def map_locations(
+        keys: np.typing.NDArray[np.int64],
+    ) -> clib.IntIndexer:  # type ignore
         return indexer_map_locations(keys)
 
-'''For internal use'''
-def indexer_map_locations(keys: np.ndarray[np.int64, Any]) -> clib.IntIndexer:
+
+"""For internal use"""
+
+
+def indexer_map_locations(keys: np.typing.NDArray[np.int64]) -> clib.IntIndexer:
     if len(np.unique(keys)) != len(keys):
         raise pd.errors.InvalidIndexError(
             "Reindexing only valid with uniquely valued Index objects"
