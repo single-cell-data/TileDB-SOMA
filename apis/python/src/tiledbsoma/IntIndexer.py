1895e285576789230fb36d7bc39284e161ee79cc
Behnam Robatmili <brobatmili@chanzuckerberg.com>
2023-11-15 15:54:55 -0800

    Moving CIndexer factory to _experiments
    Merge issues

--- apis/python/src/tiledbsoma/IntIndexer.py
+++ apis/python/src/tiledbsoma/IntIndexer.py
@@ -1,10 +1,7 @@
 import numpy as np
-import pandas as pd
-import tiledb
 
 from . import pytiledbsoma as clib
-from .options import SOMATileDBContext
-from .options._soma_tiledb_context import _validate_soma_tiledb_context
+from ._experiment import _indexer_map_locations
 
 """External API"""
 
@@ -14,26 +11,7 @@ class IntIndexer:
     def map_locations(
         keys: np.typing.NDArray[np.int64],
     ) -> clib.IntIndexer:  # type ignore
-        return indexer_map_locations(keys)
+        return _indexer_map_locations(keys)
 
 
 """For internal use"""
-
-
-def indexer_map_locations(keys: np.typing.NDArray[np.int64]) -> clib.IntIndexer:
-    if len(np.unique(keys)) != len(keys):
-        raise pd.errors.InvalidIndexError(
-            "Reindexing only valid with uniquely valued Index objects"
-        )
-    context: SOMATileDBContext = _validate_soma_tiledb_context(
-        SOMATileDBContext(tiledb.default_ctx())
-    )
-    compute_concurrency: int = 5
-    if context._tiledb_ctx:
-        compute_concurrency = int(
-            int(context._tiledb_ctx.config()["sm.compute_concurrency_level"]) / 2
-        )
-    thread_counts = int(compute_concurrency)
-    reindexer = clib.IntIndexer()
-    reindexer.map_locations(keys, thread_counts)
-    return reindexer
--- libtiledbsoma/test/test_indexer.py
+++ libtiledbsoma/test/test_indexer.py
@@ -7,21 +7,37 @@ from tiledbsoma.IntIndexer import IntIndexer
 
 
 # 1d array to list
-def indexer_test(keys: np.array, lookups: np.array):
+
+
+def indexer_test(keys: np.array, lookups: np.array, fail: bool):
+    if fail:
+        indexer_test_fail(keys, lookups)
+    else:
+        indexer_test_pass(keys, lookups)
+
+
+def indexer_test_fail(keys: np.array, lookups: np.array):
     reindexing_exception: bool = False
     try:
         indexer = IntIndexer.map_locations(keys)
-        results = indexer.get_indexer(lookups)
+        indexer.get_indexer(lookups)
     except pd.errors.InvalidIndexError:
         reindexing_exception = True
 
     try:
         panda_indexer = pd.Index(keys)
-        panda_results = panda_indexer.get_indexer(lookups)
+        panda_indexer.get_indexer(lookups)
     except pd.errors.InvalidIndexError as exception:
         if reindexing_exception:
             return
         raise exception
+
+
+def indexer_test_pass(keys: np.array, lookups: np.array):
+    indexer = IntIndexer.map_locations(keys)
+    results = indexer.get_indexer(lookups)
+    panda_indexer = pd.Index(keys)
+    panda_results = panda_indexer.get_indexer(lookups)
     assert np.equal(results.all(), panda_results.all())
 
 
@@ -29,6 +45,7 @@ test_data = [
     {
         "keys": [-1, -1, -1, 0, 0, 0],
         "lookups": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+        "pass": False,
     },
     {
         "keys": [
@@ -58,6 +75,7 @@ test_data = [
             5,
         ],
         "lookups": [-10000, 1, 2, 3, 5, 6],
+        "pass": False,
     },
     {
         "keys": [-1, 1, 2, 3, 4, 5],
@@ -87,6 +105,7 @@ test_data = [
             4,
             5,
         ],
+        "pass": True,
     },
     {
         "keys": [-10000, -100000, 200000, 5, 1, 7],
@@ -116,6 +135,7 @@ test_data = [
             4,
             5,
         ],
+        "pass": True,
     },
     {
         "keys": [-10000, -200000, 1000, 3000, 1, 2],
@@ -145,18 +165,24 @@ test_data = [
             4,
             5,
         ],
+        "pass": True,
     },
-    {"keys": [i for i in range(1, 10000)], "lookups": [i for i in range(1, 10)]},
+    {
+        "keys": [i for i in range(1, 10000)],
+        "lookups": [i for i in range(1, 10)],
+        "pass": True,
+    },
     {
         "keys": [1, 2, 3, 4, 5, 2**63 - 1],
         "lookups": [i for i in range(2**63 - 1000, 2**63 - 1)],
+        "pass": True,
     },
 ]
 
 
 def main():
     for data in test_data:
-        indexer_test(data["keys"], data["lookups"])
+        indexer_test(data["keys"], data["lookups"], not data["pass"])
 
 
 if __name__ == "__main__":
