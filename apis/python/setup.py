284076c79ed5edd4bd78ecf1ffb839c624c1803f
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-12-04 13:04:08 -0800

    upgrade to somacore==1.0.6

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -283,7 +283,7 @@ setuptools.setup(
         "pyarrow>=9.0.0",
         "scanpy>=1.9.2",
         "scipy",
-        "somacore==1.0.4",
+        "somacore==1.0.6",
         "tiledb~=0.23.3",
         "typing-extensions",  # Note "-" even though `import typing_extensions`
     ],
