63e24853076db833056bd6410f6f60dc07bfd76c
Dirk Eddelbuettel <edd@debian.org>
2023-12-14 12:41:04 -0600

    Tweaking testing

--- apis/r/tests/testthat.R
+++ apis/r/tests/testthat.R
@@ -1,9 +1,3 @@
-if (Sys.info()[["sysname"]] == "Linux") {
-    Sys.setenv("TILEDB_SM_COMPUTE_CONCURRENCY_LEVEL"=1,
-               "TILEDB_SM_IO_CONCURRENCY_LEVEL"=1,
-               "OMP_THREAD_LIMIT"=1)
-}
-
 library(testthat)
 library(tiledbsoma)
 
