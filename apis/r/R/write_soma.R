b7a167220907a6819a2050189ccca382f03fd1b0
Paul Hoffman <mojaveazure@users.noreply.github.com>
2024-01-17 17:54:49 -0500

    merge

--- apis/r/R/write_soma.R
+++ apis/r/R/write_soma.R
@@ -54,6 +54,9 @@ NULL
 #' \code{\link{SOMADataFrame}}
 #' @param index_column_names Names of columns in \code{x} to index in the
 #' resulting SOMA object
+#' @param key Optionally register the resulting \code{SOMADataFrame} in
+#' \code{soma_parent} as \code{key}; pass \code{NULL} to prevent registration
+#' to handle manually
 #'
 #' @rdname write_soma_objects
 #'
@@ -88,6 +91,7 @@ write_soma.data.frame <- function(
   df_index = NULL,
   index_column_names = 'soma_joinid',
   ...,
+  key = NULL,
   platform_config = NULL,
   tiledbsoma_ctx = NULL,
   relative = TRUE
@@ -95,8 +99,10 @@ write_soma.data.frame <- function(
   stopifnot(
     "'x' must be named" = is_named(x, allow_empty = FALSE),
     "'df_index' must be a single character value" = is.null(df_index) ||
-      is_scalar_character(df_index),
-    "'index_column_names' must be a character vector" = is.character(index_column_names)
+      (is_scalar_character(df_index) && nzchar(key)),
+    "'index_column_names' must be a character vector" = is.character(index_column_names),
+    "'key' must be a single character value" = is.null(key) ||
+      (is_scalar_character(key) && nzchar(key))
   )
   # Create a proper URI
   uri <- .check_soma_uri(
@@ -104,6 +110,9 @@ write_soma.data.frame <- function(
     soma_parent = soma_parent,
     relative = relative
   )
+  if (is.character(key) && is.null(soma_parent)) {
+    stop("'soma_parent' must be a SOMACollection if 'key' is provided")
+  }
   # Clean up data types in `x`
   remove <- vector(mode = 'logical', length = ncol(x))
   for (i in seq_len(ncol(x))) {
@@ -163,8 +172,13 @@ write_soma.data.frame <- function(
     platform_config = platform_config,
     tiledbsoma_ctx = tiledbsoma_ctx
   )
-  # Write and return
+  # Write values
   sdf$write(tbl)
+  # Add to `soma_parent`
+  if (is.character(key)) {
+    soma_parent$set(sdf, name = key, relative = relative)
+  }
+  # Return
   return(sdf)
 }
 
@@ -194,6 +208,7 @@ write_soma.matrix <- function(
   type = NULL,
   transpose = FALSE,
   ...,
+  key = NULL,
   platform_config = NULL,
   tiledbsoma_ctx = NULL,
   relative = TRUE
@@ -201,7 +216,9 @@ write_soma.matrix <- function(
   stopifnot(
     "'sparse' must be a single logical value" = is_scalar_logical(sparse),
     "'type' must be an Arrow type" = is.null(type) || is_arrow_data_type(type),
-    "'transpose' must be a single logical value" = is_scalar_logical(transpose)
+    "'transpose' must be a single logical value" = is_scalar_logical(transpose),
+    "'key' must be a single character value" = is.null(key) ||
+      (is_scalar_character(key) && nzchar(key))
   )
   if (!isTRUE(sparse) && inherits(x = x, what = 'sparseMatrix')) {
     stop(
@@ -218,6 +235,7 @@ write_soma.matrix <- function(
       type = type,
       transpose = transpose,
       ...,
+      key = key,
       platform_config = platform_config,
       tiledbsoma_ctx = tiledbsoma_ctx,
       relative = relative
@@ -233,6 +251,9 @@ write_soma.matrix <- function(
     soma_parent = soma_parent,
     relative = relative
   )
+  if (is.character(key) && is.null(soma_parent)) {
+    stop("'soma_parent' must be a SOMACollection if 'key' is provided")
+  }
   # Transpose the matrix
   if (isTRUE(transpose)) {
     x <- t(x)
@@ -245,8 +266,13 @@ write_soma.matrix <- function(
     platform_config = platform_config,
     tiledbsoma_ctx = tiledbsoma_ctx
   )
-  # Write and return
+  # Write values
   array$write(x)
+  # Add to `soma_parent`
+  if (is.character(key)) {
+    soma_parent$set(array, name = key, relative = relative)
+  }
+  # Return
   return(array)
 }
 
@@ -284,6 +310,7 @@ write_soma.TsparseMatrix <- function(
   type = NULL,
   transpose = FALSE,
   ...,
+  key = NULL,
   platform_config = NULL,
   tiledbsoma_ctx = NULL,
   relative = TRUE
@@ -293,7 +320,9 @@ write_soma.TsparseMatrix <- function(
     "'x' must not be a pattern matrix" = !inherits(x = x, what = 'nsparseMatrix'),
     "'type' must be an Arrow type" = is.null(type) ||
       (R6::is.R6(type) && inherits(x = type, what = 'DataType')),
-    "'transpose' must be a single logical value" = is_scalar_logical(transpose)
+    "'transpose' must be a single logical value" = is_scalar_logical(transpose),
+    "'key' must be a single character value" = is.null(key) ||
+      (is_scalar_character(key) && nzchar(key))
   )
   # Create a proper URI
   uri <- .check_soma_uri(
@@ -301,6 +330,9 @@ write_soma.TsparseMatrix <- function(
     soma_parent = soma_parent,
     relative = relative
   )
+  if (is.character(key) && is.null(soma_parent)) {
+    stop("'soma_parent' must be a SOMACollection if 'key' is provided")
+  }
   # Transpose the matrix
   if (isTRUE(transpose)) {
     x <- Matrix::t(x)
@@ -313,8 +345,13 @@ write_soma.TsparseMatrix <- function(
     platform_config = platform_config,
     tiledbsoma_ctx = tiledbsoma_ctx
   )
-  # Write and return
+  # Write values
   array$write(x)
+  # Add to `soma_parent`
+  if (is.character(key)) {
+    soma_parent$set(array, name = key, relative = relative)
+  }
+  # Return
   return(array)
 }
 
