c17f145fd153a82cfe0f48380210f54430cbd61a
Paul Hoffman <paul.hoffman@tiledb.com>
2024-01-18 17:34:45 -0500

    [r] Support Outgestion of `SeuratCommands` Add support for outgestion of `SeuratCommand` logs. Functionality is provided by modified `SOMAExperimentAxisQuery$to_seurat()`
    Modified SOMA methods:
    - `SOMAExperimentAxisQuery$to_seurat()`: checks to see if `uns` is present; if so, ingests command logs
    
    Notes:
    - this PR is a smaller redux of #1944 and #1966
    - this PR does not implement ingestion of `SeuratCommand` logs; ingestion is handled by #2023

--- apis/r/R/utils-seurat.R
+++ apis/r/R/utils-seurat.R
@@ -19,6 +19,71 @@
   ))
 }
 
+#' @importFrom methods getClassDef slotNames
+#'
+.load_seurat_command <- function(uns, ms_names) {
+  key <- 'seurat_commands'
+  check_package('jsonlite')
+  check_package('SeuratObject', version = .MINIMUM_SEURAT_VERSION())
+  stopifnot(
+    "'uns' must be a SOMACollection" = inherits(uns, what = 'SOMACollection'),
+    "Cannot find a SOMACollection with command logs in 'uns'" = key %in% uns$names() &&
+      inherits(logs <- uns$get(key), what = 'SOMACollection'),
+    "'ms_names' must be a character vector with no empty strings" = is.character(ms_names) &&
+      all(nzchar(ms_names))
+  )
+  slots <- slotNames(getClassDef('SeuratCommand', package = 'SeuratObject'))
+  hint <- uns_hint('1d')
+  lognames <- logs$names()
+  commands <- setNames(vector('list', length = length(lognames)), lognames)
+  for (x in lognames) {
+    spdl::info("Attempting to read command log {}", x)
+    xdf <- logs$get(x)
+    if (!inherits(xdf, 'SOMADataFrame')) {
+      spdl::warn("Log {} is invalid: not a SOMADataFrame", x)
+      next
+    }
+    xhint <- tryCatch(xdf$get_metadata(names(hint)), error = function(...) '')
+    if (xhint != hint[[1L]]) {
+      spdl::warn("Log {} is invalid: not a one-dimensional character data frame")
+      next
+    }
+    spdl::info("Reading in and decoding command log")
+    tbl <- xdf$read(column_names = 'values')$concat()
+    enc <- as.data.frame(tbl)[['values']]
+    cmdlist <- jsonlite::fromJSON(enc)
+    if (!(is.null(cmdlist$assay.used) || cmdlist$assay.used %in% ms_names)) {
+      spdl::info("Skipping command log {}: assay used not requested", x)
+      next
+    }
+    spdl::info("Decoding command log parameters")
+    for (param in names(cmdlist)) {
+      cmdlist[[param]] <- if (param == 'time.stamp') {
+        ts <- sapply(
+          jsonlite::fromJSON(cmdlist[[param]]),
+          FUN = function(dt) tryCatch(.decode_from_char(dt), error = function(...) dt),
+          simplify = FALSE,
+          USE.NAMES = TRUE
+        )
+        class(ts) <- c('POSIXlt', 'POSIXt')
+        as.POSIXct(ts)
+      } else if (is.character(cmdlist[[param]])) {
+        .decode_from_char(cmdlist[[param]])
+      } else {
+        cmdlist[[param]]
+      }
+    }
+    spdl::info("Assembling command log")
+    params <- cmdlist[setdiff(names(cmdlist), slots)]
+    cmdlist <- c(cmdlist[setdiff(names(cmdlist), names(params))], list(params = params))
+    commands[[x]] <- do.call(new, c(cmdlist, Class = 'SeuratCommand'))
+  }
+  commands <- Filter(Negate(is.null), x = commands)
+  spdl::info("Returning {} command log(s)", length(commands))
+  idx <- order(sapply(commands, methods::slot, name = 'time.stamp'))
+  return(commands[idx])
+}
+
 .MINIMUM_SEURAT_VERSION <- function(repr = c('v', 'c')) {
   repr <- repr[1L]
   repr <- match.arg(arg = repr)
