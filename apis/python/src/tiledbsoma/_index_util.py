ebaf35a9cdad0130123ab01b52666b0683fd5574
Behnam Robatmili <brobatmili@chanzuckerberg.com>
2024-01-29 10:04:53 -0800

    [python] Reindexer external python API

--- apis/python/src/tiledbsoma/_index_util.py
+++ apis/python/src/tiledbsoma/_index_util.py
@@ -11,7 +11,7 @@ from tiledbsoma import pytiledbsoma as clib
 from .options import SOMATileDBContext
 
 
-def build_index(
+def build_index_tiledb_context(
     keys: np.typing.NDArray[np.int64], context: SOMATileDBContext
 ) -> clib.IntIndexer:
     """Builds an indexer object compatible with :meth:`pd.Index.get_indexer`."""
@@ -23,6 +23,19 @@ def build_index(
         context.tiledb_ctx.config().get("sm.compute_concurrency_level", 10)
     )
     thread_count = tdb_concurrency // 2
+    reindexer = clib.IntIndexer()
+    reindexer.map_locations(keys, thread_count)
+    return reindexer
+
+
+def build_index(
+    keys: np.typing.NDArray[np.int64], thread_count: int = 4
+) -> clib.IntIndexer:
+    """Builds an indexer object compatible with :meth:`pd.Index.get_indexer`."""
+    if len(np.unique(keys)) != len(keys):
+        raise pd.errors.InvalidIndexError(
+            "Reindexing only valid with uniquely valued Index objects"
+        )
     reindexer = clib.IntIndexer()
     reindexer.map_locations(keys, thread_count)
     return reindexer
