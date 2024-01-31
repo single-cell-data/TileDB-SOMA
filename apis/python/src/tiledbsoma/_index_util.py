26c95346d0b2c3485beb54a830e6d22c76e81b83
John Kerl <kerl.john.r@gmail.com>
2024-01-31 09:06:57 -0500

    fix issues

--- apis/python/src/tiledbsoma/_index_util.py
+++ apis/python/src/tiledbsoma/_index_util.py
@@ -3,6 +3,8 @@ This file is separate from _util.py, due to a circular-import issue with
 SOMATileDBContext which would otherwise ensue.
 """
 
+from typing import Optional
+
 import numpy as np
 import pandas as pd
 
@@ -11,17 +13,20 @@ from tiledbsoma import pytiledbsoma as clib
 from .options import SOMATileDBContext
 
 
-def build_index(
-    keys: np.typing.NDArray[np.int64], context: SOMATileDBContext
+def tiledbsoma_build_index(
+    keys: np.typing.NDArray[np.int64],
+    context: Optional[SOMATileDBContext],
 ) -> clib.IntIndexer:
     """Builds an indexer object compatible with :meth:`pd.Index.get_indexer`."""
     if len(np.unique(keys)) != len(keys):
         raise pd.errors.InvalidIndexError(
             "Reindexing only valid with uniquely valued Index objects"
         )
-    tdb_concurrency = int(
-        context.tiledb_ctx.config().get("sm.compute_concurrency_level", 10)
-    )
+    tdb_concurrency = 10
+    if context is not None:
+        tdb_concurrency = int(
+            context.tiledb_ctx.config().get("sm.compute_concurrency_level", 10)
+        )
     thread_count = tdb_concurrency // 2
     reindexer = clib.IntIndexer()
     reindexer.map_locations(keys, thread_count)
