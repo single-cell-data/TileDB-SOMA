fae2ac1ed80e9ea641c1b13edc11177826be804c
Bruce Martin <bruce@chanzuckerberg.com>
2024-02-12 08:16:36 -0800

    relax numba dependency (#2122)

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -281,9 +281,9 @@ setuptools.setup(
         # Tracked in https://github.com/single-cell-data/TileDB-SOMA/issues/1785
         "anndata != 0.10.0; python_version>='3.8'",
         "attrs>=22.2",
-        "numba~=0.58.0; python_version>='3.8'",
+        "numba>=0.58.0; python_version>='3.8'",
         # Older numba version needed for Python3.7.
-        # This older numba version was also incompatble with newer numpy
+        # This older numba version was also incompatible with newer numpy
         # versions, and the old pip solver (<=2020) needed us to explicate
         # that constraint here (issue #1051).
         "numba==0.56.4; python_version<'3.8'",
