ce62bbcab47b9f79149d460261cfc9f7ec3c14b3
Behnam Robatmili <brobatmili@chanzuckerberg.com>
2024-02-21 10:42:30 -0800

    [C++, python] Optimizing indexer for panda create and pyarrow lookup -Removed std::vector based lookup function to speedup panda's lookup -Add a speiclized lookup for pyarrow
    Signed-off-by: Behnam Robatmili <brobatmili@chanzuckerberg.com>

--- apis/python/src/tiledbsoma/reindexer.cc
+++ apis/python/src/tiledbsoma/reindexer.cc
@@ -31,7 +31,7 @@
  */
 
 #include <tiledbsoma/reindexer/reindexer.h>
-
+#include <tiledbsoma/utils/carrow.h>
 #include "common.h"
 
 #define DENUM(x) .value(#x, TILEDB_##x)
@@ -40,6 +40,95 @@ namespace libtiledbsomacpp {
 namespace py = pybind11;
 using namespace py::literals;
 using namespace tiledbsoma;
+
+/***
+ * Handle general lookup for Re-indexer
+ * @param indexer reference to the indexer
+ * @param lookups input values to be looked up
+ * @return looked up values
+ */
+py::array_t<int64_t> get_indexer_general(
+    IntIndexer& indexer, py::array_t<int64_t> lookups) {
+    auto input_buffer = lookups.request();
+    int64_t* input_ptr = static_cast<int64_t*>(input_buffer.ptr);
+    size_t size = input_buffer.shape[0];
+    auto results = py::array_t<int64_t>(size);
+    auto results_buffer = results.request();
+    size_t results_size = results_buffer.shape[0];
+    int64_t* results_ptr = static_cast<int64_t*>(results_buffer.ptr);
+    indexer.lookup(input_ptr, results_ptr, size);
+    return results;
+}
+
+/***
+ * Helper function to provide data and schema for an arrow object
+ * @param object python object
+ * @param arrow_array extracted array data
+ * @param arrow_schema extracted array schema
+ */
+//
+void extract_py_array_schema(
+    const pybind11::handle object,
+    ArrowArray& arrow_array,
+    ArrowSchema& arrow_schema) {
+    uintptr_t arrow_schema_ptr = (uintptr_t)(&arrow_schema);
+    uintptr_t arrow_array_ptr = (uintptr_t)(&arrow_array);
+
+    // Using array._export_to_c to get arrow array and schema.
+    object.attr("_export_to_c")(arrow_array_ptr, arrow_schema_ptr);
+}
+
+/***
+ * Handle pyarrow-based lookup for Re-indexer
+ * @param indexer reference to the indexer
+ * @py_arrow_array pyarrow inputs to be looked up
+ * @return looked up values
+ */
+py::array_t<int64_t> get_indexer_py_arrow(
+    IntIndexer& indexer, py::object py_arrow_array) {
+    // Check if it is not a pyarrow array or pyarrow chunked array
+    if (!py::hasattr(py_arrow_array, "_export_to_c") &&
+        !py::hasattr(py_arrow_array, "chunks") &&
+        !py::hasattr(py_arrow_array, "combine_chunks")) {
+        // Handle the general case (no py arrow objects)
+        return get_indexer_general(indexer, py_arrow_array);
+    }
+
+    py::list array_chunks;
+    if (py::hasattr(py_arrow_array, "chunks")) {
+        array_chunks = py_arrow_array.attr("chunks").cast<py::list>();
+    } else {
+        array_chunks.append(py_arrow_array);
+    }
+
+    // Calculate the total size of the input chunked array.
+    int total_size = 0;
+    for (const pybind11::handle array : array_chunks) {
+        ArrowSchema arrow_schema;
+        ArrowArray arrow_array;
+        extract_py_array_schema(array, arrow_array, arrow_schema);
+        total_size += arrow_array.length;
+    }
+
+    // Allocate the output
+    auto results = py::array_t<int64_t>(total_size);
+    auto results_buffer = results.request();
+    size_t results_size = results_buffer.shape[0];
+    int64_t* results_ptr = static_cast<int64_t*>(results_buffer.ptr);
+
+    // Write output (one chunk at a time)
+    int write_offset = 0;
+    for (const pybind11::handle array : array_chunks) {
+        ArrowSchema arrow_schema;
+        ArrowArray arrow_array;
+        extract_py_array_schema(array, arrow_array, arrow_schema);
+        auto input_ptr = (int64_t*)arrow_array.buffers[1];
+        indexer.lookup(
+            input_ptr, results_ptr + write_offset, arrow_array.length);
+        write_offset += arrow_array.length;
+    }
+    return results;
+}
 
 void load_reindexer(py::module& m) {
     // Efficient C++ re-indexing (aka hashing unique key values to an index
@@ -57,48 +146,19 @@ void load_reindexer(py::module& m) {
                 size_t length = buffer.shape[0];
                 indexer.map_locations(keys.data(), keys.size(), num_threads);
             })
-        .def(
-            "map_locations",
-            [](IntIndexer& indexer,
-               std::vector<int64_t> keys,
-               int num_threads) {
-                indexer.map_locations(keys.data(), keys.size(), num_threads);
-            })
-        // Perform lookup for a large input array of keys and return the looked
-        // up value array (passing ownership from C++ to python)
+        // Perform lookup for a large input array of keys and writes the
+        // looked up values into previously allocated array (works for the
+        // cases in which python and R pre-allocate the array)
         .def(
             "get_indexer",
             [](IntIndexer& indexer, py::array_t<int64_t> lookups) {
-                auto input_buffer = lookups.request();
-                int64_t* input_ptr = static_cast<int64_t*>(input_buffer.ptr);
-                size_t size = input_buffer.shape[0];
-                auto results = py::array_t<int64_t>(size);
-                auto results_buffer = results.request();
-                size_t results_size = results_buffer.shape[0];
-
-                int64_t* results_ptr = static_cast<int64_t*>(
-                    results_buffer.ptr);
-
-                indexer.lookup(input_ptr, results_ptr, size);
-                return results;
+                return get_indexer_general(indexer, lookups);
             })
-        // Perform lookup for a large input array of keys and writes the looked
-        // up values into previously allocated array (works for the cases in
-        // which python and R pre-allocate the array)
-        .def(
-            "get_indexer",
-            [](IntIndexer& indexer,
-               py::array_t<int64_t> lookups,
-               py::array_t<int64_t>& results) {
-                auto input_buffer = lookups.request();
-                int64_t* input_ptr = static_cast<int64_t*>(input_buffer.ptr);
-                size_t size = input_buffer.shape[0];
-
-                auto results_buffer = results.request();
-                int64_t* results_ptr = static_cast<int64_t*>(
-                    results_buffer.ptr);
-                size_t results_size = input_buffer.shape[0];
-                indexer.lookup(input_ptr, input_ptr, size);
-            });
+        // If the input is not arrow (does not have _export_to_c attribute),
+        // it will be handled using a general input method.
+        .def("get_indexer", [](IntIndexer& indexer, py::object py_arrow_array) {
+            return get_indexer_py_arrow(indexer, py_arrow_array);
+        });
 }
+
 }  // namespace libtiledbsomacpp
