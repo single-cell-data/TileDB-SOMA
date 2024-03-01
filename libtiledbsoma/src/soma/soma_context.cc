a8da3a8a735307a730a8384d6a7e5ca1030a7f73
beroy <beroy2000@gmail.com>
2024-02-29 21:57:10 -0800

    [python, C++] Re-indexer context thread pool (#2148)
    Each C++ SOMAContext has it's own lazily created thread pool

---
+++ libtiledbsoma/src/soma/soma_context.cc
@@ -0,0 +1,49 @@
+/**
+ * @file   soma_context.h
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2024 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ *   This file defines the SOMAContext class.
+ */
+#include "soma_context.h"
+#include <thread_pool/thread_pool.h>
+
+namespace tiledbsoma {
+
+void SOMAContext::create_thread_pool() {
+    // Extracting concurrency from tiledb config
+    auto cfg = tiledb_config();
+    int concurrency = 10;
+    if (cfg.find("sm.compute_concurrency_level") != cfg.end()) {
+        concurrency = std::stoi(cfg["sm.compute_concurrency_level"]);
+    }
+    int thread_count = std::max(1, concurrency / 2);
+    if (thread_count > 1) {
+        thread_pool_ = std::make_shared<ThreadPool>(thread_count);
+    }
+}
+}  // namespace tiledbsoma
