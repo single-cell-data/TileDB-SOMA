f3166474d7931d2c6ce5b2b60922afd5d2786969
Vivian Nguyen <vivian@tiledb.com>
2024-03-07 23:01:37 -0700

    WIP only extend enmr when present

--- apis/python/src/tiledbsoma/soma_array.cc
+++ apis/python/src/tiledbsoma/soma_array.cc
@@ -67,23 +67,25 @@ void write(SOMAArray& array, py::handle py_batch) {
             auto enmr_name = AttributeExperimental::get_enumeration_name(
                 *array.ctx()->tiledb_ctx(), attributes.at(sch_->name));
 
-            auto dict = arr_->dictionary;
-            if (enmr_name.has_value() && !dict) {
-                array.clear_column_data();
-                throw py::value_error(
-                    "Saw non-dictionary column passed to enumerated type");
-            }
+            if(enmr_name.has_value()){
+                auto dict = arr_->dictionary;
+                if (!dict) {
+                    array.clear_column_data();
+                    throw py::value_error(
+                        "Saw non-dictionary column passed to enumerated type");
+                }
 
-            const void* enmr_data;
-            uint64_t* enmr_offsets = nullptr;
-            if (dict->n_buffers == 3) {
-                enmr_offsets = (uint64_t*)dict->buffers[1];
-                enmr_data = dict->buffers[2];
-            } else {
-                enmr_data = dict->buffers[1];
+                const void* enmr_data;
+                uint64_t* enmr_offsets = nullptr;
+                if (dict->n_buffers == 3) {
+                    enmr_offsets = (uint64_t*)dict->buffers[1];
+                    enmr_data = dict->buffers[2];
+                } else {
+                    enmr_data = dict->buffers[1];
+                }
+                array.extend_enumeration(
+                    sch_->name, dict->length, enmr_data, enmr_offsets);
             }
-            array.extend_enumeration(
-                sch_->name, dict->length, enmr_data, enmr_offsets);
         }
 
         array.set_column_data(
