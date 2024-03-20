a0875894e6f91b94e0db32c5b9932ba231808f0e
Ryan Williams <ryan.williams@tiledb.com>
2024-03-20 15:37:46 -0400

    make `_FSPathWrapper` subclass `pathlib.Path`

--- apis/python/src/tiledbsoma/io/_util.py
+++ apis/python/src/tiledbsoma/io/_util.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2021-2024 TileDB, Inc.
 #
 # Licensed under the MIT License.
+import pathlib
 from contextlib import contextmanager
 from typing import (
     ContextManager,
@@ -37,8 +38,7 @@ def read_h5ad(
 # This trick lets us ingest H5AD with "r" (backed mode) from S3 URIs.  While h5ad
 # supports any file-like object, AnnData specifically wants only an `os.PathLike`
 # object. The only thing it does with the PathLike is to use it to get the filename.
-# @typeguard_ignore
-class _FSPathWrapper:
+class _FSPathWrapper(pathlib.Path):
     """Tricks anndata into thinking a file-like object is an ``os.PathLike``.
 
     While h5ad supports any file-like object, anndata specifically wants
@@ -50,12 +50,19 @@ class _FSPathWrapper:
     so here we just proxy all attributes except ``__fspath__``.
     """
 
+    def __new__(cls, _obj: object, path: Path) -> "_FSPathWrapper":
+        return super().__new__(cls, path)
+
+    # ``pathlib.Path`` construction references this attribute (``PosixFlavour`` or ``WindowsFlavour``)
+    _flavour = pathlib.Path().__class__._flavour  # type: ignore[attr-defined]
+
     def __init__(self, obj: object, path: Path) -> None:
+        super().__init__()
         self._obj = obj
         self._path = path
 
-    def __fspath__(self) -> Path:
-        return self._path
+    def __fspath__(self) -> str:
+        return self._path if isinstance(self._path, str) else str(self._path)
 
     def __getattr__(self, name: str) -> object:
         return getattr(self._obj, name)
