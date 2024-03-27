9e7756d22f3fb322d86272bd200b184d6caa915a
Julia Dark <julia@tiledb.com>
2024-03-27 17:15:46 -0400

    Add experimental ingestor for Visium data

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -348,6 +348,7 @@ setuptools.setup(
     ],
     extras_require={
         "dev": open("requirements_dev.txt").read(),
+        "spatial": ["tifffile", "pillow"],
     },
     python_requires=">=3.8",
     cmdclass={"build_ext": build_ext, "bdist_wheel": bdist_wheel},
