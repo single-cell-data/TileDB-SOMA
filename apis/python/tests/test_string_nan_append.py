7345cc25f7879959a671b210107110bc04b6cb6d
Ryan Williams <ryan.williams@tiledb.com>
2024-04-01 15:15:19 -0400

    test_string_nan_append: {3k,small} x {rename obs vs. not}

--- apis/python/tests/test_string_nan_append.py
+++ apis/python/tests/test_string_nan_append.py
@@ -1,3 +1,4 @@
+import pytest
 import scanpy as sc
 import numpy as np
 import tiledbsoma
@@ -5,20 +6,7 @@ import tiledbsoma.io
 import tempfile
 
 
-def test_string_nan_append(adata):
-    adat1 = adata
-    adat1.obsm = None
-    adat1.varm = None
-    adat1.obsp = None
-    adat1.varp = None
-
-    # Add empty column to obs
-    adat1.obs['batch_id'] = np.nan
-    adat1.obs['batch_id'] = adat1.obs['batch_id'].astype('string')
-
-    # Create a copy of the anndata object
-    adat2 = adat1.copy()
-
+def write_adatats(adat1, adat2):
     # Initial ingest
     SOMA_URI = tempfile.mkdtemp(prefix="soma-exp-")
     tiledbsoma.io.from_anndata(
@@ -43,3 +31,40 @@ def test_string_nan_append(adata):
         measurement_name="RNA",
         registration_mapping=rd
     )
+
+
+@pytest.mark.parametrize("rename_obs_index", [False, True])
+def test_string_nan_append_small(adata, rename_obs_index):
+    adat1 = adata
+    adat1.obsm = None
+    adat1.varm = None
+    adat1.obsp = None
+    adat1.varp = None
+    adat1.uns = dict()
+
+    # Add empty column to obs
+    adat1.obs['batch_id'] = np.nan
+    adat1.obs['batch_id'] = adat1.obs['batch_id'].astype('string')
+
+    # Create a copy of the anndata object
+    adat2 = adat1.copy()
+    if rename_obs_index:
+        adat2.obs.index = adat1.obs.index + "-2"
+
+    write_adatats(adat1, adat2)
+
+
+@pytest.mark.parametrize("rename_obs_index", [False, True])
+def test_string_nan_append_3k(rename_obs_index):
+    adat1 = sc.datasets.pbmc3k()
+
+    # Add empty column to obs
+    adat1.obs['batch_id'] = np.nan
+    adat1.obs['batch_id'] = adat1.obs['batch_id'].astype('string')
+
+    # Create a copy of the anndata object
+    adat2 = adat1.copy()
+    if rename_obs_index:
+        adat2.obs.index = adat1.obs.index.str.replace("1", "2")
+
+    write_adatats(adat1, adat2)
