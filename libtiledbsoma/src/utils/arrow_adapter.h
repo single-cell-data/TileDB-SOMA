c98a3963b6ded2d7ec668f23aabb430c6be6310b
Dirk Eddelbuettel <edd@debian.org>
2024-04-02 15:47:57 -0500

    Remove carrow.h and reference to it

--- libtiledbsoma/src/utils/arrow_adapter.h
+++ libtiledbsoma/src/utils/arrow_adapter.h
@@ -9,9 +9,6 @@
 // https://arrow.apache.org/docs/format/CDataInterface.html#exporting-a-simple-int32-array
 
 #include "nanoarrow.hpp"
-// #ifndef ARROW_SCHEMA_AND_ARRAY_DEFINED
-// #include "carrow.h"
-// #endif
 
 namespace tiledbsoma {
 
