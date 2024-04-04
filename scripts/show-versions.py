16272e710cef251a60a6fcf5c56ae2f85fb39fe5
John Kerl <kerl.john.r@gmail.com>
2024-04-04 14:15:56 -0400

    [python] Respect import-ordering

--- scripts/show-versions.py
+++ scripts/show-versions.py
@@ -8,18 +8,28 @@ import pandas as pd
 import pyarrow as pa
 import scanpy as sc
 import scipy as sp
-import tiledb
 
 import tiledbsoma
 
-print("tiledbsoma.__version__   ", tiledbsoma.__version__)
-print("tiledb.version()         ", ".".join(str(e) for e in tiledb.version()))
-print("core version             ", ".".join(map(str, tiledb.libtiledb.version())))
-print("anndata.__version__  (ad)", ad.__version__)
-print("numpy.__version__    (np)", np.__version__)
-print("pandas.__version__   (pd)", pd.__version__)
-print("pyarrow.__version__  (pa)", pa.__version__)
-print("scanpy.__version__   (sc)", sc.__version__)
-print("scipy.__version__    (sp)", sp.__version__)
-v = sys.version_info
-print("python__version__        ", ".".join(map(str, (v.major, v.minor, v.micro))))
+
+def main():
+    # The pre-commit-hook linter insists on moving import tiledb before import
+    # tiledbsoma but we want it after
+    # https://github.com/single-cell-data/TileDB-SOMA/issues/2293
+    import tiledb
+
+    print("tiledbsoma.__version__   ", tiledbsoma.__version__)
+    print("tiledb.version()         ", ".".join(str(e) for e in tiledb.version()))
+    print("core version             ", ".".join(map(str, tiledb.libtiledb.version())))
+    print("anndata.__version__  (ad)", ad.__version__)
+    print("numpy.__version__    (np)", np.__version__)
+    print("pandas.__version__   (pd)", pd.__version__)
+    print("pyarrow.__version__  (pa)", pa.__version__)
+    print("scanpy.__version__   (sc)", sc.__version__)
+    print("scipy.__version__    (sp)", sp.__version__)
+    v = sys.version_info
+    print("python__version__        ", ".".join(map(str, (v.major, v.minor, v.micro))))
+
+
+if __name__ == "__main__":
+    main()
