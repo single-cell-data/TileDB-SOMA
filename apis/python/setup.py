dfd48db0b7f4657bf7cf498ddf03e07ea3f00139
Dirk Eddelbuettel <edd@debian.org>
2024-04-04 13:59:06 -0500

    Adjusting Python too

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -217,6 +217,7 @@ class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
 INC_DIRS = [
     "dist_links/libtiledbsoma/include",
     "dist_links/libtiledbsoma/external/include",
+    "dist_links/libtiledbsoma/external/include/tiledbsoma",
     "../../build/externals/install/include",
     str(tiledbsoma_dir / "include"),
     str(tiledbsoma_dir.parent / "build/externals/install/include"),
