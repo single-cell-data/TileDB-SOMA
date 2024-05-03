53dbe240b7d494d439e2fb6003bf91744f4ff6b3
Julia Dark <julia@tiledb.com>
2024-05-03 17:41:03 -0400

    Add coordinate transform classes

--- apis/python/src/tiledbsoma/_scene.py
+++ apis/python/src/tiledbsoma/_scene.py
@@ -4,12 +4,12 @@
 
 """Implementation of a SOMA Scene."""
 
+from typing import MutableMapping, Union
 
-from typing import Union
-
-from somacore import scene
+from somacore import coordinates, scene
 
 from ._collection import Collection, CollectionBase
+from ._coordinates import CompositeTransform, CoordinateSystem
 from ._dataframe import DataFrame
 from ._dense_nd_array import DenseNDArray
 from ._sparse_nd_array import SparseNDArray
@@ -40,3 +40,24 @@ class Scene(  # type: ignore[misc]  # __eq__ false positive
         "obssm": ("SOMACollection",),
         "varsm": ("SOMACollection",),
     }
+
+    @property
+    def local_coordinate_system(self) -> CoordinateSystem:
+        """Coordinate system for this scene."""
+        # TODO: Metadata loading should be moved to open/reopen methods.
+        coords = self.metadata.get("_soma_local_coordinates")
+        if coords is None:
+            return CoordinateSystem([])
+        coord_sys = CoordinateSystem.from_json(coords)
+        return coord_sys
+
+    @property
+    def transformations(self) -> MutableMapping[str, coordinates.CoordinateTransform]:
+        """Transformations saved for this scene."""
+        # TODO: Metadata loading should be moved to open/reopen methods.
+        prefix = "_soma_transform_"
+        return {
+            key[len(prefix) :]: CompositeTransform.from_json(val)
+            for key, val in self.metadata.items()
+            if key.startswith(prefix)
+        }
