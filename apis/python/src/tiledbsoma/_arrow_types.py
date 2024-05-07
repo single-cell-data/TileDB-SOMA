51f60de3bb20f19259642d0519eec71fa7f0ac8f
John Kerl <kerl.john.r@gmail.com>
2024-05-07 12:14:56 -0400

    [python] Fix `failed to convert buffer` on `update_obs`/`update_var`

--- apis/python/src/tiledbsoma/_arrow_types.py
+++ apis/python/src/tiledbsoma/_arrow_types.py
@@ -248,6 +248,25 @@ def df_to_arrow(df: pd.DataFrame) -> pa.Table:
                 values=column, categories=categories, ordered=ordered
             )
 
+    # Always make string columns nullable. Context:
+    # * df_to_arrow is _solely_ for use of tiledbsoma.io
+    #   o Anyone calling the SOMA API directly has user-provided Arrow schema which
+    #     must be respected
+    #   o Anyone calling tiledbsoma.io -- including from_h5ad/from_anndata, and
+    #     update_obs/update_var -- does not provide an Arrow schema explicitly.
+    #     We compute an Arrow schema for them here.
+    # * Even when the _initial_ data is all non-null down a particular string
+    #   column, there are two ways a _subsequent_ write can provide nulls:
+    #   append-mode ingest, or, update_obs/update_var wherein the new data has
+    #   nulls even when the data used at schema-create time was non-null.
+    # * We have no way of knowing at initial ingest time whether or not users
+    #   will later be appending, or updating, with null data.
+    # * Note that Arrow has a per-field nullable flag in its schema metadata
+    #   -- and so do TileDB array schemas.
+    for key in df:
+        if df[key].dtype.name == "object":
+            nullable_fields.add(key)
+
     arrow_table = pa.Table.from_pandas(df)
 
     if nullable_fields:
