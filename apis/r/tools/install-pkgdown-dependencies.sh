f5ac4303cfcae47f29e17a5ecf034419eee4e82a
John Kerl <kerl.john.r@gmail.com>
2024-06-10 12:05:54 -0400

    [r/ci] Unbreak `pkgdown` CI [WIP]

--- apis/r/tools/install-pkgdown-dependencies.sh
+++ apis/r/tools/install-pkgdown-dependencies.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 export R_REMOTES_NO_ERRORS_FROM_WARNINGS='true'
 Rscript -e 'install.packages(c("remotes", "pkgdown"))'
 cd apis/r
-Rscript -e 'remotes::install_deps(dependencies = TRUE)'
+Rscript -e 'remotes::install_deps(dependencies = TRUE, upgrade=FALSE)'
 Rscript -e 'remotes::install_local()'
 cd ../..
