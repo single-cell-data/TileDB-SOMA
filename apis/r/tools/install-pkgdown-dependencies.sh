439842feea7c1a17977736fb6ecb645c0ee7b3c6
John Kerl <kerl.john.r@gmail.com>
2024-06-11 12:16:52 -0400

    [r/ci] Unbreak `pkgdown` CI (#2703)
    * [r/ci] Unbreak `pkgdown` CI [WIP]
    
    * feedback from paul h

--- apis/r/tools/install-pkgdown-dependencies.sh
+++ apis/r/tools/install-pkgdown-dependencies.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 export R_REMOTES_NO_ERRORS_FROM_WARNINGS='true'
 Rscript -e 'install.packages(c("remotes", "pkgdown"))'
 cd apis/r
-Rscript -e 'remotes::install_deps(dependencies = TRUE)'
-Rscript -e 'remotes::install_local()'
+Rscript -e 'remotes::install_deps(dependencies = TRUE, upgrade=FALSE)'
+# Need dependencies = FALSE to avoid getting latest/sometimes-too-new r-tiledb from CRAN when we
+# really want it from our r-universe where we control the version we want:
+Rscript -e 'remotes::install_local(dependencies = FALSE)'
 cd ../..
