15de0abad62cb4daf6ec8f92a0e64df1c817acc1
John Kerl <kerl.john.r@gmail.com>
2024-06-11 12:16:29 -0400

    feedback from paul h

--- apis/r/tools/install-pkgdown-dependencies.sh
+++ apis/r/tools/install-pkgdown-dependencies.sh
@@ -5,5 +5,7 @@ export R_REMOTES_NO_ERRORS_FROM_WARNINGS='true'
 Rscript -e 'install.packages(c("remotes", "pkgdown"))'
 cd apis/r
 Rscript -e 'remotes::install_deps(dependencies = TRUE, upgrade=FALSE)'
-Rscript -e 'remotes::install_local()'
+# Need dependencies = FALSE to avoid getting latest/sometimes-too-new r-tiledb from CRAN when we
+# really want it from our r-universe where we control the version we want:
+Rscript -e 'remotes::install_local(dependencies = FALSE)'
 cd ../..
