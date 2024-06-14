44c3d7e52f78a022cec4608a0f5e74fd2900da3b
Vivian Nguyen <vivian@tiledb.com>
2024-06-14 07:47:21 -0500

    [python] Add Pybind11 bindings for `SOMAGroup`

--- apis/python/src/tiledbsoma/soma_group.cc
+++ apis/python/src/tiledbsoma/soma_group.cc
@@ -40,6 +40,65 @@ using namespace tiledbsoma;
 
 void load_soma_group(py::module& m) {
     py::class_<SOMAGroup, SOMAObject>(m, "SOMAGroup")
-        .def_property_readonly("type", &SOMAGroup::type);
+        .def_static(
+            "create",
+            [](std::shared_ptr<SOMAContext> ctx,
+               std::string_view uri,
+               std::string soma_type,
+               std::optional<TimestampRange> timestamp) {
+                try {
+                    SOMAGroup::create(ctx, uri, soma_type, timestamp);
+                } catch (const std::exception& e) {
+                    TPY_ERROR_LOC(e.what());
+                }
+            },
+            py::kw_only(),
+            "ctx"_a,
+            "uri"_a,
+            "soma_type"_a,
+            "timestamp"_a = py::none())
+        .def("__enter__", [](SOMAGroup& group) { return group; })
+        .def(
+            "__exit__",
+            [](SOMAGroup& group,
+               py::object exc_type,
+               py::object exc_value,
+               py::object traceback) { group.close(); })
+        .def_property_readonly(
+            "mode",
+            [](SOMAGroup& group) {
+                return group.mode() == OpenMode::read ? "r" : "w";
+            })
+        .def("close", &SOMAGroup::close)
+        .def_property_readonly(
+            "closed",
+            [](SOMAGroup& group) -> bool { return not group.is_open(); })
+        .def_property_readonly("uri", &SOMAGroup::uri)
+        .def("context", &SOMAGroup::ctx)
+        .def("has", &SOMAGroup::has)
+        .def("add", &SOMAGroup::set)
+        .def("count", &SOMAGroup::count)
+        .def("remove", &SOMAGroup::del)
+        .def("members", &SOMAGroup::members)
+        .def_property_readonly(
+            "timestamp",
+            [](SOMAGroup& group) -> py::object {
+                if (!group.timestamp().has_value())
+                    return py::none();
+                return py::cast(group.timestamp()->second);
+            })
+        .def_property_readonly(
+            "meta",
+            [](SOMAGroup& group) -> py::dict {
+                return meta(group.get_metadata());
+            })
+        .def(
+            "set_metadata", 
+            [](SOMAGroup& group, const std::string& key, py::array value){
+                set_metadata(group, key, value);
+            })
+        .def("delete_metadata", &SOMAGroup::delete_metadata)
+        .def("has_metadata", &SOMAGroup::has_metadata)
+        .def("metadata_num", &SOMAGroup::metadata_num);
 }
 }  // namespace libtiledbsomacpp
