df29d33f911e01c276169b6a49de8e15826d221f
Vivian Nguyen <vivian@tiledb.com>
2024-06-14 07:23:24 -0500

    [c++] More `SOMAGroup` refinements

--- libtiledbsoma/src/soma/soma_group.h
+++ libtiledbsoma/src/soma/soma_group.h
@@ -344,7 +344,11 @@ class SOMAGroup : public SOMAObject {
     std::optional<TimestampRange> timestamp_;
 
     // Member-to-URI cache
+<<<<<<< HEAD
     std::map<std::string, SOMAGroupEntry> members_map_;
+=======
+    std::map<std::string, SOMAGroupEntry> members_;
+>>>>>>> 46adfa3e ([c++] More `SOMAGroup` refinements)
 };
 
 }  // namespace tiledbsoma
