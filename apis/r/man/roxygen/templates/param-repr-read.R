3528e34c60b287e0ae214f4db4bef1aee2a03a4f
Paul Hoffman <mojaveazure@users.noreply.github.com>
2024-06-17 12:14:32 -0400

    [r] Connect re-indexer to blockwise iterator (#2742)
    Connect the re-indexer to the blockwise iterator, allowing reads to be re-indexed on-the-fly. This PR parallels #1792 and completes #2152 and #2637; in addition, provides new shorthand for `reindex_disable_on_axis`:
     - `TRUE`: disable re-indexing on all axes
     - `FALSE: re-index on all axes
     - `NA`: re-index only on major axis, disable re-indexing on all axes (default)
    
    `BlockwiseTableReadIter$concat()` and `BlockwiseSparseReadIter$concat()` are disabled when re-indexing is requested (paralleling Python)
    
    `BlockwiseSparseReadIter` now accepts `repr = "R"` or `repr = "C"` under certain circumstances:
     - axis 0 (`soma_dim_0`) must be re-indexed to allow `repr = "R"`
     - axis 1 (`soma_dim_1`) must be re-indexed to allow `repr = "C"`
    
    `repr` of `"T"` is allowed in all circumstances and continues to be the default
    
    Two new fields are available to blockwise iterators:
     - `$axes_to_reindex`: a vector of minor axes slated to be re-indexed
     - `$reindexable`: status indicator stating if _any_ axis (major or minor) is slated to be re-indexed
    
    resolves #2671

--- apis/r/man/roxygen/templates/param-repr-read.R
+++ apis/r/man/roxygen/templates/param-repr-read.R
@@ -2,4 +2,11 @@
 #' \itemize{
 #'  \item \dQuote{\code{T}}: returns a
 #'   \code{\link[Matrix:TsparseMatrix-class]{TsparseMatrix}}
+#'  \item \dQuote{\code{R}}: returns an
+#'   \code{\link[Matrix:RsparseMatrix-class]{RsparseMatrix}}
+#'  \item \dQuote{\code{C}}: returns a
+#'   \code{\link[Matrix:CsparseMatrix-class]{CsparseMatrix}}
 #' }
+#' \strong{Note}: passing \code{repr} of \dQuote{\code{R}} or \dQuote{\code{C}}
+#' are only available if re-indexing is enabled on axes \code{0} or \code{1},
+#' respectively
