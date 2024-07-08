d9e0aa87afbbb21ea01bb34084f38c29330dd238
John Kerl <kerl.john.r@gmail.com>
2024-07-08 12:04:22 -0400

    Compare core to tiledb-py/tiledb-r major+minor, `main` branch

--- apis/r/R/Init.R
+++ apis/r/R/Init.R
@@ -5,6 +5,9 @@
 .onLoad <- function(libname, pkgname) {
     rpkg_lib_version <- tiledb::tiledb_version(compact=TRUE)
     soma_lib_version <- libtiledbsoma_version(compact=TRUE)
+    # Check major and minor but not micro: sc-50464
+    rpkg_lib_version <- strsplit(rpkg_lib_version, "\\.")[[1]][1:2]
+    soma_lib_version <- strsplit(soma_lib_version, "\\.")[[1]][1:2]
     if (rpkg_lib_version != soma_lib_version) {
         msg <- sprintf("TileDB Core version %s used by TileDB-R package, but TileDB-SOMA uses %s",
                        sQuote(rpkg_lib_version), sQuote(soma_lib_version))
