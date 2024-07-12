e2770a037c78e7ea554ea8974da24715b787ab83
Julia Dark <julia@tiledb.com>
2024-07-12 10:09:26 -0400

    Update somacore version for Python API and pre-commit

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -342,7 +342,7 @@ setuptools.setup(
         "scanpy>=1.9.2",
         "scipy",
         # Note: the somacore version is in .pre-commit-config.yaml too
-        "somacore @ git+https://github.com/single-cell-data/SOMA.git@2a5f905",  # DO NOT MERGE TO MAIN
+        "somacore @ git+https://github.com/single-cell-data/SOMA.git@e411906",  # DO NOT MERGE TO MAIN
         "tiledb~=0.31.0",
         "typing-extensions",  # Note "-" even though `import typing_extensions`
     ],
