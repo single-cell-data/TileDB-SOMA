82f32c7ce53064d73dfa9fc386f410504a7f564e
nguyenv <vivian@tiledb.com>
2024-08-06 21:42:33 -0500

    [c++] Only evolve schema when enumerations are extended (#2851)

--- libtiledbsoma/src/soma/soma_array.h
+++ libtiledbsoma/src/soma/soma_array.h
@@ -759,7 +759,7 @@ class SOMAArray : public SOMAObject {
 
     uint64_t _get_max_capacity(tiledb_datatype_t index_type);
 
-    Enumeration _extend_enumeration(
+    bool _extend_enumeration(
         ArrowSchema* value_schema,
         ArrowArray* value_array,
         ArrowSchema* index_schema,
@@ -767,7 +767,7 @@ class SOMAArray : public SOMAObject {
         ArraySchemaEvolution se);
 
     template <typename ValueType>
-    Enumeration _extend_and_evolve_schema(
+    bool _extend_and_evolve_schema(
         ArrowArray* value_array,
         ArrowSchema* index_schema,
         ArrowArray* index_array,
@@ -816,21 +816,19 @@ class SOMAArray : public SOMAObject {
             index_schema->format = ArrowAdapter::to_arrow_format(
                                        disk_index_type)
                                        .data();
-
-            return extended_enmr;
+            return true;
         }
-
-        return enmr;
+        return false;
     }
 
-    Enumeration _extend_and_evolve_schema_str(
+    bool _extend_and_evolve_schema_str(
         ArrowSchema* value_schema,
         ArrowArray* value_array,
         ArrowSchema* index_schema,
         ArrowArray* index_array,
         ArraySchemaEvolution se);
 
-    void _create_and_cast_column(
+    bool _create_and_cast_column(
         ArrowSchema* orig_column_schema,
         ArrowArray* orig_column_array,
         ArrowSchema* new_column_schema,
