e4c7d6d357a84e0853e6f92b72f6f980e60dce13
Dirk Eddelbuettel <edd@debian.org>
2024-08-27 09:26:03 -0500

    Signal script failure to calling script, polish messages

--- apis/r/tools/check_cmake_and_git.R
+++ apis/r/tools/check_cmake_and_git.R
@@ -5,12 +5,14 @@ if (cmake_bin == "" && file.exists("/Applications/CMake.app/Contents/bin/cmake")
     cmake_bin <- "/Applications/CMake.app/Contents/bin/cmake"
 
 if (cmake_bin == "") {
-    stop("No 'cmake' binary found", call. = FALSE)
+    message("\n*** No 'cmake' binary found.\n*** Please install 'cmake' to build from source.\n")
+    q(save = "no", status = 1)
 }
 
 git_bin <- Sys.which("git")
 if (git_bin == "") {
-    stop("No 'git' binary found", call. = FALSE)
+    message("\n*** No 'git' binary found.\n*** Please install 'git' to build from source.\n")
+    q(save = "no", status = 1)
 }
 
 lines <- readLines("tools/build_libtiledbsoma.sh.in")
