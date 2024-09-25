a5961fa2ec9a22cf8095bdad4ecf4d234f1a5176
John Kerl <kerl.john.r@gmail.com>
2024-09-25 10:56:35 -0400

    [r] Run `clang-format` on `apis/r/src` (#3048)
    * [r] Run `clang-format` on `apis/r` source
    
    * manual fix-ups

--- apis/r/src/rinterface.cpp
+++ apis/r/src/rinterface.cpp
@@ -1,7 +1,7 @@
-#include <Rcpp.h>                               // for R interface to C++
-#include <nanoarrow/r.h>                        // for C interface to Arrow (via R package)
-#include <nanoarrow/nanoarrow.hpp>              // for C/C++ interface to Arrow
-#include <RcppInt64>                            // for fromInteger64
+#include <Rcpp.h>                   // for R interface to C++
+#include <nanoarrow/r.h>            // for C interface to Arrow (via R package)
+#include <RcppInt64>                // for fromInteger64
+#include <nanoarrow/nanoarrow.hpp>  // for C/C++ interface to Arrow
 
 // we currently get deprecation warnings by default which are noisy
 #ifndef TILEDB_NO_API_DEPRECATION_WARNINGS
@@ -10,14 +10,14 @@
 
 // We get these via nanoarrow and must cannot include carrow.h again
 #define ARROW_SCHEMA_AND_ARRAY_DEFINED 1
-#include <tiledbsoma/tiledbsoma>
 #include <tiledbsoma/reindexer/reindexer.h>
+#include <tiledbsoma/tiledbsoma>
 #if TILEDB_VERSION_MAJOR == 2 && TILEDB_VERSION_MINOR >= 4
 #include <tiledb/tiledb_experimental>
 #endif
 
-#include "rutilities.h"         // local declarations
-#include "xptr-utils.h"         // xptr taggging utilities
+#include "rutilities.h"  // local declarations
+#include "xptr-utils.h"  // xptr taggging utilities
 
 // (Adapted) helper functions from nanoarrow
 //
@@ -25,38 +25,42 @@
 // non-null, non-released pointer when garbage collected. We use a tagged XPtr,
 // but do not set an XPtr finalizer
 Rcpp::XPtr<ArrowSchema> schema_owning_xptr(void) {
-  struct ArrowSchema* schema = (struct ArrowSchema*)ArrowMalloc(sizeof(struct ArrowSchema));
-  if (schema == NULL) Rcpp::stop("Failed to allocate ArrowSchema");
-  schema->release = NULL;
-  Rcpp::XPtr<ArrowSchema> schema_xptr = make_xptr(schema, false);
-  return schema_xptr;
+    struct ArrowSchema* schema = (struct ArrowSchema*)ArrowMalloc(
+        sizeof(struct ArrowSchema));
+    if (schema == NULL)
+        Rcpp::stop("Failed to allocate ArrowSchema");
+    schema->release = NULL;
+    Rcpp::XPtr<ArrowSchema> schema_xptr = make_xptr(schema, false);
+    return schema_xptr;
 }
 // Create an external pointer with the proper class and that will release any
 // non-null, non-released pointer when garbage collected. We use a tagged XPtr,
 // but do not set an XPtr finalizer
 Rcpp::XPtr<ArrowArray> array_owning_xptr(void) {
-  struct ArrowArray* array = (struct ArrowArray*)ArrowMalloc(sizeof(struct ArrowArray));
-  if (array == NULL) Rcpp::stop("Failed to allocate ArrowArray");
-  array->release = NULL;
-  Rcpp::XPtr<ArrowArray> array_xptr = make_xptr(array, false);
-  return array_xptr;
+    struct ArrowArray* array = (struct ArrowArray*)ArrowMalloc(
+        sizeof(struct ArrowArray));
+    if (array == NULL)
+        Rcpp::stop("Failed to allocate ArrowArray");
+    array->release = NULL;
+    Rcpp::XPtr<ArrowArray> array_xptr = make_xptr(array, false);
+    return array_xptr;
 }
 
 namespace tdbs = tiledbsoma;
 
 //' @noRd
 // [[Rcpp::export(soma_array_reader_impl)]]
-SEXP soma_array_reader(const std::string& uri,
-                       Rcpp::XPtr<somactx_wrap_t> ctxxp,
-                       Rcpp::Nullable<Rcpp::CharacterVector> colnames = R_NilValue,
-                       Rcpp::Nullable<Rcpp::XPtr<tiledb::QueryCondition>> qc = R_NilValue,
-                       Rcpp::Nullable<Rcpp::List> dim_points = R_NilValue,
-                       Rcpp::Nullable<Rcpp::List> dim_ranges = R_NilValue,
-                       std::string batch_size = "auto",
-                       std::string result_order = "auto",
-                       const std::string& loglevel = "auto",
-                       Rcpp::Nullable<Rcpp::DatetimeVector> timestamprange = R_NilValue) {
-
+SEXP soma_array_reader(
+    const std::string& uri,
+    Rcpp::XPtr<somactx_wrap_t> ctxxp,
+    Rcpp::Nullable<Rcpp::CharacterVector> colnames = R_NilValue,
+    Rcpp::Nullable<Rcpp::XPtr<tiledb::QueryCondition>> qc = R_NilValue,
+    Rcpp::Nullable<Rcpp::List> dim_points = R_NilValue,
+    Rcpp::Nullable<Rcpp::List> dim_ranges = R_NilValue,
+    std::string batch_size = "auto",
+    std::string result_order = "auto",
+    const std::string& loglevel = "auto",
+    Rcpp::Nullable<Rcpp::DatetimeVector> timestamprange = R_NilValue) {
     if (loglevel != "auto") {
         spdl::set_level(loglevel);
         tdbs::LOG_SET_LEVEL(loglevel);
@@ -68,39 +72,48 @@ SEXP soma_array_reader(const std::string& uri,
     spdl::info("[soma_array_reader] Reading from {}", uri);
 
     std::vector<std::string> column_names = {};
-    if (!colnames.isNull()) {    // If we have column names, select them
+    if (!colnames.isNull()) {  // If we have column names, select them
         column_names = Rcpp::as<std::vector<std::string>>(colnames);
-        spdl::debug("[soma_array_reader] Selecting {} columns", column_names.size());
+        spdl::debug(
+            "[soma_array_reader] Selecting {} columns", column_names.size());
     }
 
     auto tdb_result_order = get_tdb_result_order(result_order);
 
     // optional timestamp range
-    std::optional<tdbs::TimestampRange> tsrng = makeTimestampRange(timestamprange);
+    std::optional<tdbs::TimestampRange> tsrng = makeTimestampRange(
+        timestamprange);
     if (timestamprange.isNotNull()) {
         Rcpp::DatetimeVector vec(timestamprange);
-        spdl::debug("[soma_array_reader] timestamprange ({},{})", vec[0], vec[1]);
+        spdl::debug(
+            "[soma_array_reader] timestamprange ({},{})", vec[0], vec[1]);
     }
 
     // Read selected columns from the uri (return is unique_ptr<SOMAArray>)
-    auto sr = tdbs::SOMAArray::open(OpenMode::read,
-                                    uri,
-                                    somactx,
-                                    "unnamed",         // name parameter could be added
-                                    column_names,
-                                    batch_size,
-                                    tdb_result_order,
-                                    tsrng);
+    auto sr = tdbs::SOMAArray::open(
+        OpenMode::read,
+        uri,
+        somactx,
+        "unnamed",  // name parameter could be added
+        column_names,
+        batch_size,
+        tdb_result_order,
+        tsrng);
 
-    std::unordered_map<std::string, std::shared_ptr<tiledb::Dimension>> name2dim;
+    std::unordered_map<std::string, std::shared_ptr<tiledb::Dimension>>
+        name2dim;
     std::shared_ptr<tiledb::ArraySchema> schema = sr->tiledb_schema();
     tiledb::Domain domain = schema->domain();
     std::vector<tiledb::Dimension> dims = domain.dimensions();
-    for (auto& dim: dims) {
-        spdl::info("[soma_array_reader] Dimension {} type {} domain {} extent {}",
-                   dim.name(), tiledb::impl::to_str(dim.type()),
-                   dim.domain_to_str(), dim.tile_extent_to_str());
-        name2dim.emplace(std::make_pair(dim.name(), std::make_shared<tiledb::Dimension>(dim)));
+    for (auto& dim : dims) {
+        spdl::info(
+            "[soma_array_reader] Dimension {} type {} domain {} extent {}",
+            dim.name(),
+            tiledb::impl::to_str(dim.type()),
+            dim.domain_to_str(),
+            dim.tile_extent_to_str());
+        name2dim.emplace(std::make_pair(
+            dim.name(), std::make_shared<tiledb::Dimension>(dim)));
     }
 
     // If we have a query condition, apply it
@@ -111,8 +124,9 @@ SEXP soma_array_reader(const std::string& uri,
     }
 
     // If we have dimension points, apply them
-    // The interface is named list, where each (named) list elements is one (named) dimesion
-    // The List element is a simple vector of points and each point is applied to the named dimension
+    // The interface is named list, where each (named) list elements is one
+    // (named) dimesion The List element is a simple vector of points and each
+    // point is applied to the named dimension
     if (!dim_points.isNull()) {
         Rcpp::List lst(dim_points);
         apply_dim_points(sr.get(), name2dim, lst);
@@ -127,30 +141,39 @@ SEXP soma_array_reader(const std::string& uri,
     // Getting next batch:  std::optional<std::shared_ptr<ArrayBuffers>>
     auto sr_data = sr->read_next();
     if (!sr->results_complete()) {
-        Rcpp::stop("Read of '%s' is incomplete.\nConsider increasing the memory "
-                   "allocation via the configuration\noption 'soma.init_buffer_bytes', "
-                   "or using iterated partial reads.", uri);
+        Rcpp::stop(
+            "Read of '%s' is incomplete.\nConsider increasing the memory "
+            "allocation via the configuration\noption "
+            "'soma.init_buffer_bytes', "
+            "or using iterated partial reads.",
+            uri);
     }
-    spdl::info("[soma_array_reader] Read complete with {} rows and {} cols",
-               sr_data->get()->num_rows(), sr_data->get()->names().size());
+    spdl::info(
+        "[soma_array_reader] Read complete with {} rows and {} cols",
+        sr_data->get()->num_rows(),
+        sr_data->get()->names().size());
 
     const std::vector<std::string> names = sr_data->get()->names();
     auto ncol = names.size();
     // Schema first
     auto schemaxp = nanoarrow_schema_owning_xptr();
     auto sch = nanoarrow_output_schema_from_xptr(schemaxp);
-    exitIfError(ArrowSchemaInitFromType(sch, NANOARROW_TYPE_STRUCT), "Bad schema init");
+    exitIfError(
+        ArrowSchemaInitFromType(sch, NANOARROW_TYPE_STRUCT), "Bad schema init");
     exitIfError(ArrowSchemaSetName(sch, ""), "Bad schema name");
-    exitIfError(ArrowSchemaAllocateChildren(sch, ncol), "Bad schema children alloc");
+    exitIfError(
+        ArrowSchemaAllocateChildren(sch, ncol), "Bad schema children alloc");
 
     // Array second
     auto arrayxp = nanoarrow_array_owning_xptr();
     auto arr = nanoarrow_output_array_from_xptr(arrayxp);
-    exitIfError(ArrowArrayInitFromType(arr, NANOARROW_TYPE_STRUCT), "Bad array init");
-    exitIfError(ArrowArrayAllocateChildren(arr, ncol), "Bad array children alloc");
+    exitIfError(
+        ArrowArrayInitFromType(arr, NANOARROW_TYPE_STRUCT), "Bad array init");
+    exitIfError(
+        ArrowArrayAllocateChildren(arr, ncol), "Bad array children alloc");
 
-    arr->length = 0;             // initial value
-    for (size_t i=0; i<ncol; i++) {
+    arr->length = 0;  // initial value
+    for (size_t i = 0; i < ncol; i++) {
         spdl::info("[soma_array_reader] Accessing '{}' at pos {}", names[i], i);
 
         // now buf is a shared_ptr to ColumnBuffer
@@ -159,32 +182,37 @@ SEXP soma_array_reader(const std::string& uri,
         // this is pair of array and schema pointer
         auto pp = tdbs::ArrowAdapter::to_arrow(buf);
 
-        //memcpy((void*) sch->children[i], pp.second.get(), sizeof(ArrowSchema));
-        //memcpy((void*) arr->children[i], pp.first.get(), sizeof(ArrowArray));
-        ArrowArrayMove(pp.first.get(),  arr->children[i]);
+        // memcpy((void*) sch->children[i], pp.second.get(),
+        // sizeof(ArrowSchema)); memcpy((void*) arr->children[i],
+        // pp.first.get(), sizeof(ArrowArray));
+        ArrowArrayMove(pp.first.get(), arr->children[i]);
         ArrowSchemaMove(pp.second.get(), sch->children[i]);
 
-        spdl::info("[soma_array_reader] Incoming name {} length {}",
-                   std::string(pp.second->name), pp.first->length);
+        spdl::info(
+            "[soma_array_reader] Incoming name {} length {}",
+            std::string(pp.second->name),
+            pp.first->length);
 
         if (pp.first->length > arr->length) {
-            spdl::debug("[soma_array_reader] Setting array length to {}", pp.first->length);
+            spdl::debug(
+                "[soma_array_reader] Setting array length to {}",
+                pp.first->length);
             arr->length = pp.first->length;
         }
     }
 
-   // Nanoarrow special: stick schema into xptr tag to return single SEXP
-   array_xptr_set_schema(arrayxp, schemaxp); 			// embed schema in array
-   sr->close();
-   return arrayxp;
+    // Nanoarrow special: stick schema into xptr tag to return single SEXP
+    array_xptr_set_schema(arrayxp, schemaxp);  // embed schema in array
+    sr->close();
+    return arrayxp;
 }
 
 //' Set the logging level for the R package and underlying C++ library
 //'
-//' @param level A character value with logging level understood by \sQuote{spdlog}
-//' such as \dQuote{trace}, \dQuote{debug}, \dQuote{info}, or \dQuote{warn}.
-//' @return Nothing is returned as the function is invoked for the side-effect.
-//' @export
+//' @param level A character value with logging level understood by
+//\sQuote{spdlog} ' such as \dQuote{trace}, \dQuote{debug}, \dQuote{info}, or
+//\dQuote{warn}. ' @return Nothing is returned as the function is invoked for
+//the side-effect. ' @export
 // [[Rcpp::export]]
 void set_log_level(const std::string& level) {
     spdl::setup("R", level);
@@ -193,14 +221,13 @@ void set_log_level(const std::string& level) {
 
 //' @noRd
 // [[Rcpp::export]]
-Rcpp::CharacterVector get_column_types(const std::string& uri,
-                                       const std::vector<std::string>& colnames) {
-
+Rcpp::CharacterVector get_column_types(
+    const std::string& uri, const std::vector<std::string>& colnames) {
     auto sr = tdbs::SOMAArray::open(OpenMode::read, uri);
     auto sr_data = sr->read_next();
     size_t n = colnames.size();
     Rcpp::CharacterVector vs(n);
-    for (size_t i=0; i<n; i++) {
+    for (size_t i = 0; i < n; i++) {
         auto datatype = sr_data->get()->at(colnames[i])->type();
         vs[i] = std::string(tiledb::impl::to_str(datatype));
     }
@@ -220,19 +247,20 @@ double nnz(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
 //' @noRd
 // [[Rcpp::export]]
 bool check_arrow_schema_tag(Rcpp::XPtr<ArrowSchema> xp) {
-  check_xptr_tag<ArrowSchema>(xp);  // throws if mismatched
-  return true;
+    check_xptr_tag<ArrowSchema>(xp);  // throws if mismatched
+    return true;
 }
 
 //' @noRd
 // [[Rcpp::export]]
 bool check_arrow_array_tag(Rcpp::XPtr<ArrowArray> xp) {
-  check_xptr_tag<ArrowArray>(xp);  // throws if mismatched
-  return true;
+    check_xptr_tag<ArrowArray>(xp);  // throws if mismatched
+    return true;
 }
 
 // [[Rcpp::export]]
-Rcpp::NumericVector shape(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+Rcpp::NumericVector shape(
+    const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     auto sr = tdbs::SOMAArray::open(OpenMode::read, uri, ctxxp->ctxptr);
     auto retval = Rcpp::toInteger64(sr->shape());
     sr->close();
@@ -240,7 +268,8 @@ Rcpp::NumericVector shape(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctx
 }
 
 // [[Rcpp::export]]
-Rcpp::NumericVector maxshape(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+Rcpp::NumericVector maxshape(
+    const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     auto sr = tdbs::SOMAArray::open(OpenMode::read, uri, ctxxp->ctxptr);
     auto retval = Rcpp::toInteger64(sr->maxshape());
     sr->close();
@@ -248,37 +277,42 @@ Rcpp::NumericVector maxshape(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> 
 }
 
 // [[Rcpp::export]]
-Rcpp::NumericVector maybe_soma_joinid_shape(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+Rcpp::NumericVector maybe_soma_joinid_shape(
+    const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     // Pro-tip:
     // * Open with mode and uri gives a SOMAArray.
     // * Open with uri and mode gives a SOMADataFrame.
-    // This was done intentionally to resolve an ambiguous-overload compiler error.
-    // ^ Unsure. This is C++, and it is typed so member functions return objects of their class.
+    // This was done intentionally to resolve an ambiguous-overload compiler
+    // error. ^ Unsure. This is C++, and it is typed so member functions return
+    // objects of their class.
     auto sr = tdbs::SOMADataFrame::open(uri, OpenMode::read, ctxxp->ctxptr);
     auto retval = sr->maybe_soma_joinid_shape();
     sr->close();
     if (retval.has_value()) {
-      return Rcpp::toInteger64(retval.value());
+        return Rcpp::toInteger64(retval.value());
     } else {
-      return Rcpp::NumericVector::create(NA_REAL); // one element vector, and is.na() is true
+        return Rcpp::NumericVector::create(
+            NA_REAL);  // one element vector, and is.na() is true
     }
 }
 
 // [[Rcpp::export]]
-Rcpp::NumericVector maybe_soma_joinid_maxshape(const std::string& uri,
-                                               Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+Rcpp::NumericVector maybe_soma_joinid_maxshape(
+    const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     auto sr = tdbs::SOMADataFrame::open(uri, OpenMode::read, ctxxp->ctxptr);
     auto retval = sr->maybe_soma_joinid_maxshape();
     sr->close();
     if (retval.has_value()) {
-      return Rcpp::toInteger64(retval.value());
+        return Rcpp::toInteger64(retval.value());
     } else {
-      return Rcpp::NumericVector::create(NA_REAL); // one element vector, and is.na() is true
+        return Rcpp::NumericVector::create(
+            NA_REAL);  // one element vector, and is.na() is true
     }
 }
 
 // [[Rcpp::export]]
-Rcpp::LogicalVector has_current_domain(const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+Rcpp::LogicalVector has_current_domain(
+    const std::string& uri, Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     auto sr = tdbs::SOMAArray::open(OpenMode::read, uri, ctxxp->ctxptr);
     auto retval = Rcpp::LogicalVector(sr->has_current_domain());
     sr->close();
@@ -286,9 +320,10 @@ Rcpp::LogicalVector has_current_domain(const std::string& uri, Rcpp::XPtr<somact
 }
 
 // [[Rcpp::export]]
-void resize(const std::string& uri,
-            Rcpp::NumericVector new_shape,
-            Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+void resize(
+    const std::string& uri,
+    Rcpp::NumericVector new_shape,
+    Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     // This function is solely for SparseNDArray and DenseNDArray for which the
     // dims are required by the SOMA spec to be of type int64. Domain-resize for
     // variant-indexed dataframes will be separate work as tracked on
@@ -300,9 +335,10 @@ void resize(const std::string& uri,
 }
 
 // [[Rcpp::export]]
-void tiledbsoma_upgrade_shape(const std::string& uri,
-                              Rcpp::NumericVector new_shape,
-                              Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+void tiledbsoma_upgrade_shape(
+    const std::string& uri,
+    Rcpp::NumericVector new_shape,
+    Rcpp::XPtr<somactx_wrap_t> ctxxp) {
     // This function is solely for SparseNDArray and DenseNDArray for which the
     // dims are required by the SOMA spec to be of type int64. Domain-resize for
     // variant-indexed dataframes will be separate work as tracked on
