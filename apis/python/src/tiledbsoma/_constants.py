af7f1bcca446d2c926ad35975ab8fa4d0412178d
Julia Dark <julia@tiledb.com>
2024-09-27 16:23:31 -0400

    Update the encoding version to 1.1.0

--- apis/python/src/tiledbsoma/_constants.py
+++ apis/python/src/tiledbsoma/_constants.py
@@ -10,7 +10,7 @@ SOMA_JOINID = "soma_joinid"
 SOMA_GEOMETRY = "soma_geometry"
 SOMA_OBJECT_TYPE_METADATA_KEY = "soma_object_type"
 SOMA_ENCODING_VERSION_METADATA_KEY = "soma_encoding_version"
-SOMA_ENCODING_VERSION = "1"
+SOMA_ENCODING_VERSION = "1.1.0"
 
 SPATIAL_DISCLAIMER = (
     "Support for spatial types is experimental. Changes to both the API and data "
