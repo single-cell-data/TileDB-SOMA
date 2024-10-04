f4dc7fbc76443bf8cd36765c0f4a500101c274b9
Julia Dark <24235303+jp-dark@users.noreply.github.com>
2024-10-04 09:59:00 -0400

    Fix MultiscaleImage.read_spatial_region full region read (#3128)
    The `read_spatial_region` method should be able to accept `None` for a
    region. When this is provided, the whole array is returned.

--- apis/python/tests/test_multiscale_image.py
+++ apis/python/tests/test_multiscale_image.py
@@ -5,6 +5,7 @@ import pyarrow as pa
 import pytest
 
 import tiledbsoma as soma
+from tiledbsoma import ScaleTransform
 
 
 def test_multiscale_image_bad_create(tmp_path):
@@ -120,3 +121,80 @@ def test_multiscale_basic_no_channels(tmp_path):
             assert props.shape == shape
             assert props.height == shape[0]
             assert props.width == shape[1]
+
+
+class TestSimpleMultiscale2D:
+
+    @pytest.fixture(scope="class")
+    def image_uri(self, tmp_path_factory):
+        """Create a multiscale image and return the path."""
+        # Create the multiscale image.
+        baseuri = tmp_path_factory.mktemp("multiscale_image").as_uri()
+        image_uri = urljoin(baseuri, "default")
+        with soma.MultiscaleImage.create(
+            image_uri,
+            type=pa.uint8(),
+            reference_level_shape=(1, 9, 8),
+            axis_names=("c", "y", "x"),
+            axis_types=("channel", "height", "width"),
+        ) as image:
+            coords = (slice(None), slice(None), slice(None))
+            # Create levels.
+            l0 = image.add_new_level("level0", shape=(1, 9, 8))
+            l0.write(
+                coords,
+                pa.Tensor.from_numpy(np.arange(72, dtype=np.uint8).reshape(1, 9, 8)),
+            )
+
+            # Create medium sized downsample.
+            l1 = image.add_new_level("level1", shape=(1, 6, 4))
+            l1.write(
+                coords,
+                pa.Tensor.from_numpy(
+                    10 * np.arange(24, dtype=np.uint8).reshape(1, 6, 4)
+                ),
+            )
+
+            # Create very small downsample and write to it.
+            l2 = image.add_new_level("level2", shape=(1, 3, 2))
+            l2.write(
+                coords,
+                pa.Tensor.from_numpy(
+                    100 * np.arange(6, dtype=np.uint8).reshape(1, 3, 2)
+                ),
+            )
+        return image_uri
+
+    @pytest.mark.parametrize(
+        ("level", "region", "kwargs", "expected_data", "expected_transform"),
+        [
+            pytest.param(
+                2,
+                None,
+                {},
+                100 * np.arange(6, dtype=np.uint8).reshape(1, 3, 2),
+                ScaleTransform(("x", "y"), ("x", "y"), [4, 3]),
+                id="Level 2, full region, no transform",
+            ),
+        ],
+    )
+    def test_read_spatial_region(
+        self, image_uri, level, region, kwargs, expected_data, expected_transform
+    ):
+        with soma.MultiscaleImage.open(image_uri) as image:
+            result = image.read_spatial_region(level=level, region=region, **kwargs)
+        actual_data = result.data.to_numpy()
+
+        # Check data
+        np.testing.assert_array_equal(actual_data, expected_data)
+
+        # Check transform
+        actual_transform = result.coordinate_transform
+        assert actual_transform.input_axes == expected_transform.input_axes
+        assert actual_transform.output_axes == expected_transform.output_axes
+        assert isinstance(actual_transform, type(expected_transform))
+        np.testing.assert_array_almost_equal(
+            actual_transform.augmented_matrix,
+            expected_transform.augmented_matrix,
+            decimal=8,
+        )
