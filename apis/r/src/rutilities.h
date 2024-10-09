eae1484aba2bc6d536756dca9c7da73c9dbf54c6
John Kerl <kerl.john.r@gmail.com>
2024-10-09 10:41:20 -0400

    clang-format src/rutilities.h

--- apis/r/src/rutilities.h
+++ apis/r/src/rutilities.h
@@ -12,9 +12,9 @@ namespace tdbs = tiledbsoma;
 #define TileDB_Version(v, m, p) (((v)*65536) + ((m)*256) + (p))
 
 // current build is encoded in TILEDB_VERSION
-#define TILEDB_VERSION TileDB_Version(TILEDB_VERSION_MAJOR, \
-                                      TILEDB_VERSION_MINOR, \
-                                      TILEDB_VERSION_PATCH)
+#define TILEDB_VERSION \
+    TileDB_Version(    \
+        TILEDB_VERSION_MAJOR, TILEDB_VERSION_MINOR, TILEDB_VERSION_PATCH)
 
 // Applies (named list of) vectors of points to the named dimensions
 void apply_dim_points(
@@ -31,33 +31,39 @@ void apply_dim_ranges(
     Rcpp::List lst);
 
 // Convert R config vector to map<string,string> suitable for SOMAArray
-inline std::map<std::string, std::string> config_vector_to_map(Rcpp::Nullable<Rcpp::CharacterVector> config) {
+inline std::map<std::string, std::string> config_vector_to_map(
+    Rcpp::Nullable<Rcpp::CharacterVector> config) {
     std::map<std::string, std::string> platform_config;
 
     if (!config.isNull()) {
         Rcpp::CharacterVector confvec(config.get());
-        Rcpp::CharacterVector namesvec = confvec.attr("names"); // extract names from named R vector
+        Rcpp::CharacterVector namesvec = confvec.attr(
+            "names");  // extract names from named R vector
         size_t n = confvec.length();
-        for (size_t i = 0; i<n; i++) {
-            platform_config.emplace(std::make_pair(std::string(namesvec[i]), std::string(confvec[i])));
-            spdl::trace("[config_vector_to_map] adding '{}' = '{}'", std::string(namesvec[i]), std::string(confvec[i]));
+        for (size_t i = 0; i < n; i++) {
+            platform_config.emplace(std::make_pair(
+                std::string(namesvec[i]), std::string(confvec[i])));
+            spdl::trace(
+                "[config_vector_to_map] adding '{}' = '{}'",
+                std::string(namesvec[i]),
+                std::string(confvec[i]));
         }
     }
 
     return platform_config;
 }
 
-inline ResultOrder get_tdb_result_order(std::string result_order){
-	std::map<std::string, ResultOrder> result_order_map{
-		{"auto", ResultOrder::automatic},
-		{"row-major", ResultOrder::rowmajor},
-		{"column-major", ResultOrder::colmajor}
-	};
-	return result_order_map[result_order];
+inline ResultOrder get_tdb_result_order(std::string result_order) {
+    std::map<std::string, ResultOrder> result_order_map{
+        {"auto", ResultOrder::automatic},
+        {"row-major", ResultOrder::rowmajor},
+        {"column-major", ResultOrder::colmajor}};
+    return result_order_map[result_order];
 }
 
 inline void exitIfError(const ArrowErrorCode ec, const std::string& msg) {
-    if (ec != NANOARROW_OK) Rcpp::stop(msg);
+    if (ec != NANOARROW_OK)
+        Rcpp::stop(msg);
 }
 
 // Attaches a schema to an array external pointer. The nanoarrow R package
@@ -67,7 +73,8 @@ inline void array_xptr_set_schema(SEXP array_xptr, SEXP schema_xptr) {
 }
 
 // make a TimestampRange from a DatetimeVector (of size two)
-std::optional<tdbs::TimestampRange> makeTimestampRange(Rcpp::Nullable<Rcpp::DatetimeVector> tsvec);
+std::optional<tdbs::TimestampRange> makeTimestampRange(
+    Rcpp::Nullable<Rcpp::DatetimeVector> tsvec);
 
 std::vector<int64_t> i64_from_rcpp_numeric(const Rcpp::NumericVector& input);
 
