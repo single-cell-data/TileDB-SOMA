70f66cf82679ec54b23f52715dfac0e067ebaaff
John Kerl <kerl.john.r@gmail.com>
2024-10-15 15:38:32 -0400

    [r] Port the query-condition logic from TileDB-R to TileDB-SOMA-R (#3162)
    * iterating on query-condition port
    
    * tests/testthat/test-query-condition.R
    
    * code-review feedback
    
    Co-authored-by: Paul Hoffman <mojaveazure@users.noreply.github.com>
    
    * DESCRIPTION NEWS.md
    
    ---------
    
    Co-authored-by: Paul Hoffman <mojaveazure@users.noreply.github.com>

---
+++ apis/r/R/QueryCondition.R
@@ -0,0 +1,323 @@
+#  MIT License
+#
+#  Copyright (c) 2021-2024 TileDB Inc.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+# ================================================================
+#' Create a 'tiledbsoma_query_condition' object from an expression
+#'
+#' The grammar for query conditions is constrained to the operators
+#' (\code{">"}, \code{">="}, \code{"<"}, \code{"<="}, \code{"=="},
+#' \code{"!="}, \code{"%in%"}, \code{"%nin%"}), and three boolean operators
+#' (\code{"&&"}, also as \code{"&"}, (\code{"||"}, also as \code{"|"}, and
+#' \code{"!"} for negation.  Note that we locally define \code{"%nin%"} as
+#' \code{Negate()} call around \code{%in%)} which extends R a little for this
+#' use case.
+#'
+#' Expressions, in the R language syntax, are parsed locally by this function.
+#'
+#' @param expr An expression that is understood by the TileDB grammar for
+#' query conditions.
+#'
+#' @param schema The Arrow schema for the array for which a query
+#' condition is being prepared. This is necessary to obtain type information
+#' about left-hand sides of query expressions.
+#'
+#' @param strict A boolean toogle to, if set, errors if a non-existing
+#' attribute is selected or filtered on, defaults to 'TRUE'; if 'FALSE' a
+#' warning is shown but execution proceeds.
+#'
+#' @param somactx SOMAContext pointer.
+#'
+#' @return A `tiledbsoma_query_condition` object.
+#' 
+#' @noRd
+#'
+parse_query_condition_new <- function(
+  expr,
+  schema,
+  strict=TRUE,
+  somactx
+  ) {
+
+  stopifnot(
+      "The schema argument must be an Arrow Schema" =
+          is(schema, "ArrowObject") &&
+          is(schema, "Schema"),
+    "The argument must be a somactx object" =
+        is(somactx, "externalptr"))
+
+    # ----------------------------------------------------------------
+    # Helpers for walking the parse tree
+
+    # Operators
+    `%!in%` <- Negate(`%in%`)
+    .is_in_operator <- function(node) {
+        return(tolower(as.character(node)) %in% c("%in%", "%nin%"))
+    }
+    .is_comparison_operator <- function(node) {
+        return(tolower(as.character(node)) %in% c(">", ">=", "<", "<=", "==", "!=", "%in%", "%nin%"))
+    }
+    .is_boolean_operator <- function(node) {
+        return(as.character(node) %in% c("&&", "||", "!", "&", "|"))
+    }
+
+    # Leaf nodes
+    .is_ascii <- function(node) {
+        return(grepl("^[[:alnum:]_]+$", node))
+    }
+    .is_integer <- function(node) {
+        return(grepl("^[[:digit:]]+$", as.character(node)))
+    }
+    .is_double <- function(node) {
+        return(grepl("^[[:digit:]\\.]+$", as.character(node)) && length(grepRaw(".", as.character(node), fixed = TRUE, all = TRUE)) == 1)
+    }
+
+    .error_function <- if (strict) stop else warning
+
+    .map_op_to_character <- function(x) {
+        return(switch(x, `>` = "GT", `>=` = "GE", `<` = "LT", `<=` = "LE", `==` = "EQ", `!=` = "NE"))
+    }
+
+    .map_bool_to_character <- function(x) {
+        return(switch(x, `&&` = "AND", `&` = "AND", `||` = "OR", `|` = "OR", `!` = "NOT"))
+    }
+
+    # ----------------------------------------------------------------
+    # Map the R parse tree (from base-r `substitute`) to a TileDB core QueryCondition
+
+    .parse_tree_to_qc <- function(node, debug=FALSE) {
+        if (is.symbol(node)) {
+            stop("Unexpected symbol in expression: ", format(node))
+
+        } else if (.is_boolean_operator(node[1])) {
+            spdl::debug("[parseqc] boolop [{}] [{}] [{}]",
+                as.character(node[2]),
+                as.character(node[1]),
+                as.character(node[3]))
+
+            return(tiledbsoma_query_condition_combine(
+                .parse_tree_to_qc(node[[2]]),
+                .parse_tree_to_qc(node[[3]]),
+                .map_bool_to_character(as.character(node[1])),
+                somactx))
+
+        } else if (.is_in_operator(node[1])) {
+            spdl::debug("[parseqc] inop [{}] [{}] [{}]",
+                as.character(node[2]),
+                as.character(node[1]),
+                as.character(node[3]))
+
+            attr_name <- as.character(node[2])
+            r_op_name <- tolower(as.character(node[1]))
+            tdb_op_name <- if (r_op_name == "%in%") "IN" else "NOT_IN"
+
+            arrow_field <- schema[[attr_name]]
+            if (is.null(arrow_field)) {
+                .error_function("No attribute '", attr_name, "' is present.", call. = FALSE)
+            }
+            arrow_type_name <- arrow_field$type$name
+            is_enum <- is(arrow_field$type, "DictionaryType")
+
+            values <- eval(parse(text=as.character(node[3])))
+            if (arrow_type_name == "int32" && !is_enum) {
+                values <- as.integer(values)
+            }
+
+            return(tiledbsoma_query_condition_in_nin(attr_name, tdb_op_name, values, somactx))
+
+        } else if (.is_comparison_operator(node[1])) {
+            spdl::debug("[parseqc] cmpop [{}] [{}] [{}]",
+                as.character(node[2]),
+                as.character(node[1]),
+                as.character(node[3]))
+
+            op_name <- as.character(node[1])
+            attr_name <- as.character(node[2])
+            rhs_text <- as.character(node[3])
+
+            arrow_field <- schema[[attr_name]]
+            if (is.null(arrow_field)) {
+                .error_function("No attribute '", attr_name, "' is present.", call. = FALSE)
+            }
+            arrow_type_name <- arrow_field$type$name
+
+            # Take care of factor (aka "enum" case) and set the data type to ASCII
+            if (arrow_type_name == "dictionary") {
+                arrow_type_name <- "utf8"
+            }
+
+            # General case of extracting appropriate value given type info
+            return(tiledbsoma_query_condition_from_triple(
+                attr_name = attr_name,
+                value = switch(
+                    arrow_type_name,
+                    ascii = rhs_text,
+                    utf8 = rhs_text,
+                    bool = as.logical(rhs_text),
+                    date32 = as.POSIXct(rhs_text),
+                    timestamp = as.Date(rhs_text),
+                    as.numeric(rhs_text)),
+                arrow_type_name = arrow_type_name,
+                op_name = .map_op_to_character(op_name),
+                qc = tiledbsoma_empty_query_condition(somactx)))
+
+        } else {
+            stop("Unexpected token in expression: ", format(node))
+        }
+    }
+
+    # Use base-r `substitute` to map the user-provided expression to a parse tree
+    parse_tree <- substitute(expr)
+
+    # Map the parse tree to TileDB core QueryCondition
+    return(.parse_tree_to_qc(parse_tree, debug))
+}
+
+# ================================================================
+#' An S4 class for a TileDB QueryCondition object
+#'
+#' @slot ptr An external pointer to the underlying implementation
+#' @slot init A logical variable tracking if the query condition object has been
+#' initialized
+setClass(
+    "tiledbsoma_query_condition",
+    slots = list(ptr = "externalptr", init = "logical"))
+
+# ================================================================
+#' Creates a 'tiledbsoma_query_condition' object
+#'
+#' @param somactx (optional) A TileDB Ctx object; if not supplied the default
+#' context object is retrieved
+#' @return A 'tiledbsoma_query_condition' object
+#' @export
+tiledbsoma_empty_query_condition <- function(somactx) {
+    stopifnot("The argument must be a somactx object" = is(somactx, "externalptr"))
+    ptr <- libtiledbsoma_empty_query_condition(somactx)
+    query_condition <- new("tiledbsoma_query_condition", ptr = ptr, init = FALSE)
+    invisible(query_condition)
+}
+
+# ================================================================
+#' Initialize a 'tiledbsoma_query_condition' object
+#'
+#' Initializes (and possibly allocates) a query condition object using a triplet of
+#' attribute name, comparison value, and operator.  Six types of conditions are supported,
+#' they all take a single scalar comparison argument and attribute to compare against.
+#' At present only integer or numeric attribute comparisons are implemented.
+#' @param attr_name A character value with the scheme attribute name
+#' @param value A scalar value that the attribute is compared against
+#' @param arrow_type_name A character value with the TileDB data type of the attribute column, for
+#' example 'float' or 'int32'
+#' @param op_name A character value with the comparison operation. This must be one of
+#' 'LT', 'LE', 'GT', 'GE', 'EQ', 'NE'.
+#' @param qc A 'tiledbsoma_query_condition' object to be initialized by this call.
+#' @return The initialized 'tiledbsoma_query_condition' object
+#' 
+#' @noRd
+#'
+tiledbsoma_query_condition_from_triple <- function(
+    attr_name,
+    value,
+    arrow_type_name,
+    op_name,
+    qc) {
+
+    stopifnot(
+        "Argument 'qc' with query condition object required" = inherits(qc, "tiledbsoma_query_condition"),
+        "Argument 'attr_name' must be character" = is.character(attr_name),
+        "Argument 'value' must be of length one" = (
+            is.vector(value) ||
+            bit64::is.integer64(value) ||
+            inherits(value, "POSIXt") ||
+            inherits(value, "Date")) && all.equal(length(value),1),
+        "Argument 'arrow_type_name' must be character" = is.character(arrow_type_name),
+        "Argument 'op_name' must be character" = is.character(op_name))
+
+    op_name <- match.arg(op_name, c("LT", "LE", "GT", "GE", "EQ", "NE"))
+    # If arrow_type_name is int64 or uint64 but the class of value does not yet inherit from
+    # integer64, cast.
+    if (grepl("int64", arrow_type_name) && !inherits(value, "integer64")) {
+        value <- bit64::as.integer64(value)
+    }
+    libtiledbsoma_query_condition_from_triple(qc@ptr, attr_name, value, arrow_type_name, op_name)
+    qc@init <- TRUE
+    invisible(qc)
+}
+
+# ================================================================
+#' Combine two 'tiledbsoma_query_condition' objects
+#'
+#' Combines two query condition objects using a relatiional operator.
+#'
+#' @param lhs A 'tiledbsoma_query_condition' object on the left-hand side of the relation
+#' @param rhs A 'tiledbsoma_query_condition' object on the right-hand side of the relation
+#' @param op_name A character value with the relation, which must be one of 'AND', 'OR' or 'NOT'.
+#' @param somactx SOMAContext pointer.
+#' @return The combined 'tiledbsoma_query_condition' object
+#' 
+#' @noRd
+#'
+tiledbsoma_query_condition_combine <- function(lhs, rhs, op_name, somactx) {
+    stopifnot(
+        "Argument 'lhs' must be a query condition object" = is(lhs, "tiledbsoma_query_condition"),
+        "Argument 'rhs' must be a query condition object" = is(rhs, "tiledbsoma_query_condition"),
+        "Argument 'op_name' must be a character" = is.character(op_name))
+    op_name <- match.arg(op_name, c("AND", "OR", "NOT"))
+    qc <- tiledbsoma_empty_query_condition(somactx)
+    qc@ptr <- libtiledbsoma_query_condition_combine(lhs@ptr, rhs@ptr, op_name)
+    qc@init <- TRUE
+    invisible(qc)
+}
+
+# ================================================================
+#' Create a query condition for vector 'IN' and 'NOT_IN' operations
+#'
+#' Uses \sQuote{IN} and \sQuote{NOT_IN} operators on given attribute
+#'
+#' @param attr_name A character value with the schema attribute name.
+#'
+#' @param op_name A character value with the chosen set operation. This must be one of
+#' \sQuote{IN} or \sQuote{NOT_IN}.
+#'
+#' @param values A vector wiith the given values. Supported types are integer, double,
+#' integer64, and character.
+#'
+#' @param somactx SOMAContext pointer.
+#'
+#' @return A query-condition object is returned
+#' 
+#' @noRd
+#'
+tiledbsoma_query_condition_in_nin <- function(
+  attr_name,
+  op_name = "IN",
+  values,
+  somactx) {
+    stopifnot("Argument 'attr_name' must be character" = is.character(attr_name),
+              "Argument 'values' must be int, double, int64 or char" =
+                  (is.numeric(values) || bit64::is.integer64(values) || is.character(values)),
+              "Argument 'op_name' must be one of 'IN' or 'NOT_IN'" = op_name %in% c("IN", "NOT_IN"))
+
+    qc <- tiledbsoma_empty_query_condition(somactx)
+    qc@ptr <- libtiledbsoma_query_condition_in_nin(somactx, attr_name, op_name, values)
+    qc@init <- TRUE
+    invisible(qc)
+}
---
+++ apis/r/src/query_condition.cpp
@@ -0,0 +1,220 @@
+#include <Rcpp.h>                   // for R interface to C++
+#include <nanoarrow/r.h>            // for C interface to Arrow (via R package)
+#include <RcppInt64>                // for fromInteger64
+#include <nanoarrow/nanoarrow.hpp>  // for C/C++ interface to Arrow
+
+// we currently get deprecation warnings by default which are noisy
+#ifndef TILEDB_NO_API_DEPRECATION_WARNINGS
+#define TILEDB_NO_API_DEPRECATION_WARNINGS
+#endif
+
+// We get these via nanoarrow and must cannot include carrow.h again
+#define ARROW_SCHEMA_AND_ARRAY_DEFINED 1
+#include <tiledb/tiledb_experimental>
+#include <tiledbsoma/tiledbsoma>
+
+#include "rutilities.h"  // local declarations
+#include "xptr-utils.h"  // xptr taggging utilities
+
+// Helper
+tiledb_query_condition_combination_op_t
+_tiledb_query_string_to_condition_combination_op(const std::string& opstr) {
+    if (opstr == "AND") {
+        return TILEDB_AND;
+    } else if (opstr == "OR") {
+        return TILEDB_OR;
+    } else if (opstr == "NOT") {
+        return TILEDB_NOT;
+    } else {
+        Rcpp::stop("Unknown TileDB combination op string '%s'", opstr.c_str());
+    }
+}
+
+// Helper
+tiledb_query_condition_op_t _op_name_to_tdb_op(const std::string& opstr) {
+    if (opstr == "LT") {
+        return TILEDB_LT;
+    } else if (opstr == "LE") {
+        return TILEDB_LE;
+    } else if (opstr == "GT") {
+        return TILEDB_GT;
+    } else if (opstr == "GE") {
+        return TILEDB_GE;
+    } else if (opstr == "EQ") {
+        return TILEDB_EQ;
+    } else if (opstr == "NE") {
+        return TILEDB_NE;
+    } else if (opstr == "IN") {
+        return TILEDB_IN;
+    } else if (opstr == "NOT_IN") {
+        return TILEDB_NOT_IN;
+    } else {
+        Rcpp::stop("Unknown TileDB op string '%s'", opstr.c_str());
+    }
+}
+
+// [[Rcpp::export]]
+Rcpp::XPtr<tdbs::QueryCondition> libtiledbsoma_empty_query_condition(
+    Rcpp::XPtr<somactx_wrap_t> ctxxp) {
+    // Shared pointer to SOMAContext from external pointer wrapper:
+    std::shared_ptr<tdbs::SOMAContext> sctx = ctxxp->ctxptr;
+    // Shared pointer to TileDB Context from SOMAContext:
+    std::shared_ptr<tiledb::Context> ctx = sctx->tiledb_ctx();
+    // Core constructor
+    return make_xptr<tdbs::QueryCondition>(
+        new tdbs::QueryCondition(*ctx.get()));
+}
+
+// [[Rcpp::export]]
+void libtiledbsoma_query_condition_from_triple(
+    Rcpp::XPtr<tdbs::QueryCondition> query_cond,
+    const std::string& attr_name,
+    SEXP condition_value,
+    const std::string& arrow_type_name,
+    const std::string& cond_op_string) {
+    // No such:
+    // print(arrow::large_string()$name)
+    // print(arrow::double()$name)
+
+    // print(arrow::int64()$name)         [1] "int64"
+    // print(arrow::uint64()$name)        [1] "uint64"
+    // print(arrow::int32()$name)         [1] "int32"
+    // print(arrow::uint32()$name)        [1] "uint32"
+    // print(arrow::int16()$name)         [1] "int16"
+    // print(arrow::uint16()$name)        [1] "uint16"
+    // print(arrow::int8()$name)          [1] "int8"
+    // print(arrow::uint8()$name)         [1] "uint8"
+    // print(arrow::float64()$name)       [1] "double"
+    // print(arrow::float()$name)         [1] "float"
+    // print(arrow::float32()$name)       [1] "float"
+    // print(arrow::string()$name)        [1] "utf8"
+    // print(arrow::binary()$name)        [1] "binary"
+    // print(arrow::large_binary()$name)  [1] "large_binary"
+    // print(arrow::bool()$name)          [1] "bool"
+    // print(arrow::boolean()$name)       [1] "bool"
+    // print(arrow::date64()$name)        [1] "date64"
+    // print(arrow::date32()$name)        [1] "date32"
+    // print(arrow::time32()$name)        [1] "time32"
+    // print(arrow::time64()$name)        [1] "time64"
+
+    check_xptr_tag<tdbs::QueryCondition>(query_cond);
+    tiledb_query_condition_op_t op = _op_name_to_tdb_op(cond_op_string);
+
+    if (arrow_type_name == "int64" || arrow_type_name == "uint64") {
+        int64_t v = Rcpp::fromInteger64(Rcpp::as<double>(condition_value));
+        uint64_t cond_val_size = sizeof(int64_t);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "int32" || arrow_type_name == "uint32") {
+        int v = Rcpp::as<int>(condition_value);
+        uint64_t cond_val_size = sizeof(int);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "int16" || arrow_type_name == "uint16") {
+        int v = Rcpp::as<int>(condition_value);
+        uint64_t cond_val_size = sizeof(int16_t);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "int8" || arrow_type_name == "uint8") {
+        int v = Rcpp::as<int>(condition_value);
+        uint64_t cond_val_size = sizeof(int8_t);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "double") {
+        double v = Rcpp::as<double>(condition_value);
+        uint64_t cond_val_size = sizeof(double);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "float") {
+        float v = static_cast<float>(Rcpp::as<double>(condition_value));
+        uint64_t cond_val_size = sizeof(float);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "utf8" || arrow_type_name == "large_utf8") {
+        std::string v = Rcpp::as<std::string>(condition_value);
+        query_cond->init(attr_name, v, op);
+
+    } else if (arrow_type_name == "bool") {
+        bool v = Rcpp::as<bool>(condition_value);
+        uint64_t cond_val_size = sizeof(bool);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "timestamp") {
+        // Arrow timestamp TileDB DATETIME_MS
+        int64_t v = static_cast<int64_t>(
+            Rcpp::as<double>(condition_value) * 1000);
+        uint64_t cond_val_size = sizeof(int64_t);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else if (arrow_type_name == "date32") {
+        // Arrow date32 TileDB DATETIME_DAY
+        int64_t v = static_cast<int64_t>(Rcpp::as<double>(condition_value));
+        uint64_t cond_val_size = sizeof(int64_t);
+        query_cond->init(attr_name, (void*)&v, cond_val_size, op);
+
+    } else {
+        Rcpp::stop(
+            "tiledbsoma query condition: currently unsupported type \"%s\"",
+            arrow_type_name);
+    }
+}
+
+// [[Rcpp::export]]
+Rcpp::XPtr<tdbs::QueryCondition> libtiledbsoma_query_condition_combine(
+    Rcpp::XPtr<tdbs::QueryCondition> lhs,
+    Rcpp::XPtr<tdbs::QueryCondition> rhs,
+    const std::string& str) {
+    check_xptr_tag<tdbs::QueryCondition>(lhs);
+    check_xptr_tag<tdbs::QueryCondition>(lhs);
+    tiledb_query_condition_combination_op_t
+        op = _tiledb_query_string_to_condition_combination_op(str);
+    tdbs::QueryCondition res = lhs->combine(*rhs.get(), op);
+    return make_xptr<tdbs::QueryCondition>(new tdbs::QueryCondition(res));
+}
+
+// [[Rcpp::export]]
+Rcpp::XPtr<tdbs::QueryCondition> libtiledbsoma_query_condition_in_nin(
+    Rcpp::XPtr<somactx_wrap_t> ctxxp,
+    const std::string& attr_name,
+    const std::string& op_name,
+    SEXP values) {
+    // Shared pointer to SOMAContext from external pointer wrapper:
+    std::shared_ptr<tdbs::SOMAContext> sctx = ctxxp->ctxptr;
+    // Shared pointer to TileDB Context from SOMAContext:
+    std::shared_ptr<tiledb::Context> ctx = sctx->tiledb_ctx();
+
+    tiledb_query_condition_op_t op = _op_name_to_tdb_op(op_name);
+
+    if (TYPEOF(values) == INTSXP) {
+        std::vector<int32_t> iv = Rcpp::as<std::vector<int32_t>>(values);
+        auto qc = tdbs::QueryConditionExperimental::create<int32_t>(
+            *ctx.get(), attr_name, iv, op);
+        return make_xptr<tdbs::QueryCondition>(new tdbs::QueryCondition(qc));
+
+    } else if (TYPEOF(values) == REALSXP) {
+        if (Rcpp::isInteger64(values)) {
+            std::vector<int64_t> dv = Rcpp::fromInteger64(
+                Rcpp::NumericVector(values));
+            auto qc = tdbs::QueryConditionExperimental::create<int64_t>(
+                *ctx.get(), attr_name, dv, op);
+            return make_xptr<tdbs::QueryCondition>(
+                new tdbs::QueryCondition(qc));
+        } else {
+            std::vector<double> dv = Rcpp::as<std::vector<double>>(values);
+            auto qc = tdbs::QueryConditionExperimental::create<double>(
+                *ctx.get(), attr_name, dv, op);
+            return make_xptr<tdbs::QueryCondition>(
+                new tdbs::QueryCondition(qc));
+        }
+
+    } else if (TYPEOF(values) == STRSXP) {
+        std::vector<std::string> sv = Rcpp::as<std::vector<std::string>>(
+            values);
+        auto qc = tdbs::QueryConditionExperimental::create(
+            *ctx.get(), attr_name, sv, op);
+        return make_xptr<tdbs::QueryCondition>(new tdbs::QueryCondition(qc));
+
+    } else {
+        Rcpp::stop("No support (yet) for type '%s'.", Rcpp::type2name(values));
+    }
+}
