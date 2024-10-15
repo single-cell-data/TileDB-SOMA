dac05a7c0f3571128e0463cdb73f2f064b93f7bd
Julia Dark <julia@tiledb.com>
2024-10-15 12:00:45 -0400

    Add spatial-io optional dependencies

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -341,6 +341,9 @@ setuptools.setup(
     ],
     extras_require={
         "dev": open("requirements_dev.txt").read(),
+        "spatial-io": open("requirements_spatial.txt").read(),
+        "all": open("requirements_dev.txt").read()
+        + open("requirements_spatial.txt").read(),
     },
     python_requires=">=3.9",
     cmdclass={"build_ext": build_ext, "bdist_wheel": bdist_wheel},
