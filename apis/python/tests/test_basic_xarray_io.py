1eb0c7ef6544dcebf9666ff6e9b93d1dce271915
Julia Dark <julia@tiledb.com>
2024-10-22 14:37:07 -0400

    (WIP) Create an Xarray `BackendArray` subclass for `DenseNDArray`

---
+++ apis/python/tests/test_basic_xarray_io.py
@@ -0,0 +1,148 @@
+from urllib.parse import urljoin
+
+import numpy as np
+import pyarrow as pa
+import pytest
+
+import tiledbsoma as soma
+
+soma_xarray = pytest.importorskip("tiledbsoma.experimental._xarray_backend")
+xr = pytest.importorskip("xarray")
+
+
+@pytest.fixture(scope="module")
+def sample_1d_dense_array(tmp_path_factory):
+    """Create a sample DenseNDArray with data and return open for reading."""
+    baseuri = tmp_path_factory.mktemp("basic_xarray_io").as_uri()
+    array_uri = urljoin(baseuri, "sample_1d_dense_array")
+    array = soma.DenseNDArray.create(array_uri, type=pa.uint8(), shape=(8,))
+    data = pa.Tensor.from_numpy(np.arange(8, dtype=np.uint8))
+    array.write((None,), data)
+    array.close()
+    array = soma.DenseNDArray.open(array_uri)
+    return array
+
+
+@pytest.fixture(scope="module")
+def sample_3d_dense_array(tmp_path_factory):
+    """Create a sample DenseNDArray with data and return open for reading."""
+    baseuri = tmp_path_factory.mktemp("basic_xarray_io").as_uri()
+    array_uri = urljoin(baseuri, "sample_3d_dense_array")
+    array = soma.DenseNDArray.create(array_uri, type=pa.uint8(), shape=(8, 2, 4))
+    data = pa.Tensor.from_numpy(np.reshape(np.arange(64, dtype=np.uint8), (8, 2, 4)))
+    array.write((None,), data)
+    array.close()
+    array = soma.DenseNDArray.open(array_uri)
+    return array
+
+
+class Test1DSampleDenseNDArrayWrapper:
+
+    @pytest.fixture(scope="class")
+    def soma_variable(self, sample_1d_dense_array):
+        array_wrapper = soma_xarray.DenseNDArrayWrapper(sample_1d_dense_array)
+        assert isinstance(array_wrapper, xr.backends.BackendArray)
+        return xr.Variable(
+            ("xdim",), xr.core.indexing.LazilyIndexedArray(array_wrapper)
+        )
+
+    @pytest.fixture(scope="class")
+    def numpy_variable(self):
+        data = np.arange(8, dtype=np.uint8)
+        return xr.Variable(("xdim",), data)
+
+    def test_variable_basics(self, soma_variable):
+        assert soma_variable.shape == (8,)
+        assert soma_variable.dims == ("xdim",)
+        assert isinstance(soma_variable._data, xr.core.indexing.LazilyIndexedArray)
+
+    def test_getitem_all(self, soma_variable, numpy_variable):
+        expected = numpy_variable[...]
+
+        actual = soma_variable[...]
+        assert actual.shape == (8,)
+        np.testing.assert_equal(actual.data, expected.data)
+
+        actual = soma_variable[:]
+        assert actual.shape == (8,)
+        np.testing.assert_equal(actual.data, expected.data)
+
+    @pytest.mark.parametrize(
+        "key",
+        [
+            (2,),
+            (-1,),
+            (slice(1, 2),),
+            (slice(None, None),),
+            (slice(1, -1),),
+            (slice(0, 4, 2),),
+            ([0, 3],),
+            ([0, -3],),
+        ],
+    )
+    def test_getitem(self, soma_variable, numpy_variable, key):
+        actual = soma_variable[*key]
+        expected = numpy_variable[*key]
+
+        assert actual.dims == expected.dims
+        assert actual.shape == expected.shape
+
+        np.testing.assert_equal(actual.data, expected.data)
+
+
+class TestSample3DDenseNDArrayWrapper:
+
+    @pytest.fixture(scope="class")
+    def soma_variable(self, sample_3d_dense_array):
+        array_wrapper = soma_xarray.DenseNDArrayWrapper(sample_3d_dense_array)
+        assert isinstance(array_wrapper, xr.backends.BackendArray)
+        return xr.Variable(
+            ("xdim", "ydim", "zdim"), xr.core.indexing.LazilyIndexedArray(array_wrapper)
+        )
+
+    @pytest.fixture(scope="class")
+    def numpy_variable(self):
+        data = np.reshape(np.arange(64, dtype=np.uint8), (8, 2, 4))
+        return xr.Variable(("xdim", "ydim", "zdim"), data)
+
+    def test_variable_basics(self, soma_variable):
+        assert soma_variable.shape == (8, 2, 4)
+        assert soma_variable.dims == ("xdim", "ydim", "zdim")
+        assert isinstance(soma_variable._data, xr.core.indexing.LazilyIndexedArray)
+
+    def test_getitem_all(self, soma_variable, numpy_variable):
+        expected = numpy_variable[...]
+
+        actual = soma_variable[...]
+        assert actual.shape == (8, 2, 4)
+        np.testing.assert_equal(actual.data, expected.data)
+
+        actual = soma_variable[:]
+        assert actual.shape == (8, 2, 4)
+        np.testing.assert_equal(actual.data, expected.data)
+
+        actual = soma_variable[:, :, :]
+        assert actual.shape == (8, 2, 4)
+        np.testing.assert_equal(actual.data, expected.data)
+
+    @pytest.mark.parametrize(
+        "key",
+        [
+            (2, 1, 3),
+            (0, 0, slice(1, 2)),
+            (2, 1, slice(None, None)),
+            (0, slice(None, None), 1),
+            ([1, 3], 1, 1),
+            ([1, 3], 1, [0, 2]),
+            (-1, -1, -1),
+            (0, 1, slice(0, 4, 2)),
+        ],
+    )
+    def test_getitem(self, soma_variable, numpy_variable, key):
+        actual = soma_variable[*key]
+        expected = numpy_variable[*key]
+
+        assert actual.dims == expected.dims
+        assert actual.shape == expected.shape
+
+        np.testing.assert_equal(actual.data, expected.data)
