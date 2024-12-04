49fc7281c72ee53faa5680d2efa82e4e846838c1
Paul Hoffman <paul.hoffman@tiledb.com>
2024-12-03 22:19:46 -0500

    Complete updated re-indexer

--- apis/r/R/BlockwiseIter.R
+++ apis/r/R/BlockwiseIter.R
@@ -346,35 +346,47 @@ BlockwiseSparseReadIter <- R6::R6Class(
     .zero_based = FALSE,
     soma_reader_transform = function(x) {
       tbl <- private$reindex_arrow_table(soma_array_to_arrow_table(x))
-      print(names(tbl))
       shape <- private$.shape
-      names(shape) <- names(self$coords)
-      # For re-indexed blockwise iterators, shape should reflect the re-indexed axes
-      # this can generally be the stride of the iterator, but for the end of each iterator
-      # this needs to be smaller (see ?`%%`)
-      if (self$reindexable) {
-        axes_to_reindex <- c(
-          if (self$axis %in% self$reindex_disable_on_axis) {
-            bit64::integer64()
-          } else {
-            bit64::as.integer64(self$axis)
-          },
-          self$axes_to_reindex
-        )
-        for (i in seq_along(axes_to_reindex)) {
-          ax <- axes_to_reindex[i]
-          axname <- sprintf("soma_dim_%i", as.integer(ax))
-          # self$axes_to_reindex does not contain self$axis, so this is the equivalent of
-          # ax == self$axis
-          if (!ax %in% self$axes_to_reindex) {
-            shape[axname] <- if (self$coords[[axname]]$has_next()) {
-              self$coords[[axname]]$stride
-            } else {
-              self$coords[[axname]]$end %% self$coords[[axname]]$stride
-            }
-          }
-          # TODO: Handle cases where minor axes are indexed
+      axis <- as.integer(self$axis)
+      axname <- sprintf("soma_dim_%i", axis)
+      stride <- self$coords[[axname]]$stride
+      # For re-indexed blockwise iterators, shape should reflect the re-indexed
+      # axes; this can generally be the stride of the iterator, but for the end
+      # of each iterator this needs to the remainder (see ?`%%`)
+      # Note: this only applies to the major axis, minor axes always return
+      # the full domain
+      if (self$reindexable && shape[axis + 1L] > stride) {
+        shape[axis + 1L] <- if (self$coords[[axname]]$has_next()) {
+          as.numeric(stride)
+        } else {
+          as.numeric(self$coords[[axname]]$end %% stride) + 1L
         }
+        # axes_to_reindex <- c(
+        #   if (self$axis %in% self$reindex_disable_on_axis) {
+        #     bit64::integer64()
+        #   } else {
+        #     bit64::as.integer64(self$axis)
+        #   },
+        #   self$axes_to_reindex
+        # )
+        # for (i in seq_along(axes_to_reindex)) {
+        #   ax <- axes_to_reindex[i]
+        #   axname <- sprintf("soma_dim_%i", as.integer(ax))
+        #   # self$axes_to_reindex does not contain self$axis,
+        #   # so this is the equivalent of
+        #   # ax == self$axis
+        #   # but `ax` and `self$axes_to_reindex` are integers
+        #   # while `self$axis` is an int64
+        #   if (!ax %in% self$axes_to_reindex) {
+        #     shape[axname] <- if (self$coords[[axname]]$has_next()) {
+        #       self$coords[[axname]]$stride
+        #     } else {
+        #       self$coords[[axname]]$end %% self$coords[[axname]]$stride
+        #     }
+        #     next
+        #   }
+        #   # TODO: Handle cases where minor axes are indexed
+        # }
       }
       mat <- arrow_table_to_sparse(
         tbl,
