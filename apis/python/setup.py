3f28c62949a667928af70b7deb8b9f8f3aea70cc
John Kerl <kerl.john.r@gmail.com>
2024-12-15 13:46:30 -0500

    [python] Pre-emptively avoid SciPy 1.15, pending a bugfix (#3445)
    * [python] Pre-emptively avoid scipy 1.15
    
    * code-review feedback

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -339,7 +339,9 @@ setuptools.setup(
         "pandas",
         "pyarrow",
         "scanpy>=1.9.2",
-        "scipy",
+        # https://github.com/single-cell-data/TileDB-SOMA/issues/3444
+        # https://github.com/scverse/anndata/issues/1802
+        "scipy<1.15.0",
         # Note: the somacore version is also in .pre-commit-config.yaml
         "somacore==1.0.24",
         "typing-extensions",  # Note "-" even though `import typing_extensions`
