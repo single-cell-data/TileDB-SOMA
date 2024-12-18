5d172beb8fd9e8cd5f889aa080de0ea0696a55f4
John Kerl <kerl.john.r@gmail.com>
2024-12-18 17:09:08 -0500

    [python] Re-enable `tiledbsoma.ExperimentAxisQuery`

--- apis/python/src/tiledbsoma/__init__.py
+++ apis/python/src/tiledbsoma/__init__.py
@@ -133,16 +133,17 @@ except ImportError:
     except OSError:
         # Otherwise try loading by name only.
         ctypes.CDLL(libtiledbsoma_name)
-
 from somacore import (
-    Axis,
-    CoordinateSpace,
     AffineTransform,
-    ScaleTransform,
-    IdentityTransform,
-    UniformScaleTransform,
     AxisColumnNames,
     AxisQuery,
+    CoordinateSpace,
+    IdentityTransform,
+    ScaleTransform,
+    Axis,
+    UniformScaleTransform,
+)
+from ._query import (
     ExperimentAxisQuery,
 )
 from somacore.options import ResultOrder
