5e0db116d5afba85bdf30b1dec5beda0701e09a2
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-01-02 16:44:31 +0200

    Add missing comment

--- libtiledbsoma/src/soma/soma_column.cc
+++ libtiledbsoma/src/soma/soma_column.cc
@@ -44,6 +44,25 @@ template <>
 std::pair<std::string, std::string>
 SOMAColumn::core_current_domain_slot<std::string>(
     const SOMAContext& ctx, Array& array) const {
+    // Here is an intersection of a few oddities:
+    //
+    // * Core domain for string dims must be a nullptr pair; it cannot
+    // be
+    //   anything else.
+    // * TileDB-Py shows this by using an empty-string pair, which we
+    //   imitate.
+    // * Core current domain for string dims must _not_ be a nullptr
+    // pair.
+    // * In TileDB-SOMA, unless the user specifies otherwise, we use ""
+    // for
+    //   min and "\x7f" for max. (We could use "\x7f" but that causes
+    //   display problems in Python.)
+    //
+    // To work with all these factors, if the current domain is the
+    // default
+    // "" to "\x7f", return an empty-string pair just as we do for
+    // domain. (There was some pre-1.15 software using "\xff" and it's
+    // super-cheap to check for that as well.)
     try {
         std::pair<std::string, std::string>
             current_domain = std::any_cast<std::pair<std::string, std::string>>(
