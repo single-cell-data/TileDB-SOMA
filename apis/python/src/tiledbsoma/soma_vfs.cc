5622d1f05c490ca50f748901f3040dfb558729d0
John Kerl <kerl.john.r@gmail.com>
2025-01-16 09:53:33 -0500

    Revert 8fcf89bb in favor of 3571

--- apis/python/src/tiledbsoma/soma_vfs.cc
+++ apis/python/src/tiledbsoma/soma_vfs.cc
@@ -108,10 +108,8 @@ void load_soma_vfs(py::module& m) {
             [](SOMAVFSFilebuf& buf, const std::string& uri) {
                 auto fb = buf.open(uri, std::ios::in);
                 if (fb == nullptr) {
-                    // No std::format in C++17, and fmt::format is overkill here
-                    std::stringstream ss;
-                    ss << "URI {" << uri << "} is not a valid URI";
-                    TPY_ERROR_LOC(ss.str());
+                    TPY_ERROR_LOC(
+                        std::format("URI {} is not a valid URI", uri));
                 }
                 return fb;
             },
