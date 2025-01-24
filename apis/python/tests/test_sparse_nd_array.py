9958d8c2f6a194a44f9b24ed365d5699cb0e29e7
Vivian Nguyen <vivian@tiledb.com>
2025-01-24 09:00:12 -0800

    format

--- apis/python/tests/test_sparse_nd_array.py
+++ apis/python/tests/test_sparse_nd_array.py
@@ -2030,7 +2030,7 @@ def test_sparse_nd_array_null(tmp_path):
 
     pydict["soma_dim_0"] = pa.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     table = pa.Table.from_pydict(pydict)
-    
+
     with soma.SparseNDArray.open(uri, "w") as A:
         A.write(table[:5])
         A.write(table[5:])
