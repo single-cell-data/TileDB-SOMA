a1ba80efeb54dda271920ecccee321f88606ac6a
John Kerl <kerl.john.r@gmail.com>
2025-02-04 18:21:18 -0500

    [Backport release-1.15] [python] Resolve issue with #3611 and `pyarrow<17` (#3671)
    * [Backport release-1.15] [python] Resolve issue with #3611 and `pyarrow<17`
    
    * Fix issue with pyarrow<17

--- libtiledbsoma/src/soma/column_buffer.cc
+++ libtiledbsoma/src/soma/column_buffer.cc
@@ -157,9 +157,13 @@ void ColumnBuffer::attach(Query& query, std::optional<Subarray> subarray) {
             "to attach to Query");
     }
 
+    // As of version 1.15.6 we were throwing here. However, we found a
+    // compatibility issue with pyarrow versions below 17. Thus we log and
+    // continue.
     if (!validity_.empty() && is_dim) {
-        throw TileDBSOMAError(fmt::format(
-            "[ColumnBuffer::attach] Validity buffer passed for dimension '{}'",
+        LOG_DEBUG(fmt::format(
+            "[ColumnBuffer::attach] Validity buffer passed for dimension '{}' "
+            "is being ignored",
             name_));
     }
 
