57c3afbb7aafc626ece15889dc6a959429b0ee5d
Ryan Williams <ryan.williams@tiledb.com>
2025-02-05 09:58:37 -0500

    comment fixes

--- apis/python/tests/test_dense_nd_array.py
+++ apis/python/tests/test_dense_nd_array.py
@@ -550,7 +550,7 @@ def test_pass_configs(tmp_path):
     ) as sdf:
 
         # This errors out as 0 is not a valid value to set the total memory
-        # budget or nummber of threads
+        # budget or number of threads
         with pytest.raises(soma.SOMAError):
             sdf.read()
 
