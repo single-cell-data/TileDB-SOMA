27317c0add3ced70c592febd85e06da87d26c182
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-02-11 13:31:43 +0200

    Python implementation of transformers

--- apis/python/src/tiledbsoma/pytiledbsoma.cc
+++ apis/python/src/tiledbsoma/pytiledbsoma.cc
@@ -31,6 +31,7 @@ void load_reindexer(py::module&);
 void load_soma_vfs(py::module&);
 void load_managed_query(py::module&);
 void load_fastercsx(py::module&);
+void load_transformers(py::module&);
 
 PYBIND11_MODULE(pytiledbsoma, m) {
     py::register_exception<TileDBSOMAError>(m, "SOMAError");
@@ -175,6 +176,7 @@ PYBIND11_MODULE(pytiledbsoma, m) {
     load_soma_vfs(m);
     load_managed_query(m);
     load_fastercsx(m);
+    load_transformers(m);
 }
 
 };  // namespace libtiledbsomacpp
