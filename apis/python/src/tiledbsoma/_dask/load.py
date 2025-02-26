ab50b03b78cba3c03040d82a7b680e0a3f083120
Ryan Williams <ryan.williams@tiledb.com>
2025-02-26 16:30:08 +0000

    prototype Dask export

---
+++ apis/python/src/tiledbsoma/_dask/load.py
@@ -0,0 +1,184 @@
+# Copyright (c) TileDB, Inc. and The Chan Zuckerberg Initiative Foundation
+#
+# Licensed under the MIT License.
+
+"""Utilities for loading a TileDB-SOMA ``X`` matrix as a Dask Array.
+"""
+
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+)
+
+import numpy as np
+import numpy.typing as npt
+import pyarrow as pa
+import scipy.sparse as sp
+from somacore.options import PlatformConfig, ResultOrder, ResultOrderStr, SparseNDCoords
+
+from tiledbsoma._dask.util import (
+    ChunkSize,
+    DaskConfig,
+    JoinIDs,
+    chunk_ids_sizes,
+    coords_to_joinids,
+    make_context,
+)
+from tiledbsoma._dense_nd_array import DenseNDArray
+from tiledbsoma._fastercsx import CompressedMatrix, Format
+from tiledbsoma._indexer import IntIndexer
+from tiledbsoma._sparse_nd_array import SparseNDArray
+from tiledbsoma.options._soma_tiledb_context import ConfigDict, ConfigVal
+
+if TYPE_CHECKING:
+    try:
+        import dask.array as da
+    except ImportError:
+        pass
+
+
+__all__ = [
+    # Re-export `DaskConfig` here, to save most users from needing separate `tiledbsoma._dask` import lines. It also
+    # appears in the public API of `SparseNDArrayRead.dask_array`, which imports it directly from
+    # `tiledbsoma._dask.util` (to avoid a circular dependency, since `SparseNDArray` is also referenced in this file).
+    "DaskConfig",
+    "load_daskarray",
+    "sparse_chunk",
+]
+
+
+def sparse_chunk(
+    block: npt.NDArray[tuple[JoinIDs, JoinIDs]],
+    block_info: dict[int | None, Any],
+    uri: str,
+    tiledb_config: ConfigDict,
+    tdb_concurrency: int | None = None,
+    format: Format = "csr",
+    result_order: ResultOrderStr = ResultOrder.AUTO,
+    platform_config: PlatformConfig | None = None,
+) -> sp.csr_matrix | sp.csc_matrix:
+    """Load a slice of a TileDB-SOMA ``X`` matrix, as a block of a CSR-backed Dask Array."""
+    shape = block_info[None]["chunk-shape"]
+    assert block.shape == (1, 1)
+    joinids = block[0, 0]
+    obs_joinids, var_joinids = joinids
+    obs_joinids = obs_joinids.astype("int64")
+    var_joinids = var_joinids.astype("int64")
+    # TODO: create sequence of tables for CM.from_soma; use record batches (idiomatic Arrow)
+
+    soma_ctx = make_context(
+        tiledb_config=tiledb_config,
+        tdb_concurrency=tdb_concurrency,
+    )
+    with SparseNDArray.open(uri, context=soma_ctx) as arr:
+        tbls = arr.read(
+            coords=(obs_joinids, var_joinids),
+            result_order=result_order,
+            platform_config=platform_config,
+        ).tables()
+        # TODO: i32-based reindexing?
+        obs_indexer = IntIndexer(obs_joinids, context=soma_ctx)
+        var_indexer = IntIndexer(var_joinids, context=soma_ctx)
+        new_tbls = []
+        for tbl in tbls:
+            new_dim0 = obs_indexer.get_indexer(tbl["soma_dim_0"]).astype("int32")
+            new_dim1 = var_indexer.get_indexer(tbl["soma_dim_1"]).astype("int32")
+            new_tbl = pa.Table.from_pydict(
+                {
+                    "soma_dim_0": new_dim0,
+                    "soma_dim_1": new_dim1,
+                    "soma_data": tbl["soma_data"],
+                }
+            )
+            new_tbls.append(new_tbl)
+        cm = CompressedMatrix.from_soma(
+            new_tbls,
+            shape=shape,
+            format=format,
+            make_sorted=True,
+            context=soma_ctx,
+        )
+        csx = cm.to_scipy()
+        return csx
+
+
+def load_daskarray(
+    layer: SparseNDArray | DenseNDArray,
+    coords: SparseNDCoords | None = None,
+    config: DaskConfig | None = None,
+    chunk_size: ChunkSize | None = None,
+    tdb_concurrency: int | None = None,
+    tdb_configs: dict[str, ConfigVal] | None = None,
+    format: Format = "csr",
+    result_order: ResultOrderStr = ResultOrder.AUTO,
+    platform_config: PlatformConfig | None = None,
+) -> "da.Array":
+    """Load a TileDB-SOMA X layer as a Dask array."""
+    import dask.array as da
+
+    if config:
+        chunk_size = chunk_size if chunk_size is not None else config.chunk_size
+        tdb_concurrency = (
+            tdb_concurrency if tdb_concurrency is not None else config.tdb_concurrency
+        )
+        tdb_configs = tdb_configs if tdb_configs is not None else config.tdb_configs
+    else:
+        if chunk_size is None:
+            raise ValueError("chunk_size required (directly or via `config`)")
+        if tdb_concurrency is None:
+            tdb_concurrency = 1
+
+    _, _, data_dtype = layer.schema.types
+    dtype = data_dtype.to_pandas_dtype()
+    nobs, nvar = layer.shape
+
+    obs_ids, var_ids = coords_to_joinids(coords, shape=layer.shape)
+
+    if isinstance(chunk_size, int):
+        obs_chunk_size = chunk_size
+        var_chunk_size = nvar
+    else:
+        _obs_chunk_size, var_chunk_size = chunk_size
+        obs_chunk_size = _obs_chunk_size or nobs
+
+    obs_chunk_joinids, obs_chunk_sizes = chunk_ids_sizes(obs_ids, obs_chunk_size, nobs)
+    var_chunk_joinids, var_chunk_sizes = chunk_ids_sizes(var_ids, var_chunk_size, nvar)
+
+    arr = np.empty((len(obs_chunk_joinids), len(var_chunk_joinids)), dtype=object)
+    for obs_chunk_idx, obs_chunk_ids in enumerate(obs_chunk_joinids):
+        for var_chunk_idx, var_chunk_ids in enumerate(var_chunk_joinids):
+            arr[obs_chunk_idx, var_chunk_idx] = (obs_chunk_ids, var_chunk_ids)
+
+    if isinstance(layer, SparseNDArray):
+        if tdb_configs:
+            tiledb_config = {**layer.context.tiledb_config}
+            tiledb_config.update(**tdb_configs)
+        else:
+            tiledb_config = layer.context.tiledb_config
+
+        chunk_joinids = da.from_array(arr, chunks=(1, 1))
+        meta = (
+            sp.csr_matrix((0, 0), dtype=dtype)
+            if format == "csr"
+            else sp.csc_matrix((0, 0), dtype=dtype)
+        )
+        X = da.map_blocks(
+            sparse_chunk,
+            chunk_joinids,
+            chunks=(tuple(obs_chunk_sizes), tuple(var_chunk_sizes)),
+            meta=meta,
+            uri=layer.uri,
+            format=format,
+            tdb_concurrency=tdb_concurrency,
+            tiledb_config=tiledb_config,
+            result_order=result_order,
+            platform_config=platform_config,
+        )
+    else:
+        # TODO: combine with Spatial DenseNDArray Dask code
+        raise NotImplementedError(
+            f"Dask-loading not implemented yet for DenseNDArray ({layer.uri})"
+        )
+    return X
--- apis/python/src/tiledbsoma/_sparse_nd_array.py
+++ apis/python/src/tiledbsoma/_sparse_nd_array.py
@@ -9,6 +9,7 @@ Implementation of SOMA SparseNDArray.
 from __future__ import annotations
 
 from typing import (
+    TYPE_CHECKING,
     Sequence,
     Tuple,
     Union,
@@ -28,6 +29,7 @@ from . import _util
 from . import pytiledbsoma as clib
 from ._arrow_types import pyarrow_to_carrow_type
 from ._common_nd_array import NDArray
+from ._dask.util import DaskConfig
 from ._exception import SOMAError, map_exception_for_create
 from ._read_iters import (
     BlockwiseScipyReadIter,
@@ -46,6 +48,13 @@ from .options._tiledb_create_write_options import (
     TileDBCreateOptions,
     TileDBWriteOptions,
 )
+
+if TYPE_CHECKING:
+    try:
+        import dask.array as da
+    except ImportError:
+        pass
+
 
 _UNBATCHED = options.BatchSize()
 
@@ -462,6 +471,21 @@ class SparseNDArrayRead(_SparseNDArrayReadBase):
     Lifecycle:
         Maturing.
     """
+
+    def dask_array(
+        self,
+        config: DaskConfig,
+    ) -> "da.Array":
+        """Load a TileDB-SOMA X layer as a Dask array."""
+        from tiledbsoma._dask.load import load_daskarray
+
+        return load_daskarray(
+            layer=self.array,
+            coords=self.coords,
+            result_order=self.result_order,
+            platform_config=self.platform_config,
+            config=config,
+        )
 
     def coos(self, shape: NTuple | None = None) -> SparseCOOTensorReadIter:
         """
