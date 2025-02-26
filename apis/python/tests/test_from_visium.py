100c13f288f64429ae6e8713da75f9530fa16b9f
Julia Dark <julia@tiledb.com>
2025-02-26 10:21:19 -0500

    Add Visium v1 test

--- apis/python/tests/test_from_visium.py
+++ apis/python/tests/test_from_visium.py
@@ -91,6 +91,68 @@ def test_visium_paths_v2(visium_v2_path):
 
 
 @pytest.mark.slow
+def test_from_visium_for_visium_v1(tmp_path, visium_v1_path):
+    """Test `from_visium` runs without error."""
+    PIL = pytest.importorskip("PIL")
+    uri = f"{tmp_path.as_uri()}/from_visium_for_visium_v2"
+    exp_uri = spatial_io.from_visium(
+        uri,
+        visium_v1_path,
+        "RNA",
+        "fresh_frozen_mouse_brain",
+        write_obs_spatial_presence=True,
+        write_var_spatial_presence=True,
+    )
+    with soma.Experiment.open(exp_uri) as exp:
+
+        # Check for the existance of obs, RNA/X, and RNA/var
+        assert isinstance(exp.obs, soma.DataFrame)
+        assert isinstance(exp.ms["RNA"].X["data"], soma.SparseNDArray)
+        assert isinstance(exp.ms["RNA"].var, soma.DataFrame)
+
+        # Check for the existance of the presence matrices.
+        assert isinstance(exp.obs_spatial_presence, soma.DataFrame)
+        assert isinstance(exp.ms["RNA"].var_spatial_presence, soma.DataFrame)
+
+        # Check for scene.
+        assert isinstance(exp.spatial["fresh_frozen_mouse_brain"], soma.Scene)
+
+        # Check expected datatypes in scene.
+        scene = exp.spatial["fresh_frozen_mouse_brain"]
+        assert isinstance(scene.obsl["loc"], soma.PointCloudDataFrame)
+        assert len(scene.varl.items()) == 0
+        assert isinstance(scene.img["tissue"], soma.MultiscaleImage)
+
+        # Check point cloud dataframe data.
+        output_points_df = scene.obsl["loc"].read().concat().to_pandas()
+        assert output_points_df.columns.tolist() == [
+            "x",
+            "y",
+            "soma_joinid",
+            "in_tissue",
+            "array_row",
+            "array_col",
+            "spot_diameter_fullres",
+        ]
+        assert len(output_points_df) == 4035
+
+        # Check image.
+        image = scene.img["tissue"]
+        hires_data = np.moveaxis(image["hires"].read().to_numpy(), 0, -1)
+        with PIL.Image.open(
+            visium_v1_path / "spatial" / "tissue_hires_image.png"
+        ) as input_hires:
+            expected = np.array(input_hires)
+            np.testing.assert_equal(expected, hires_data)
+        lowres_data = np.moveaxis(image["lowres"].read().to_numpy(), 0, -1)
+        with PIL.Image.open(
+            visium_v1_path / "spatial" / "tissue_lowres_image.png"
+        ) as input_lowres:
+            expected = np.array(input_lowres)
+            np.testing.assert_equal(expected, lowres_data)
+
+
+@pytest.mark.slow
 def test_from_visium_for_visium_v2(tmp_path, visium_v2_path):
     """Test `from_visium` runs without error."""
     PIL = pytest.importorskip("PIL")
