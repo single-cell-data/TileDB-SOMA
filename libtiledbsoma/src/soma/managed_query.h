b55b6fca66b517708d90a68a6b5a98f6519847c0
Vivian Nguyen <vivian@tiledb.com>
2025-03-05 09:03:51 -0800

    [python/c++] Allow GOW multi-submit for single commit

--- libtiledbsoma/src/soma/managed_query.h
+++ libtiledbsoma/src/soma/managed_query.h
@@ -400,7 +400,29 @@ class ManagedQuery {
      * @brief Submit the write query.
      *
      */
-    void submit_write(bool sort_coords = true);
+    void submit_write() {
+        _setup_write();
+        query_->submit();
+        _teardown_write();
+    }
+
+    /**
+     * @brief Finalize the write query.
+     *
+     */
+    void finalize() {
+        query_->finalize();
+    }
+
+    /**
+     * @brief Submit and finalize the write query.
+     *
+     */
+    void submit_and_finalize() {
+        _setup_write();
+        query_->submit_and_finalize();
+        _teardown_write();
+    }
 
     /**
      * @brief Get the schema of the array.
@@ -1049,6 +1071,19 @@ class ManagedQuery {
                 return std::memcmp(&target, &candidate, sizeof(T)) == 0;
             });
     }
+
+    /**
+     * @brief Check if the array is opened in write mode and set the subarray
+     * for dense arrays.
+     */
+    void _setup_write();
+
+    /**
+     * @brief Reset the ManagedQuery options and update the ArraySchema
+     * on-disk by re-opening the Array (required when doing schema evolution
+     * when extending enumerations).
+     */
+    void _teardown_write();
 };
 
 // These are all specializations to string/bool of various methods
