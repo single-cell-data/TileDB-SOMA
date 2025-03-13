423cc5c9ec807bf9855e4e61ea3f981283c0b594
Paul Hoffman <paul.hoffman@tiledb.com>
2025-03-13 10:17:03 -0400

    [r] [WIP] Replace `TileDB*` base classes with `SOMA*` base classes Replace the various tiledb-r wrapper base classes with new and expanded SOMA base classes:
     - `TileDBObject` replaced by new `SOMAObject`
     - `TileDBArray` replaced by expanded `SOMAArrayBase`
     - `TileDBGroup` replaced by expanded `SOMACollectionBase`
    
    [SC-64345](https://app.shortcut.com/tiledb-inc/story/64345)
    
    continues work for #2406

--- apis/r/R/SOMAArrayBase.R
+++ apis/r/R/SOMAArrayBase.R
@@ -1,14 +1,37 @@
 #' SOMA Array Base Class
 #'
-#' Adds SOMA-specific functionality to the [`TileDBArray`] class.  (lifecycle:
-#' maturing)
+#' Adds SOMA-specific functionality to the [`TileDBArray`] class.
+#' (lifecycle: maturing)
+#'
 #' @keywords internal
-
+#'
 SOMAArrayBase <- R6::R6Class(
   classname = "SOMAArrayBase",
   inherit = TileDBArray,
+  public = list(
+    #' @description Retrieve the array schema as an Arrow schema
+    #' (lifecycle: maturing)
+    #'
+    #' @return An Arrow \code{\link[arrow:Schema]{Schema}} object
+    #'
+    schema = \() arrow::as_schema(c_schema(self$uri, private$.soma_context)),
+
+    #' @description Retrieve attribute names (lifecycle: maturing)
+    #'
+    #' @return A character vector with the array's attribute names
+    #'
+    attrnames = \() c_attrnames(self$uri, private$.soma_context),
+
+    #' @description Retrieve dimension names (lifecycle: maturing)
+    #' @return A character vector with the array's dimension names
+    dimnames = function() {
+      c_dimnames(self$uri, private$.soma_context)
+    }
+
+  ),
   active = list(
     #' @field soma_type Retrieve the SOMA object type.
+    #'
     soma_type = function(value) {
       stopifnot("'soma_type' is a read-only field" = missing(value))
       if (is.null(private$soma_type_cache)) {
@@ -18,7 +41,6 @@ SOMAArrayBase <- R6::R6Class(
     }
   ),
   private = list(
-
     # Cache object's SOMA_OBJECT_TYPE_METADATA_KEY
     soma_type_cache = NULL,
     update_soma_type_cache = function() {
