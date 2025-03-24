f4472d33bef8393d16f930d9c1fd4a108e87dcfa
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-03-24 21:23:30 +0200

    Use `TenXCountMatrixReader`

--- apis/python/tests/test_from_xenium.py
+++ apis/python/tests/test_from_xenium.py
@@ -1,7 +1,5 @@
 import os
 
-import numpy as np
-import pandas as pd
 import pytest
 
 import tiledbsoma as soma
@@ -25,7 +23,7 @@ def xenium_v1_path():
         "cells.parquet",
         "cell_boundaries.parquet",
         "nucleus_boundaries.parquet",
-        "experiment.xenium"
+        "experiment.xenium",
     ]:
         if not os.path.isfile(xenium_path / filename):
             raise RuntimeError(
@@ -35,19 +33,6 @@ def xenium_v1_path():
             )
 
     return xenium_path
-
-
-def test_visium_paths_v1(visium_v1_path):
-    """Test ``VisiumPaths`` for Visium v1 in standard structure."""
-    visium_paths = spatial_io.XeniumPaths.from_base_folder(visium_v1_path)
-    assert os.path.isfile(visium_paths.gene_expression)
-    assert os.path.isfile(visium_paths.tissue_positions)
-    assert visium_paths.fullres_image is None
-    assert os.path.isfile(visium_paths.hires_image)
-    assert os.path.isfile(visium_paths.lowres_image)
-    assert visium_paths.version == (1, 4, 0)
-    assert visium_paths.has_image
-    assert visium_paths.major_version == 1
 
 
 @pytest.mark.slow
@@ -98,122 +83,3 @@ def test_from_xenium_for_xenium_v1(tmp_path, xenium_v1_path):
         assert isinstance(loc_transform, soma.IdentityTransform)
         assert loc_transform.input_axes == ("x", "y")
         assert loc_transform.output_axes == ("x", "y")
-
-        # Check transform to `img`.
-        img_transform = scene.get_transform_to_multiscale_image("tissue", "img")
-        assert isinstance(img_transform, soma.ScaleTransform)
-        assert img_transform.input_axes == ("x", "y")
-        assert img_transform.output_axes == ("x", "y")
-        np.testing.assert_allclose(
-            img_transform.scale_factors,
-            np.array([0.170105, 0.170111], dtype=np.float64),
-            atol=0.000001,
-        )
-
-        # Check spot locations.
-        loc = scene.obsl["loc"]
-        assert loc.coordinate_space == pixel_coord_space
-        actual_loc_data = loc.read().concat().to_pandas()
-        expected_loc_data = pd.read_csv(visium_v1_path / "filtered_visium1_loc.csv")
-        pd.testing.assert_frame_equal(actual_loc_data, expected_loc_data)
-
-        # Check tissue image.
-        image = scene.img["tissue"]
-        assert image.coordinate_space == pixel_coord_space
-        hires_data = np.moveaxis(image["hires"].read().to_numpy(), 0, -1)
-        with PIL.Image.open(
-            visium_v1_path / "spatial" / "tissue_hires_image.png"
-        ) as input_hires:
-            expected = np.array(input_hires)
-            np.testing.assert_equal(expected, hires_data)
-        lowres_data = np.moveaxis(image["lowres"].read().to_numpy(), 0, -1)
-        with PIL.Image.open(
-            visium_v1_path / "spatial" / "tissue_lowres_image.png"
-        ) as input_lowres:
-            expected = np.array(input_lowres)
-            np.testing.assert_equal(expected, lowres_data)
-
-
-@pytest.mark.slow
-def test_from_visium_for_visium_v2(tmp_path, visium_v2_path):
-    """Test `from_visium` runs without error."""
-    PIL = pytest.importorskip("PIL")
-    uri = f"{tmp_path.as_uri()}/from_visium_for_visium_v2"
-    exp_uri = spatial_io.from_visium(
-        uri,
-        visium_v2_path,
-        "RNA",
-        "fresh_frozen_mouse_brain",
-        write_obs_spatial_presence=True,
-        write_var_spatial_presence=True,
-    )
-    with soma.Experiment.open(exp_uri) as exp:
-
-        # Check for the existance of obs, RNA/X, and RNA/var
-        assert isinstance(exp.obs, soma.DataFrame)
-        assert isinstance(exp.ms["RNA"].X["data"], soma.SparseNDArray)
-        assert isinstance(exp.ms["RNA"].var, soma.DataFrame)
-
-        # Check for the existance of the presence matrices.
-        assert isinstance(exp.obs_spatial_presence, soma.DataFrame)
-        assert isinstance(exp.ms["RNA"].var_spatial_presence, soma.DataFrame)
-
-        # Define some expected data.
-        pixel_coord_space = soma.CoordinateSpace(
-            (soma.Axis("x", "pixels"), soma.Axis("y", "pixels"))
-        )
-
-        # Check the scene exists and has the desired metadata.
-        assert isinstance(exp.spatial["fresh_frozen_mouse_brain"], soma.Scene)
-        scene = exp.spatial["fresh_frozen_mouse_brain"]
-        assert scene.coordinate_space == pixel_coord_space
-
-        # Check the scene subcollections:
-        # - `obsl` only has a point cloud dataframe named `loc`,
-        # - `varl` is empty,
-        # - `img` only has a multiscale image named `tissue`.
-        assert len(scene.obsl.items()) == 1
-        assert isinstance(scene.obsl["loc"], soma.PointCloudDataFrame)
-        assert len(scene.varl.items()) == 0
-        assert len(scene.img.items()) == 1
-        assert isinstance(scene.img["tissue"], soma.MultiscaleImage)
-
-        # Check transform to `loc`.
-        loc_transform = scene.get_transform_to_point_cloud_dataframe("loc", "obsl")
-        assert isinstance(loc_transform, soma.IdentityTransform)
-        assert loc_transform.input_axes == ("x", "y")
-        assert loc_transform.output_axes == ("x", "y")
-
-        # Check transform to `img`.
-        img_transform = scene.get_transform_to_multiscale_image("tissue", "img")
-        assert isinstance(img_transform, soma.ScaleTransform)
-        assert img_transform.input_axes == ("x", "y")
-        assert img_transform.output_axes == ("x", "y")
-        np.testing.assert_allclose(
-            img_transform.scale_factors,
-            np.array([0.150015, 0.150015], dtype=np.float64),
-            atol=0.000001,
-        )
-
-        # Check point cloud dataframe data.
-        loc = scene.obsl["loc"]
-        assert loc.coordinate_space == pixel_coord_space
-        actual_loc_data = loc.read().concat().to_pandas()
-        expected_loc_data = pd.read_csv(visium_v2_path / "filtered_visium2_loc.csv")
-        pd.testing.assert_frame_equal(actual_loc_data, expected_loc_data)
-
-        # Check image.
-        image = scene.img["tissue"]
-        assert image.coordinate_space == pixel_coord_space
-        hires_data = np.moveaxis(image["hires"].read().to_numpy(), 0, -1)
-        with PIL.Image.open(
-            visium_v2_path / "spatial" / "tissue_hires_image.png"
-        ) as input_hires:
-            expected = np.array(input_hires)
-            np.testing.assert_equal(expected, hires_data)
-        lowres_data = np.moveaxis(image["lowres"].read().to_numpy(), 0, -1)
-        with PIL.Image.open(
-            visium_v2_path / "spatial" / "tissue_lowres_image.png"
-        ) as input_lowres:
-            expected = np.array(input_lowres)
-            np.testing.assert_equal(expected, lowres_data)
