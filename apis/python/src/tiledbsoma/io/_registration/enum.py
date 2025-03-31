d457fdf556290377a03f8b5d2669e169a1bd4c5e
bkmartinjr <bruce.martin@tiledb.com>
2025-03-31 09:12:17 -0700

    add new extend enum code (commented out)

--- apis/python/src/tiledbsoma/io/_registration/enum.py
+++ apis/python/src/tiledbsoma/io/_registration/enum.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import numpy as np
 import pandas as pd
 import pyarrow as pa
 
@@ -16,9 +15,28 @@ def _get_enumeration(df: DataFrame, column_name: str) -> pd.CategoricalDtype:
 def _extend_enumeration(df: DataFrame, column_name: str, values: pa.Array) -> None:
 
     # first confirm we are a dictionary. If we have been decategorical-ized, we
-    # will just be a plain-ol-array of value type.
+    # will just be an array of value type.
     if not pa.types.is_dictionary(df.schema.field(column_name).type):
         return
+
+    ## NEW API (not yet merged)
+
+    # # determine if we have any new enum values
+    # existing_dtype = _get_enumeration(df, column_name)
+    # new_enum_values = pd.Index(values).difference(existing_dtype.categories, sort=False)
+    # if len(new_enum_values) == 0:
+    #     return
+
+    # new_enum_values = pa.array(new_enum_values.to_numpy())
+    # if df.mode == "w":
+    #     df.extend_enumeration_values({column_name: new_enum_values}, deduplicate=False)
+    # else:
+    #     with df.reopen(mode="w") as wdf:
+    #         wdf.extend_enumeration_values(
+    #             {column_name: new_enum_values}, deduplicate=False
+    #         )
+
+    import numpy as np
 
     import tiledb
 
