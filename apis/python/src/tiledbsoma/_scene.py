71cf0eea3524975aa46df8e4e174a9ab0c42e05b
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-03-31 13:59:49 +0300

    Add subcollection type info

--- apis/python/src/tiledbsoma/_scene.py
+++ apis/python/src/tiledbsoma/_scene.py
@@ -663,7 +663,7 @@ class Scene(  # type: ignore[misc]   # __eq__ false positive
         return base_transform.inverse_transform() @ level_transform
 
     def get_transform_from_point_cloud_dataframe(
-        self, key: str, subcollection: str = "obsl"
+        self, key: str, subcollection: Union[str, Sequence[str]] = "obsl"
     ) -> CoordinateTransform:
         """Returns the coordinate transformation from the requested point cloud
         dataframe to the scene.
@@ -758,7 +758,7 @@ class Scene(  # type: ignore[misc]   # __eq__ false positive
         return level_transform @ base_transform
 
     def get_transform_to_point_cloud_dataframe(
-        self, key: str, subcollection: str = "obsl"
+        self, key: str, subcollection: Union[str, Sequence[str]] = "obsl"
     ) -> CoordinateTransform:
         """Returns the coordinate transformation from the scene to a requested
         point cloud dataframe.
