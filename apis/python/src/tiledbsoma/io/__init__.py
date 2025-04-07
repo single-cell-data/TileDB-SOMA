f4631b9c45e3a6762009c9b1db08102d1809e4ac
John Kerl <kerl.john.r@gmail.com>
2025-04-07 15:45:21 -0400

    [python] Protect against an AnnData/SciPy incompatibility

--- apis/python/src/tiledbsoma/io/__init__.py
+++ apis/python/src/tiledbsoma/io/__init__.py
@@ -2,6 +2,10 @@
 #
 # Licensed under the MIT License.
 
+import anndata
+import scipy
+from packaging.version import Version
+
 from ._registration import (
     ExperimentAmbientLabelMapping,
 )
@@ -29,6 +33,15 @@ from .shaping import (
     show_experiment_shapes,
     upgrade_experiment_shapes,
 )
+
+# https://github.com/single-cell-data/TileDB-SOMA/issues/3920
+scipy_version = Version(scipy.__version__)
+anndata_version = Version(anndata.__version__)
+if anndata_version >= Version("0.10.7") and anndata_version < Version("0.11.2"):
+    if scipy_version >= Version("1.15.0"):
+        raise RuntimeError(
+            f"anndata '{anndata.__version__}' is incompatible with '{scipy.__version__}', and more generally, anndata 0.10.7-0.11.1 are incompatible with scipy 0.15. Please upgrade your anndata to >= 0.11.2, or downgrade your scipy to < 0.15."
+        )
 
 __all__ = (
     "add_matrix_to_collection",
