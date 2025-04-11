2b2984ad07cbd04ed9c8096be86a1da82285a67d
John Kerl <kerl.john.r@gmail.com>
2025-04-11 17:43:26 -0400

    [python] Test `get_experiment_shapes` for underpopulated experiments

--- apis/python/src/tiledbsoma/io/shaping.py
+++ apis/python/src/tiledbsoma/io/shaping.py
@@ -11,7 +11,6 @@ https://github.com/single-cell-data/TileDB-SOMA/issues/2407."""
 from __future__ import annotations
 
 import io
-import json
 import sys
 from collections.abc import Callable
 from typing import Any, Dict, Tuple, TypeVar, Union, cast
@@ -195,7 +194,6 @@ def show_experiment_shapes(
         output_handle=output_handle,
         context=context,
     )
-    print(json.dumps(retval, indent=2))
     return _check_statuses(retval)
 
 
@@ -473,14 +471,17 @@ def _treewalk(
         # Non-terminal
         if isinstance(item, tiledbsoma.Experiment):
             retval = {}
-            retval["obs"] = _recurse(item, node_name="obs", **kwargs)
-            retval["ms"] = _recurse(item, node_name="ms", **kwargs)
+            if "obs" in item:
+                retval["obs"] = _recurse(item, node_name="obs", **kwargs)
+            if "ms" in item:
+                retval["ms"] = _recurse(item, node_name="ms", **kwargs)
             return retval
 
         # Non-terminal
         if isinstance(item, tiledbsoma.Measurement):
             retval = {}
-            retval["var"] = _recurse(item, node_name="var", **kwargs)
+            if "var" in item:
+                retval["var"] = _recurse(item, node_name="var", **kwargs)
             for iter_coll_name in ["X", "obsm", "obsp", "varm", "varp"]:
                 kwargs["coll_name"] = iter_coll_name
                 children_dict = _recurse(item, node_name=iter_coll_name, **kwargs)
