ae42b3205b05c6f5ab136bec7bc093e78510dd93
Vivian Nguyen <vivian@tiledb.com>
2025-04-21 20:44:56 -0700

    Also construct_member in SOMACollectionBase

--- apis/r/R/SOMACollectionBase.R
+++ apis/r/R/SOMACollectionBase.R
@@ -211,6 +211,16 @@ SOMACollectionBase <- R6::R6Class(
         SOMAArray = TileDBArray$new,
         GROUP = TileDBGroup$new,
         SOMAGroup = TileDBGroup$new,
+        SOMADataFrame = TileDBArray$new,
+        SOMADenseNDArray = TileDBArray$new,
+        SOMAGeometryDataFrame = TileDBArray$new,
+        SOMAPointCloudDataFrame = TileDBArray$new,
+        SOMASparseNDArray = TileDBArray$new,
+        SOMACollection = TileDBGroup$new,
+        SOMAExperiment = TileDBGroup$new,
+        SOMAMeasurement = TileDBGroup$new,
+        SOMAScene = TileDBGroup$new,
+        SOMAMultiscaleImage = TileDBGroup$new,
         stop(sprintf("Unknown member TileDB type: %s", type), call. = FALSE)
       )
 
