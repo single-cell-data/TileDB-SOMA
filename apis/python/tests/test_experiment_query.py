0af3421201c1e315c9f9c699a2ed26afbc201554
bkmartinjr <bruce.martin@tiledb.com>
2025-06-24 11:38:07 -0700

    debug attempt 11

--- apis/python/tests/test_experiment_query.py
+++ apis/python/tests/test_experiment_query.py
@@ -903,7 +903,7 @@ def test_experiment_query_historical(version, obs_params, var_params):
         assert len(var) == var_count
 
 
-@pytest.mark.parametrize("version", ["1.14.5"])  # ["1.7.3", "1.12.3", "1.14.5", "1.15.0", "1.15.7"])
+@pytest.mark.parametrize("version", ["1.7.3", "1.12.3", "1.14.5"])  # ["1.7.3", "1.12.3", "1.14.5", "1.15.0", "1.15.7"])
 @pytest.mark.parametrize("obsm_layers", [(), ("X_pca",), ("X_tsne",), ("X_draw_graph_fr", "X_pca", "X_tsne", "X_umap")])
 @pytest.mark.parametrize("obsp_layers", [(), ("connectivities",), ("distances",)])
 @pytest.mark.parametrize("varp_layers", [()])
