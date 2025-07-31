66084893cfb619e99f5290fb59bb26f313870fe7
Julia Dark <julia@tiledb.com>
2025-07-31 16:36:18 -0400

    Add initial pybind for constructing selection

--- apis/python/src/tiledbsoma/_coordinate_selection.py
+++ apis/python/src/tiledbsoma/_coordinate_selection.py
@@ -8,24 +8,29 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Sequence
 
+import attrs
 import numpy as np
 import pyarrow as pa
 from somacore import options
+from typing_extensions import Self
 
 from . import pytiledbsoma as clib
 from ._types import is_nonstringy_sequence
-from ._util import _cast_domainish, pa_types_is_string_or_bytes, to_unix_ts
+from ._util import pa_types_is_string_or_bytes, to_unix_ts
 
 if TYPE_CHECKING:
     from ._soma_array import SOMAArray
 
 
-class CoordinateValueFilter:
-    _handle: clib.SOMAIndexColumnFilter
+@attrs.define(frozen=True)
+class CoordinateSelection:
     _array: SOMAArray
+    _handle: clib.CoordinateValueFilter = attrs.field(init=False)
 
     @classmethod
-    def create(cls, array: SOMAArray, coords: options.SparseDFCoords | options.SparseNDCoords | options.DenseNDCoords):
+    def create(
+        cls, array: SOMAArray, coords: options.SparseDFCoords | options.SparseNDCoords | options.DenseNDCoords
+    ) -> Self:
         if not is_nonstringy_sequence(coords):
             raise TypeError(f"The coords type {type(coords)} must be a regular sequence, not str or bytes")
 
@@ -34,58 +39,70 @@ class CoordinateValueFilter:
                 f"The coords ({len(coords)} elements) must be shorter than ndim ({len(array._handle._handle.dimension_names)})",
             )
 
-        qc = cls(array)
+        value_filter = cls(array)
         for column_index, coord in enumerate(coords):
-            qc.add_coordinate_selection(column_index, coord)
+            value_filter.add_coordinate_selection(column_index, coord)
+        return value_filter
 
-    def __init__(self, array: SOMAArray):
-        self._array = array
-        self._handle = clib.SOMAIndexQueryCondition(array)
+    def __attrs_post_init__(self) -> None:
+        object.__setattr__(self, "_handle", clib.CoordinateValueFilter(self._array._handle._handle))
 
     def add_coordinate_selection(
-        self, column_index: int, coord: options.SparseDFCoord | options.SparseNDCoord | options.DenseNDCoord
+        self, column_index: int, coord: options.SparseDFCoord | options.SparseNDCoord | options.DenseCoord
     ) -> None:
         array_handle = self._array._handle._handle
         dim = array_handle.schema.field(column_index)
-        domain = _cast_domainish(array_handle.domain())[column_index]
         if dim.metadata is not None and dim.metadata[b"dtype"].decode("utf-8") == "WKB":
             raise NotImplementedError("Support for adding a selection to a geometry column is not yet implemented.")
+
+        if isinstance(coord, (pa.Array, pa.ChunkedArray)):
+            # self._handle.add_arrow_points(column_index, coord)
+            # return
+            raise NotImplementedError  # TODO
+
         if isinstance(coord, slice):
             if coord.step is not None and coord.step != 1:
                 raise ValueError(
                     f"Invalid coordinate selection on column number '{column_index}'. Slice steps are not supported."
                 )
-            start = coord.start
-            stop = coord.stop
+
             if pa_types_is_string_or_bytes(dim.type):
-                dim_type = type(domain[0])
-                # A ``None`` or empty start is always equivalent to empty str/bytes.
-                start = coord.start or dim_type()
-                if coord.stop is None:
-                    # There's no way to specify "to infinity" for strings. We have to use this to get the nonempty domain
-                    # and use that in the end.
-                    ned = _cast_domainish(array_handle._handle.non_empty_domain())
-                    _, stop = ned[column_index]
-                else:
-                    stop = coord.stop
-                return self._handle.add_string_slice(start, stop)
-            if pa.types.is_timestamp(dim.type) and (
-                (start is None or isinstance(start, (np.datetime64, pa.TimestampScalar, int)))
-                and (stop is None or isinstance(stop, (np.datetime64, pa.TimestampScalar, int)))
-            ):
-                # Convert datetime stamp to int64.
-                istart = to_unix_ts(coord.start or domain[0])
-                istop = to_unix_ts(coord.stop or domain[1])
-                return self._handle.add_int64_slice(column_index, istart, istop)
-            return self._handle.add_slice(column_index, start, stop)
-        if isinstance(coord, (str, bytes)):
-            return self._handle.add_string_points(column_index, coord)
-        if isinstance(coord, (pa.Array, pa.ChunkedArray)):
-            return self._handle.add_arrow_points(column_index, coord)
+                self._handle.add_slice_string(column_index, coord.start, coord.stop)
+                return
+
+            if pa.types.is_timestamp(dim.type):
+                if (coord.start is None or isinstance(coord.start, (np.datetime64, pa.TimestampScalar, int))) and (
+                    coord.stop is None or isinstance(coord.stop, (np.datetime64, pa.TimestampScalar, int))
+                ):
+                    # Convert datetime stamp to int64.
+                    start = None if coord.start is None else to_unix_ts(coord.start)
+                    stop = None if coord.stop is None else to_unix_ts(coord.stop)
+                    self._handle.add_slice_int64(column_index, start, stop)
+                    return
+                raise TypeError("Unexpected type on column.")  # TODO Better error message before merging
+
+            add_slice_function = getattr(self._handle, f"add_slice_{dim.type}")
+            add_slice_function(column_index, coord.start, coord.stop)
+            return
+
+        if pa_types_is_string_or_bytes(dim.type):
+            if isinstance(coord, np.ndarray) and coord.ndim != 1:
+                raise ValueError(
+                    f"Cannot set points on column index={column_index}. Can only use 1D numpy arrays, but got an array with {coord.ndim}."
+                )
+            self._handle.add_points_string(column_index, coord)
+            return
+
+        if pa.types.is_timestamp(dim.type):
+            raise NotImplementedError  # TODO
+
         if isinstance(coord, (Sequence, np.ndarray)):
             if isinstance(coord, np.ndarray) and coord.ndim != 1:
                 raise ValueError(
                     f"Cannot set points on column index={column_index}. Can only use 1D numpy arrays, but got an array with {coord.ndim}."
                 )
-            self._handle.add_points(column_index, coord)
-        raise TypeError("Unhandled type {dim.type} for index column named {dim.name}.")
+            add_points_function = getattr(self._handle, f"add_points_{dim.type}")
+            add_points_function(column_index, coord)
+            return
+
+        raise TypeError  # TODO: Add error message
--- apis/python/src/tiledbsoma/coordinate_selection.cc
+++ apis/python/src/tiledbsoma/coordinate_selection.cc
@@ -27,17 +27,48 @@ namespace py = pybind11;
 using namespace py::literals;
 using namespace tiledbsoma;
 
-void load_column_filter(py::module& m) {
+template <typename T>
+void add_points(CoordinateValueFilter& value_filter, int64_t col_index, const std::vector<T>& values) {
+    value_filter.add_points<T>(col_index, SOMAPointSelection<T>(values));
+}
+
+template <typename T>
+void add_slice(CoordinateValueFilter& value_filter, int64_t col_index, std::optional<T> start, std::optional<T> stop) {
+    value_filter.add_slice<T>(col_index, SOMASliceSelection<T>(start, stop));
+}
+
+void load_coordinate_selection(py::module& m) {
     py::class_<CoordinateValueFilter>(m, "CoordinateValueFilter")
-        .def(py::init<const SOMAArray&>())
+        .def(py::init([](SOMAArray array) { return array.create_coordinate_value_filter(); }), py::arg("array"))
         .def(
-            "add_arrow_points", []() {}, "column_index"_a, "points"_a)
-        .def("add_int64_slice", &CoordinateValueFilter::<int64_t>add_slice)
-        .def("add_int64_point_selection", &SOMAIndexColumnFilter::<int64_t>add_points)
-        .def("add_point_selection", &CoordinateValueFilter::add_points)
-        .def("add_slice", &CoordinateValueFilter::add_slice)
-        .def("add_string_slice", &CoordinateValueFilter::add_slice)
-        .def("add_string_point_selection", &CoordinateValueFilter::add_string_points);
+            "add_slice_string",
+            [](CoordinateValueFilter& value_filter,
+               int64_t col_index,
+               const std::optional<std::string>& start,
+               const std::optional<std::string>& stop) {
+                value_filter.add_slice(col_index, SOMASliceSelection<std::string>(start, stop));
+            })
+        .def("add_slice_int8", add_slice<int8_t>)
+        .def("add_slice_int16", add_slice<int16_t>)
+        .def("add_slice_int32", add_slice<int32_t>)
+        .def("add_slice_int64", add_slice<int64_t>)
+        .def("add_slice_uint8", add_slice<uint8_t>)
+        .def("add_slice_uint16", add_slice<uint16_t>)
+        .def("add_slice_uint32", add_slice<uint32_t>)
+        .def("add_slice_uint64", add_slice<uint64_t>)
+        .def(
+            "add_point_string",
+            [](CoordinateValueFilter& value_filter, int64_t col_index, const std::vector<std::string>& values) {
+                value_filter.add_points(col_index, SOMAPointSelection<std::string>(values));
+            })
+        .def("add_point_int8", add_points<int8_t>)
+        .def("add_point_int16", add_points<int16_t>)
+        .def("add_point_int32", add_points<int32_t>)
+        .def("add_point_int64", add_points<int64_t>)
+        .def("add_point_uint8", add_points<uint8_t>)
+        .def("add_point_uint16", add_points<uint16_t>)
+        .def("add_point_uint32", add_points<uint32_t>)
+        .def("add_point_uint64", add_points<uint64_t>);
 }
 
 }  // namespace libtiledbsomacpp
