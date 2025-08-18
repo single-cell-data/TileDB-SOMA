c4fda7fc1b4db2dca3efd21ce506287761f4337f
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2025-08-18 16:42:14 +0300

    [HOTFIX] Upgrading the xcode-pinned-version in CI (#4203)
    * Upgrading the xcode-pinned-version in CI
    
    * small ctx already in use gc and deletions limiting used mem
    
    * pre-commit fixes
    
    * Condtional xcode-version for packaging
    
    * Deprecate macos-13
    
    * Roll-back change to macos runner size
    
    * Pin different versions of xcode depending on macos version
    
    * Remove macos checks from Ubuntu-only ci
    
    * Fix macos-15 xcode version
    
    * Garbage collection
    
    * More garbage collection
    
    * Macos-15 for smoke tests on arm-64
    
    * Macos-13 for smoke tests on x86-64
    
    ---------
    
    Co-authored-by: Julia Dark <julia@tiledb.com>

--- apis/python/tests/test_soma_experiment_versions.py
+++ apis/python/tests/test_soma_experiment_versions.py
@@ -1,3 +1,4 @@
+import gc
 import os
 
 import pytest
@@ -53,6 +54,9 @@ def test_to_anndata(soma_tiledb_context, version, name_and_expected_shape):
                 assert adata.obsp[key].shape == (expected_nobs, expected_nobs)
 
             assert adata.varm["PCs"].shape == (expected_nvar, 50)
+
+        del adata
+        gc.collect()
 
 
 @pytest.mark.parametrize(
