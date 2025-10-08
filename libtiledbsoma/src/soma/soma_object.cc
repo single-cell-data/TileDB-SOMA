7e0b516c9f91cbf04466a4b1495b34dfa74ed101
Julia Dark <24235303+jp-dark@users.noreply.github.com>
2025-10-08 10:41:56 -0400

    Improve error messages when unable to open the TileDB object (#4276)

--- libtiledbsoma/src/soma/soma_object.cc
+++ libtiledbsoma/src/soma/soma_object.cc
@@ -26,69 +26,101 @@ std::unique_ptr<SOMAObject> SOMAObject::open(
     std::shared_ptr<SOMAContext> ctx,
     std::optional<TimestampRange> timestamp,
     std::optional<std::string> soma_type) {
-    if (soma_type == std::nullopt) {
-        auto tiledb_type = Object::object(*ctx->tiledb_ctx(), std::string(uri)).type();
-        soma_type = util::soma_type_from_tiledb_type(tiledb_type);
+    auto tiledb_type = Object::Type::Invalid;
+    if (soma_type.has_value()) {
+        if (soma_type == "SOMAArray") {
+            tiledb_type = Object::Type::Array;
+        } else if (soma_type == "SOMAGroup") {
+            tiledb_type = Object::Type::Group;
+        } else {
+            throw TileDBSOMAError(fmt::format("Internal error: Invalid soma base type '{}'.", soma_type.value()));
+        }
+    } else {
+        tiledb_type = Object::object(*ctx->tiledb_ctx(), std::string(uri)).type();
     }
 
-    if (soma_type == "SOMAArray") {
-        auto array_ = SOMAArray::open(mode, uri, ctx, timestamp);
-        auto array_type = array_->type();
+    switch (tiledb_type) {
+        case Object::Type::Array: {
+            auto array_ = SOMAArray::open(mode, uri, ctx, timestamp);
+            auto array_type = array_->type();
 
-        if (!array_type.has_value())
-            throw TileDBSOMAError(fmt::format("[SOMAObject::open] SOMAArray '{}' has no type info", uri));
+            if (!array_type.has_value()) {
+                throw TileDBSOMAError(
+                    fmt::format(
+                        "Unable to open the TileDB array at '{}'. The array is missing the required '{}' metadata "
+                        "key.",
+                        uri,
+                        SOMA_OBJECT_TYPE_KEY));
+            }
 
-        std::transform(array_type->begin(), array_type->end(), array_type->begin(), [](unsigned char c) {
-            return std::tolower(c);
-        });
+            std::transform(array_type->begin(), array_type->end(), array_type->begin(), [](unsigned char c) {
+                return std::tolower(c);
+            });
 
-        if (array_type == "somadataframe") {
-            return std::make_unique<SOMADataFrame>(*array_);
-        } else if (array_type == "somasparsendarray") {
-            return std::make_unique<SOMASparseNDArray>(*array_);
-        } else if (array_type == "somadensendarray") {
-            return std::make_unique<SOMADenseNDArray>(*array_);
-        } else if (array_type == "somapointclouddataframe") {
-            return std::make_unique<SOMAPointCloudDataFrame>(*array_);
-        } else if (array_type == "somageometrydataframe") {
-            return std::make_unique<SOMAGeometryDataFrame>(*array_);
-        } else {
-            throw TileDBSOMAError("[SOMAObject::open] Saw invalid SOMAArray type");
+            if (array_type == "somadataframe") {
+                return std::make_unique<SOMADataFrame>(*array_);
+            } else if (array_type == "somasparsendarray") {
+                return std::make_unique<SOMASparseNDArray>(*array_);
+            } else if (array_type == "somadensendarray") {
+                return std::make_unique<SOMADenseNDArray>(*array_);
+            } else if (array_type == "somapointclouddataframe") {
+                return std::make_unique<SOMAPointCloudDataFrame>(*array_);
+            } else if (array_type == "somageometrydataframe") {
+                return std::make_unique<SOMAGeometryDataFrame>(*array_);
+            } else {
+                throw TileDBSOMAError(
+                    fmt::format(
+                        "Unable to open the TileDB array at '{}' with unrecognized SOMA array type '{}'.",
+                        uri,
+                        array_->type().value()));
+            }
         }
-    } else if (soma_type == "SOMAGroup") {
-        auto group_ = SOMAGroup::open(mode, uri, ctx, "", timestamp);
-        auto group_type = group_->type();
+        case Object::Type::Group: {
+            auto group_ = SOMAGroup::open(mode, uri, ctx, "", timestamp);
+            auto group_type = group_->type();
 
-        if (!group_type.has_value())
-            throw TileDBSOMAError(fmt::format("[SOMAObject::open] SOMAGroup '{}' has no type info", uri));
+            if (!group_type.has_value()) {
+                throw TileDBSOMAError(
+                    fmt::format(
+                        "Unable to open the TileDB group at '{}'. The group is missing the required '{}' metadata "
+                        "key.",
+                        uri,
+                        SOMA_OBJECT_TYPE_KEY));
+            }
 
-        std::transform(group_type->begin(), group_type->end(), group_type->begin(), [](unsigned char c) {
-            return std::tolower(c);
-        });
+            std::transform(group_type->begin(), group_type->end(), group_type->begin(), [](unsigned char c) {
+                return std::tolower(c);
+            });
 
-        if (group_type == "somacollection") {
-            return std::make_unique<SOMACollection>(*group_);
-        } else if (group_type == "somaexperiment") {
-            return std::make_unique<SOMAExperiment>(*group_);
-        } else if (group_type == "somameasurement") {
-            return std::make_unique<SOMAMeasurement>(*group_);
-        } else if (group_type == "somascene") {
-            return std::make_unique<SOMAScene>(*group_);
-        } else if (group_type == "somamultiscaleimage") {
-            return std::make_unique<SOMAMultiscaleImage>(*group_);
-        } else {
-            throw TileDBSOMAError("[SOMAObject::open] Saw invalid SOMAGroup type");
+            if (group_type == "somacollection") {
+                return std::make_unique<SOMACollection>(*group_);
+            } else if (group_type == "somaexperiment") {
+                return std::make_unique<SOMAExperiment>(*group_);
+            } else if (group_type == "somameasurement") {
+                return std::make_unique<SOMAMeasurement>(*group_);
+            } else if (group_type == "somascene") {
+                return std::make_unique<SOMAScene>(*group_);
+            } else if (group_type == "somamultiscaleimage") {
+                return std::make_unique<SOMAMultiscaleImage>(*group_);
+            } else {
+                throw TileDBSOMAError(
+                    fmt::format(
+                        "Unable to open the TileDB group at '{}' with unrecognized SOMA group type '{}'.",
+                        uri,
+                        group_->type().value()));
+            }
         }
+        default:
+            throw TileDBSOMAError(fmt::format("The object at URI '{}' is not a valid TileDB array or group.", uri));
     }
-
-    throw TileDBSOMAError("[SOMAObject::open] Invalid TileDB object passed to SOMAObject::open");
 }
 
 const std::optional<std::string> SOMAObject::type() {
     auto soma_object_type = this->get_metadata(SOMA_OBJECT_TYPE_KEY);
 
-    if (!soma_object_type.has_value())
+    if (!soma_object_type.has_value()) {
         return std::nullopt;
+    }
 
     const char* dtype = (const char*)std::get<MetadataInfo::value>(*soma_object_type);
     uint32_t sz = std::get<MetadataInfo::num>(*soma_object_type);
