752f7a3e95a7b398a55bc2cd09cff77b6e0e2e54
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-11-13 15:47:31 +0200

    Use new column buffers in ArrayBuffers

--- libtiledbsoma/src/utils/common.h
+++ libtiledbsoma/src/utils/common.h
@@ -21,6 +21,20 @@
 
 namespace tiledbsoma {
 
+template <typename T>
+concept is_data_buffer = std::same_as<std::unique_ptr<std::byte[]>, T> ||
+                         (std::is_pointer_v<T> &&
+                          (std::same_as<std::remove_const_t<std::remove_pointer_t<T>>, void> ||
+                           std::same_as<std::remove_const_t<std::remove_pointer_t<T>>, std::byte> ||
+                           std::integral<std::remove_const_t<std::remove_pointer_t<T>>> ||
+                           std::floating_point<std::remove_const_t<std::remove_pointer_t<T>>>));
+
+template <typename T>
+concept is_offset_buffer = std::same_as<T, std::unique_ptr<uint64_t[]>> ||
+                           (std::is_pointer_v<T> &&
+                            (std::same_as<std::remove_const_t<std::remove_pointer_t<T>>, uint32_t> ||
+                             std::same_as<std::remove_const_t<std::remove_pointer_t<T>>, uint64_t>));
+
 constexpr std::string_view SOMA_JOINID = "soma_joinid";
 
 const std::string SOMA_OBJECT_TYPE_KEY = "soma_object_type";
