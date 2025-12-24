390d412eaceea910b7e1d911988d5d389e40735c
Aaron Wolen <aaron@wolen.com>
2025-12-24 14:30:18 -0600

    Add tests for collection$add_new_ methods

--- apis/r/tests/testthat/test-00-carrara.R
+++ apis/r/tests/testthat/test-00-carrara.R
@@ -22,24 +22,24 @@ WORKSPACE_NAME <- Sys.getenv("CARRARA_TEST_WORKSPACE", "TileDB-Inc-Staging")
 TEAMSPACE_NAME <- Sys.getenv("CARRARA_TEST_TEAMSPACE", "aaron-dev")
 TEST_FOLDER <- Sys.getenv("CARRARA_TEST_FOLDER", "remote_test")
 REST_SERVER <- Sys.getenv(
-    "TILEDB_REST_SERVER_ADDRESS",
-    "https://api.staging.tiledb.io"
+  "TILEDB_REST_SERVER_ADDRESS",
+  "https://api.staging.tiledb.io"
 )
 
 BASE_URI <- sprintf(
-    "tiledb://%s/%s/%s",
-    WORKSPACE_NAME,
-    TEAMSPACE_NAME,
-    TEST_FOLDER
+  "tiledb://%s/%s/%s",
+  WORKSPACE_NAME,
+  TEAMSPACE_NAME,
+  TEST_FOLDER
 )
 
 clean_env <- function() {
-    env_vars <- names(Sys.getenv())
-    tiledb_vars <- which(grepl("CARRARA", env_vars) | grepl("TILEDB", env_vars))
-    for (var in env_vars[tiledb_vars]) {
-        message(sprintf("Unsetting %s", var))
-        Sys.unsetenv(var)
-    }
+  env_vars <- names(Sys.getenv())
+  tiledb_vars <- which(grepl("CARRARA", env_vars) | grepl("TILEDB", env_vars))
+  for (var in env_vars[tiledb_vars]) {
+    message(sprintf("Unsetting %s", var))
+    Sys.unsetenv(var)
+  }
 }
 
 create_arrow_table <- function(nrows = 10L, factors = FALSE) {
@@ -115,11 +115,125 @@ test_that("SOMADataFrame creation", {
 test_that("SOMACollection creation", {
   clean_env()
   uri <- create_group_path()
-  collection <- SOMACollectionCreate(uri = uri)
-  collection$close()
+  collection <- SOMACollectionCreate(uri = uri)$close()
 
   collection2 <- SOMACollectionOpen(uri)
   # Using equivalence because soma_type returns a named string for collections
   expect_equivalent(collection2$soma_type, "SOMACollection")
   expect_equal(length(collection2$names()), 0)
+})
+
+test_that("SOMACollection add_new_* methods", {
+  clean_env()
+  uri <- create_group_path()
+
+  schema <- arrow::schema(
+    arrow::field("soma_joinid", arrow::int64(), nullable = FALSE),
+    arrow::field("A", arrow::int32())
+  )
+  domain <- list(soma_joinid = c(0L, 100L))
+  type <- arrow::float32()
+  shape <- c(99L, 101L)
+
+  children <- paste0("child", 1:7)
+
+  # Create collection
+  collection <- SOMACollectionCreate(uri = uri)$close()
+
+  # child1: SOMACollection
+  child1_uri <- file.path(uri, "child1")
+  child1 <- SOMACollectionCreate(child1_uri)
+
+  # For Carrara URIs children created at nested URIs are automatically added
+  # to the parent, so this is a no-op.
+  collection <- SOMACollectionOpen(uri, mode = "WRITE")
+  expect_no_error({
+    collection$add_new_collection(child1, "child1")
+  })
+
+  # Carrara requires that the member name matches the final segment of the URI
+  expect_error(
+    collection$add_new_collection(child1, "not_child1"),
+    "Member name `not_child1` must match the final segment of the URI"
+  )
+  collection$close()
+
+  # child2: SOMAExperiment
+  child2_uri <- file.path(uri, "child2")
+  child2 <- SOMAExperimentCreate(child2_uri)$close()
+  child2 <- SOMACollectionOpen(child2_uri)
+  expect_true(child2$exists())
+  expect_equivalent(child2$soma_type, "SOMAExperiment")
+
+  # child3: SOMAMeasurement
+  child3_uri <- file.path(uri, "child3")
+  child3 <- SOMAMeasurementCreate(child3_uri)$close()
+  child3 <- SOMACollectionOpen(child3_uri)
+  expect_true(child3$exists())
+  expect_equivalent(child3$soma_type, "SOMAMeasurement")
+
+  # child4: SOMACollection
+  child4_uri <- file.path(uri, "child4")
+  child4 <- SOMACollectionCreate(child4_uri)$close()
+  child4 <- SOMACollectionOpen(child4_uri)
+  expect_true(child4$exists())
+  expect_equivalent(child4$soma_type, "SOMACollection")
+
+  # child5: SOMADataFrame
+  collection <- SOMACollectionOpen(uri, mode = "WRITE")
+  child5 <- collection$add_new_dataframe(
+    "child5d",
+    schema = schema,
+    index_column_names = "soma_joinid",
+    domain = domain
+  )
+  collection$close()
+
+  # child6: SOMASparseNDArray
+  collection <- SOMACollectionOpen(uri, mode = "WRITE")
+  child6 <- collection$add_new_sparse_ndarray(
+    "child6b",
+    type = type,
+    shape = shape
+  )
+  collection$close()
+
+  # # child7: SOMADenseNDArray
+  collection <- SOMACollectionOpen(uri, mode = "WRITE")
+  child7 <- collection$add_new_dense_ndarray(
+    "child7a",
+    type = type,
+    shape = shape
+  )
+  collection$close()
+
+  # # Verify
+  # collection <- SOMACollectionOpen(uri, mode = "READ")
+
+  # expect_equal(collection$length(), length(children))
+  # expect_setequal(collection$names(), children)
+
+  # # Verify types
+  # expect_equal(collection$get("child1")$soma_type, "SOMACollection")
+  # expect_equal(collection$get("child2")$soma_type, "SOMAExperiment")
+  # expect_equal(collection$get("child3")$soma_type, "SOMAMeasurement")
+  # expect_equal(collection$get("child4")$soma_type, "SOMACollection")
+  # expect_equal(collection$get("child5")$soma_type, "SOMADataFrame")
+  # expect_equal(collection$get("child6")$soma_type, "SOMASparseNDArray")
+  # expect_equal(collection$get("child7")$soma_type, "SOMADenseNDArray")
+
+  # # Verify properties for child5, child6, child7
+  # c5 <- collection$get("child5")
+  # expect_equal(c5$schema(), schema)
+  # expect_equal(c5$domain(), domain)
+
+  # c6 <- collection$get("child6")
+  # expect_equal(c6$type(), type)
+  # expect_equal(c6$shape(), bit64::as.integer64(shape))
+
+  # c7 <- collection$get("child7")
+  # expect_equal(c7$type(), type)
+  # expect_equal(c7$shape(), bit64::as.integer64(shape))
+
+  # collection$close()
 })
