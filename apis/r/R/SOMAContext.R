2d75052f511bfc6c5f532508af3de7ce86c7174c
Aaron Wolen <aaron@wolen.com>
2025-12-31 09:57:15 -0600

    add methods to check tiledbv2 and tiledbv3 semantics

--- apis/r/R/SOMAContext.R
+++ apis/r/R/SOMAContext.R
@@ -32,8 +32,19 @@ SOMAContext <- R6::R6Class(
     #'
     get_data_protocol = function(uri) {
       return(get_data_protocol_from_soma_context(private$.handle, uri))
-    }
+    },
 
+    #' @param uri A URI for a SOMA object
+    #' @return TRUE if the URI will use `tiledbv2` semantics.
+    is_tiledbv2 = function(uri) {
+      self$get_data_protocol(uri) == "tiledbv2"
+    },
+
+    #' @param uri A URI for a SOMA object
+    #' @return TRUE if the URI will use `tiledbv3` semantics.
+    is_tiledbv3 = function(uri) {
+      self$get_data_protocol(uri) == "tiledbv3"
+    }
   ),
   active = list(
     #' @field handle External pointer to the C++ interface
@@ -44,7 +55,6 @@ SOMAContext <- R6::R6Class(
       }
       return(private$.handle)
     }
-
   ),
   private = list(
     # @field Internal 'external pointer' for C++ interface ...
