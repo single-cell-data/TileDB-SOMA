d2526e4e43a3c0d64e5a09c5fc9e57e350ad78d0
Julia Dark <24235303+jp-dark@users.noreply.github.com>
2026-01-13 12:15:47 -0500

    Apply suggestions from code review
    Co-authored-by: Aaron Wolen <aaron@wolen.com>

--- apis/r/R/SOMAContext.R
+++ apis/r/R/SOMAContext.R
@@ -51,14 +51,24 @@ SOMAContext <- R6::R6Class(
   )
 )
 
-#' Create and return a global default context object
+#' Set the Default Global Context
+#'
+#' Configure a default \code{\link{SOMAContext}} to be used by all TileDB-SOMA
+#' operations when no explicit context is provided.
+#'
+#' This function should be called once at the beginning of your session before
+#' opening any SOMA objects if you want to customize the TileDB context
+#' parameters that will apply to all subsequent operations. Otherwise, a default
+#' context will be created automatically with standard parameters when you first
+#' open a SOMA object.
 #'
 #' If the global context was already set, an error will be raised unless
 #' \code{replace=True}. Setting a new default context will not change the
 #' context for TileDB-SOMA objects that were already created.
 #'
 #' @template param-config
-#' @param replace Allow replacing an existing default context with a new default context
+#' @param replace Allow replacing the existing default context with new
+#' configuration parameters.
 #'
 #' @return Invisibly, the default default context object.
 #'
