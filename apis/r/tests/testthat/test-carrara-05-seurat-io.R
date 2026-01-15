68130d6fdb3552e4a93a902ce6d7612c5ca77779
Aaron Wolen <aaron@wolen.com>
2026-01-15 15:24:13 -0600

    Simplify carrara test setup for Seurat

--- apis/r/tests/testthat/test-carrara-05-seurat-io.R
+++ apis/r/tests/testthat/test-carrara-05-seurat-io.R
@@ -5,19 +5,7 @@ test_that("write_soma Seurat with no error", {
   skip_if_not_installed("SeuratObject", .MINIMUM_SEURAT_VERSION("c"))
   with_carrara_env()
 
-  # Get test data and simplify for faster testing
-  pbmc_small <- get_data("pbmc_small", package = "SeuratObject")
-  # Remove command logs to simplify
-  for (cmd in SeuratObject::Command(pbmc_small)) {
-    pbmc_small[[cmd]] <- NULL
-  }
-
-  # Remove scale.data
-  pbmc_small[["RNA"]]@counts <- new("matrix")
-  pbmc_small[["RNA"]]@scale.data <- new("matrix")
-
-  # Remove reductions
-  pbmc_small@reductions <- list()
+  pbmc_small <- get_test_seurat_object()
 
   uri <- carrara_group_path()
 
