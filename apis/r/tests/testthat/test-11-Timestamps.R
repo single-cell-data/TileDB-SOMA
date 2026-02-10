2e19f432372064a97b441ed65b6d3898ee8bb317
Julia Dark <julia@tiledb.com>
2026-02-10 16:57:33 -0500

    Remove timestamp tests before metadata write

--- apis/r/tests/testthat/test-11-Timestamps.R
+++ apis/r/tests/testthat/test-11-Timestamps.R
@@ -92,11 +92,6 @@ test_that("SOMANDSparseArray", {
   res <- snda$read()$tables()$concat()
   expect_equal(dim(res), c(60, 3))
 
-  ## read at t = 1, expect zero rows as read is from (0, 1)
-  ts3 <- as.POSIXct(1, tz = "UTC", origin = "1970-01-01")
-  snda <- tiledbsoma::SOMASparseNDArrayOpen(uri, tiledb_timestamp = ts3)
-  res <- snda$read()$tables()$concat()
-  expect_equal(dim(res), c(0, 3))
 })
 
 test_that("SOMANDDenseArray", {
@@ -118,14 +113,4 @@ test_that("SOMANDDenseArray", {
   dnda <- tiledbsoma::SOMADenseNDArrayOpen(uri, tiledb_timestamp = ts2)
   res <- dnda$read_arrow_table()
   expect_equal(dim(res), c(10, 1))
-
-  ## read at t = 1, expect zero rows as read is from (0, 1)
-  ## NB that this requires a) nullable arrow schema and b) na.omit() on result
-  ## It also works without a) as the fill value is also NA
-  ts3 <- as.POSIXct(1, tz = "UTC", origin = "1970-01-01")
-  dnda <- tiledbsoma::SOMADenseNDArrayOpen(uri, tiledb_timestamp = ts3)
-  res <- dnda$read_arrow_table()
-  # print(res$soma_data)
-  vec <- tibble::as_tibble(res)
-  expect_equal(dim(na.omit(vec)), c(0, 1))
 })
